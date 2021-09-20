4f60bab983e0ccba27c53827dc28e775c2f35a1c
David Spohr <spohr.david@gmail.com>
2021-09-20 17:03:41 +0200

    Fixed javascript selectors

--- production/mafra/idnes.cz_hry.js
+++ production/mafra/idnes.cz_hry.js
@@ -1,8 +1,8 @@
 window.cpexWebsiteSettings = {
   formats: {
     skin: {
-      backgroundEl: document.getElementById('main'),
-      contentEl: document.getElementById('portal')
+      backgroundEl: (document.getElementById('main')),
+      contentEl: (document.getElementById('portal'))
     }
   }
 }
