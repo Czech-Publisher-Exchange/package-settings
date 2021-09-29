ae98946460b8a8b92d59aef41b96f8e3cf8a7b6f
User nodejs app management <powernodejs@cdn01.cpex.cz>
2021-09-29 16:10:03 +0200

    David: Znovu povoleny formáty

--- production/csfd/csfd.cz.js
+++ production/csfd/csfd.cz.js
@@ -3,32 +3,64 @@ window.cpexWebsiteSettings = {
     name: 'csfd.cz'
   },
   headerbidding: {
-    adUnits: [
-      {
-        code: 'rectangle-1',
-        mediaTypes: {
-          banner: { sizes: [[300, 250], [300, 300], [250, 250]] }
-        },
-        bids: [
-          { bidder: 'rubicon', params: { accountId: '10900', siteId: 140646, zoneId: 1713098, position: 'atf', sizes: [15, 48, 14], inventory: {} } },
-          { bidder: 'pubmatic', params: { publisherId: '158732', adSlot: '2918143' } },
-          { bidder: 'appnexus', params: { placementId: 19286498 } },
-          { bidder: 'rtbhouse', params: { region: 'prebid-eu', publisherId: 'w6O8SDnYNMT7Cb6AHGZf' } }
-        ]
-      }
-    ]
+    adUnits: [{
+      code: 'rectangle-1',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [300, 300],
+            [250, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          accountId: '10900',
+          siteId: 140646,
+          zoneId: 1713098,
+          position: 'atf',
+          sizes: [15, 48, 14],
+          inventory: {}
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '2918143'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 19286498
+        }
+      }, {
+        bidder: 'rtbhouse',
+        params: {
+          region: 'prebid-eu',
+          publisherId: 'w6O8SDnYNMT7Cb6AHGZf'
+        }
+      }]
+    }]
   },
   formats: {
     skin: {
       backgroundEl: (document.body),
       contentEl: (document.getElementById('page-wrapper')),
-      hide: [(document.getElementById('leaderboard-wrapper'))]
+      hide: [(document.getElementById('leaderboard-wrapper'))],
+      enabled: true
     },
     slideup: {
-      adUnit: 'slide_up'
+      adUnit: 'slide_up',
+      enabled: true
     },
     vignette: {
-      adUnit: 'cpex_vignette'
+      adUnit: 'cpex_vignette',
+      enabled: true
+    },
+    interscroller: {
+      enabled: true
     }
   }
 }
