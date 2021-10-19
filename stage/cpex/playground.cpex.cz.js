15c095764adfd694841f3b666af1ee0ca4da420e
CPEx Dev <dev@cpex.cz>
2021-10-19 13:26:22 +0200

    David: Test commitu z aplikace editoru

--- stage/cpex/playground.cpex.cz.js
+++ stage/cpex/playground.cpex.cz.js
@@ -1,53 +1,128 @@
 window.cpexWebsiteSettings = {
   website: {
     name: 'cpex.cz',
-    note: 'Test'
+    note: 'Test z editoru'
   },
   headerbidding: {
-    adUnits: [
-      {
-        code: 'rectangle-1',
-        mediaTypes: {
-          banner: { sizes: [[300, 250], [300, 300], [250, 250]] }
-        },
-        bids: [
-          { bidder: 'rubicon', params: { accountId: '10900', siteId: 140646, zoneId: 1713098, position: 'atf', sizes: [15, 48, 14], inventory: {} } },
-          { bidder: 'pubmatic', params: { publisherId: '158732', adSlot: '2918143' } },
-          { bidder: 'appnexus', params: { placementId: 19286498 } },
-          { bidder: 'rtbhouse', params: { region: 'prebid-eu', publisherId: 'w6O8SDnYNMT7Cb6AHGZf' } }
-        ]
+    adUnits: [{
+      code: 'rectangle-1',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [300, 300],
+            [250, 250]
+          ]
+        }
       },
-      {
-        code: 'rectangle-2',
-        mediaTypes: {
-          banner: { sizes: [[300, 250], [300, 300], [250, 250]] }
-        },
-        bids: [
-          { bidder: 'rubicon', params: { accountId: '10900', siteId: 140646, zoneId: 1713100, position: 'atf', sizes: [15, 48, 14], inventory: {} } },
-          { bidder: 'pubmatic', params: { publisherId: '158732', adSlot: '2918144' } },
-          { bidder: 'appnexus', params: { placementId: 19286538 } }
-        ]
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          accountId: '10900',
+          siteId: 140646,
+          zoneId: 1713098,
+          position: 'atf',
+          sizes: [15, 48, 14],
+          inventory: {}
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '2918143'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 19286498
+        }
+      }, {
+        bidder: 'rtbhouse',
+        params: {
+          region: 'prebid-eu',
+          publisherId: 'w6O8SDnYNMT7Cb6AHGZf'
+        }
+      }]
+    }, {
+      code: 'rectangle-2',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [300, 300],
+            [250, 250]
+          ]
+        }
       },
-      {
-        code: 'leaderboard-1',
-        mediaTypes: {
-          banner: { sizes: [[728, 90], [2000, 1400]] }
-        },
-        bids: [
-          { bidder: 'rubicon', params: { accountId: '10900', siteId: 140646, zoneId: 1960456, position: 'atf', sizes: [2, 230], inventory: {} } },
-          { bidder: 'appnexus', params: { placementId: 21341120 } }
-        ]
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          accountId: '10900',
+          siteId: 140646,
+          zoneId: 1713100,
+          position: 'atf',
+          sizes: [15, 48, 14],
+          inventory: {}
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '2918144'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 19286538
+        }
+      }]
+    }, {
+      code: 'leaderboard-1',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [728, 90],
+            [2000, 1400]
+          ]
+        }
       },
-      {
-        code: 'native-2',
-        mediaTypes: {
-          banner: { sizes: [[548, 150]] }
-        },
-        bids: [
-          { bidder: 'rubicon', params: { accountId: '10900', siteId: 140646, zoneId: 2062058, position: 'btf', sizes: [90], inventory: {} } }
-        ]
-      }
-    ]
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          accountId: '10900',
+          siteId: 140646,
+          zoneId: 1960456,
+          position: 'atf',
+          sizes: [2, 230],
+          inventory: {}
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 21341120
+        }
+      }]
+    }, {
+      code: 'native-2',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [548, 150]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          accountId: '10900',
+          siteId: 140646,
+          zoneId: 2062058,
+          position: 'btf',
+          sizes: [90],
+          inventory: {}
+        }
+      }]
+    }]
   },
   adserver: {
     loadPrerequisites: true,
@@ -68,37 +143,8 @@ window.cpexWebsiteSettings = {
     },
     native: {
       templates: {
-        generic: `
-          <style>
-            .cpex-native a { display: flex; text-decoration: none; color: inherit }
-            .cpex-native img { width: 200px; object-fit: cover }
-            .cpex-native .text { padding-left: 20px }
-          </style>
-          <div class='cpex-native article'>
-            <a href='\${link}' target='_blank' >
-              <img src='\${img}'>
-              <div class='text'>
-                <h2>\${title}</h2>
-                <p>\${desc}</p>
-              </div>
-            </a>
-          </div>`,
-        'native-2': `
-          <style>
-            .cpex-native.individual { background-color: #c9b3ff }
-            .cpex-native a { display: flex; text-decoration: none; color: inherit }
-            .cpex-native img { width: 200px; object-fit: cover }
-            .cpex-native .text { padding-left: 20px }
-          </style>
-          <div class='cpex-native article individual'>
-            <a href='\${link}' target='_blank' >
-              <img src='\${img}'>
-              <div class='text'>
-                <h2>Custom template for an adUnit</h2>
-                <p>You use any custom HTML, CSS and content for a specific adUnit name. It overrides anything present in template properties of the native settings.</p>
-              </div>
-            </a>
-          </div>`
+        generic: "\n          <style>\n            .cpex-native a { display: flex; text-decoration: none; color: inherit }\n            .cpex-native img { width: 200px; object-fit: cover }\n            .cpex-native .text { padding-left: 20px }\n          </style>\n          <div class='cpex-native article'>\n            <a href='${link}' target='_blank' >\n              <img src='${img}'>\n              <div class='text'>\n                <h2>${title}</h2>\n                <p>${desc}</p>\n              </div>\n            </a>\n          </div>",
+        'native-2': "\n          <style>\n            .cpex-native.individual { background-color: #c9b3ff }\n            .cpex-native a { display: flex; text-decoration: none; color: inherit }\n            .cpex-native img { width: 200px; object-fit: cover }\n            .cpex-native .text { padding-left: 20px }\n          </style>\n          <div class='cpex-native article individual'>\n            <a href='${link}' target='_blank' >\n              <img src='${img}'>\n              <div class='text'>\n                <h2>Custom template for an adUnit</h2>\n                <p>You use any custom HTML, CSS and content for a specific adUnit name. It overrides anything present in template properties of the native settings.</p>\n              </div>\n            </a>\n          </div>"
       }
     }
   }
