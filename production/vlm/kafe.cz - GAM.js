fe1f5ffbc9f152838c19f803d49f1f6611d08d0f
CPEx Dev <dev@cpex.cz>
2021-11-16 14:21:03 +0100

    Iveta: settings pro native a skin na kafe.cz

---
+++ production/vlm/kafe.cz - GAM.js
@@ -0,0 +1,17 @@
+window.cpexWebsiteSettings = {
+  aam: {
+    settings: {
+      publisher: 'vlm'
+    }
+  },
+  formats: {
+    skin: {
+      enabled: true,
+      backgroundEl: (document.body),
+      contentEl: (document.getElementsByClassName("page")[0])
+    },
+    native: {
+      enabled: true
+    }
+  }
+}
