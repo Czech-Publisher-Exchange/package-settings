9ea8e7c62543ebf6184ddcfe8f5255c78792a166
CPEx Dev <dev@cpex.cz>
2021-12-14 15:24:23 +0100

    David: Added generic native template

--- production/vlm/kafe.cz_gam.js
+++ production/vlm/kafe.cz_gam.js
@@ -23,7 +23,10 @@ window.cpexWebsiteSettings = {
       hide: [(document.getElementsByClassName('leaderboard-top')[0])]
     },
     native: {
-      enabled: true
+      enabled: true,
+      templates: {
+        generic: "<article class='box-article'>\n  <header class='box-article-header'>\n    <div class='box-article-image'>\n      <a href='${link}'><div class='box-article-img'><img src='${img}'></div></a>\n    </div>\n    <div class='box-article-header-bottom'>\n      <div class='box-article-title'>\n        <a href='${link}'><h2>${title}</h2></a>\n      </div>\n    </div>\n  </header>\n  <blockquote class='box-article-excerpt'>${desc}</blockquote>\n</article>"
+      }
     }
   }
 }
