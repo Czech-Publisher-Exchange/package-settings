362d0412d196f54720b1c192c4624ab66fe25470
CPEx Dev <dev@cpex.cz>
2021-12-21 16:45:32 +0100

    Iveta: nové website settings

--- production/udv/ulovdomov.cz.js
+++ production/udv/ulovdomov.cz.js
@@ -1,5 +1,121 @@
 window.cpexWebsiteSettings = {
   website: {
     name: 'ulovdomov.cz'
+  },
+  aam: {
+    settings: {
+      publisher: 'udv'
+    }
+  },
+  headerbidding: {
+    adUnits: [{
+      code: 'billboard1',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [970, 310],
+            [970, 250],
+            [970, 210],
+            [970, 90],
+            [728, 90]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          accountId: '10900',
+          siteId: '401296',
+          zoneId: '2249216',
+          position: 'atf',
+          sizes: ['94', '57', '96', '55', '2']
+        }
+      }, {
+        bidder: 'rubicon',
+        params: {
+          accountId: '10900',
+          siteId: '401294',
+          zoneId: '2249194',
+          position: 'atf',
+          sizes: ['94', '57', '96', '55', '2']
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '401294'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: '759954',
+          id: '184929',
+          size: [
+            [970, 310],
+            [970, 250],
+            [970, 210],
+            [970, 90],
+            [728, 90]
+          ]
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: '23725134'
+        }
+      }]
+    }, {
+      code: 'mediumRectangle1',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 300],
+            [300, 250],
+            [250, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          accountId: '10900',
+          siteId: '401296',
+          zoneId: '2249218',
+          position: 'atf',
+          sizes: ['48', '15', '14']
+        }
+      }, {
+        bidder: 'rubicon',
+        params: {
+          accountId: '10900',
+          siteId: '401294',
+          zoneId: '2249212',
+          position: 'atf',
+          sizes: ['48', '15', '14']
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '401294'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: '759955',
+          id: '184929',
+          size: [
+            [300, 300],
+            [300, 250],
+            [250, 250]
+          ]
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: '23725141'
+        }
+      }]
+    }]
   }
 }
