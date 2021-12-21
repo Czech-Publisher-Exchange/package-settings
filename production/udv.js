962d943b8821d97e6431893b5c7d12f576cdb00e
CPEx Dev <dev@cpex.cz>
2021-12-21 15:22:28 +0100

    Iveta: nové publisher settings

--- production/udv.js
+++ production/udv.js
@@ -1,6 +1,18 @@
 window.cpexPublisherSettings = {
   publisher: {
     name: 'UlovDomov',
-    sellerId: '23'
+    sellerId: '23',
+    code: 'udv'
+  },
+  cmp: {
+    enabled: false
+  },
+  aam: {
+    settings: {
+      publisher: 'udv'
+    }
+  },
+  adserver: {
+    adapter: 'googletag'
   }
 }
