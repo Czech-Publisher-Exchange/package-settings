e77dbcee4cdac008b08c6ce51904ffdde82cfebb
CPEx Dev <dev@cpex.cz>
2022-01-12 15:54:29 +0100

    František: slunecnice.cz

---
+++ production/iinfo/slunecnice.cz.js
@@ -0,0 +1,16 @@
+window.cpexWebsiteSettings = {
+  aam: {
+    settings: {
+      publisher: 'iinfo'
+    }
+  },
+  adserver: {
+    adapter: 'sastracker',
+    allowedSSPs: {
+      pubmatic: 1,
+      index: 3,
+      magnite: 4,
+      xandr: 5
+    }
+  }
+}
