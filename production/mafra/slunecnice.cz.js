b3bc4b82b84793371b216ece3acca3b88d849013
CPEx Dev <dev@cpex.cz>
2022-01-12 15:19:47 +0100

    Frantisek: slunecnice.cz

---
+++ production/mafra/slunecnice.cz.js
@@ -0,0 +1,21 @@
+window.cpexWebsiteSettings = {
+  aam: {
+    settings: {
+      publisher: 'mafra'
+    }
+  },
+  adserver: {
+    adapter: 'sastracker',
+    allowedSSPs: {
+      pubmatic: 1,
+      index: 3,
+      magnite: 4,
+      xandr: 5
+    }
+  },
+  formats: {
+    native: {
+      enabled: true
+    }
+  }
+}
