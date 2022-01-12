c4b0af3cd4017e39483eb4035425caa933741fb1
CPEx Dev <dev@cpex.cz>
2022-01-12 15:09:39 +0100

    Frantisek: iinfo publisher settings

---
+++ production/iinfo.js
@@ -0,0 +1,20 @@
+window.cpexPublisherSettings = {
+  publisher: {
+    code: 'iinfo',
+    sellerId: '012'
+  },
+  aam: {
+    settings: {
+      publisher: 'iinfo'
+    }
+  },
+  adserver: {
+    adapter: 'sastracker',
+    allowedSSPs: {
+      pubmatic: 1,
+      index: 3,
+      magnite: 4,
+      xandr: 5
+    }
+  }
+}
