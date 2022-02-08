73c7cdb62188d1add43d3096edf1325444ae1c12
CPEx Dev <dev@cpex.cz>
2022-02-08 17:46:12 +0100

    Frantisek: 1st settings

---
+++ production/eco.js
@@ -0,0 +1,24 @@
+window.cpexPublisherSettings = {
+  publisher: {
+    code: 'eco',
+    name: 'Economia',
+    sellerId: '002'
+  },
+  cmp: {
+    enabled: false
+  },
+  aam: {
+    settings: {
+      publisher: 'eco'
+    }
+  },
+  adserver: {
+    adapter: 'sasic',
+    allowedSSPs: {
+      pubmatic: 2,
+      index: 1228,
+      magnite: 1503,
+      xandr: 2919
+    }
+  }
+}
