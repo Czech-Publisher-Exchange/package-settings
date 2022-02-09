8bab72a684ac7f072fe7d5e27330a1933f825424
CPEx Dev <dev@cpex.cz>
2022-02-09 09:12:33 +0100

    Frantisek: zakladni hb settings

--- production/eco/vareni.cz.js
+++ production/eco/vareni.cz.js
@@ -6,5 +6,230 @@ window.cpexWebsiteSettings = {
     settings: {
       publisher: 'eco'
     }
+  },
+  adserver: {
+    allowedSSPs: {
+      pubmatic: 1,
+      index: 3,
+      magnite: 4,
+      xandr: 5
+    }
+  },
+  headerbidding: {
+    adUnits: [{
+      code: 'leader',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [2000, 1400]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          accountId: '10900',
+          siteId: '361048',
+          zoneId: '1958646',
+          position: 'atf',
+          sizes: ['230']
+        }
+      }]
+    }, {
+      code: 'leader',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [970, 310],
+            [970, 250],
+            [970, 210],
+            [970, 100],
+            [728, 90]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          accountId: '10900',
+          siteId: '361048',
+          zoneId: '1965808',
+          position: 'atf',
+          sizes: ['94', '57', '96', '95', '2']
+        }
+      }]
+    }, {
+      code: 'megaboard',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [970, 310],
+            [970, 250],
+            [970, 210],
+            [970, 100],
+            [728, 90]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          accountId: '10900',
+          siteId: '361048',
+          zoneId: '1965820',
+          position: 'atf',
+          sizes: ['94', '57', '96', '95', '2']
+        }
+      }]
+    }, {
+      code: 'wallpaper-1',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [480, 300],
+            [300, 300],
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          accountId: '10900',
+          siteId: '361048',
+          zoneId: '1965824',
+          position: 'atf',
+          sizes: ['83', '48', '15']
+        }
+      }]
+    }, {
+      code: 'wallpaper-x',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [480, 300],
+            [300, 300],
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          accountId: '10900',
+          siteId: '361048',
+          zoneId: '1965828',
+          position: 'atf',
+          sizes: ['83', '48', '15']
+        }
+      }]
+    }, {
+      code: 'wallpaper-2',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [480, 300],
+            [300, 300],
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          accountId: '10900',
+          siteId: '361464',
+          zoneId: '1965830',
+          position: 'atf',
+          sizes: ['83', '48', '15']
+        }
+      }]
+    }, {
+      code: 'wallpaper-3',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [480, 300],
+            [300, 300],
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          accountId: '10900',
+          siteId: '361522',
+          zoneId: '1965838',
+          position: 'atf',
+          sizes: ['83', '48', '15']
+        }
+      }]
+    }, {
+      code: 'wallpaper-4',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [480, 300],
+            [300, 300],
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          accountId: '10900',
+          siteId: '361522',
+          zoneId: '1965842',
+          position: 'atf',
+          sizes: ['83', '48', '15']
+        }
+      }]
+    }, {
+      code: 'halfpage-1',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [160, 600],
+            [300, 300],
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          accountId: '10900',
+          siteId: '361048',
+          zoneId: '1958638',
+          position: 'atf',
+          sizes: ['10', '9', '48', '15']
+        }
+      }]
+    }, {
+      code: 'halfpage-2',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [160, 600],
+            [300, 300],
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          accountId: '10900',
+          siteId: '361048',
+          zoneId: '1958642',
+          position: 'atf',
+          sizes: ['10', '9', '48', '15']
+        }
+      }]
+    }]
   }
 }
