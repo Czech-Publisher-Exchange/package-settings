634914bf6c8a883c28f2dc664430b6e44160a07c
CPEx Dev <dev@cpex.cz>
2022-03-07 15:28:09 +0100

    Frantisek: hb settings

--- production/eco/centrum.cz.js
+++ production/eco/centrum.cz.js
@@ -9,5 +9,102 @@ window.cpexWebsiteSettings = {
     settings: {
       publisher: 'eco'
     }
+  },
+  headerbidding: {
+    adUnits: [{
+      code: 'leader',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [970, 250],
+            [970, 210],
+            [970, 100],
+            [728, 90]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          accountId: '10900',
+          siteId: '385178',
+          zoneId: '2142926',
+          position: 'atf',
+          sizes: ['57', '96', '95', '2']
+        }
+      }, {
+        bidder: 'rubicon',
+        params: {
+          accountId: '10900',
+          siteId: '385180',
+          zoneId: '2142928',
+          position: 'atf',
+          sizes: ['57', '96', '95', '2']
+        }
+      }]
+    }, {
+      code: 'halfpage',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [160, 600],
+            [120, 600],
+            [300, 300],
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          accountId: '10900',
+          siteId: '385178',
+          zoneId: '2142942',
+          position: 'atf',
+          sizes: ['10', '9', '8', '48', '15']
+        }
+      }, {
+        bidder: 'rubicon',
+        params: {
+          accountId: '10900',
+          siteId: '385180',
+          zoneId: '2142944',
+          position: 'atf',
+          sizes: ['10', '9', '8', '48', '15']
+        }
+      }]
+    }, {
+      code: 'mediumrectangle',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 300],
+            [300, 250],
+            [250, 250],
+            [300, 50]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          accountId: '10900',
+          siteId: '385178',
+          zoneId: '2142934',
+          position: 'atf',
+          sizes: ['48', '15', '14', '44']
+        }
+      }, {
+        bidder: 'rubicon',
+        params: {
+          accountId: '10900',
+          siteId: '385180',
+          zoneId: '2142936',
+          position: 'atf',
+          sizes: ['48', '15', '14', '44']
+        }
+      }]
+    }]
   }
 }
