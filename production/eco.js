6b6c1effa47b0472345db3710c0bb95ed460533f
CPEx Dev <dev@cpex.cz>
2022-03-22 17:04:05 +0100

    David: Custom bidderTable

--- production/eco.js
+++ production/eco.js
@@ -19,6 +19,21 @@ window.cpexPublisherSettings = {
       index: 1228,
       magnite: 1503,
       xandr: 2919
+    },
+    bidderTable: {
+      stroeerCore: 'stroeer',
+      appnexus: 'xandr_hb',
+      pubmatic: 'pubmatic_hb',
+      pubmatic_eco_hb: 'pubmatic_eco_hb',
+      rubicon: 'magnite_hb',
+      adform: 'adform',
+      adform_im_hb: 'adform_im_hb',
+      r2b2: 'r2b2',
+      triplelift: 'triplelift_hb',
+      ix: 'index_hb',
+      smart: 'smart',
+      teads: 'teads',
+      rtbhouse: 'rtbhouse'
     }
   }
 }
