3a754b628b948025970662cb37d03b3478b4c6cf
CPEx Dev <dev@cpex.cz>
2022-03-28 16:33:25 +0200

    David: Reconstructed SAS settings for kafe.cz

---
+++ production/vlm/kafe.cz.js
@@ -0,0 +1,614 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'kafe.cz'
+  },
+  aam: {
+    settings: {
+      publisher: 'vlm'
+    }
+  },
+  adserver: {
+    adapter: 'sastracker',
+    allowedSSPs: {
+      pubmatic: 1,
+      index: 3,
+      magnite: 4,
+      xandr: 5
+    }
+  },
+  headerbidding: {
+    adUnits: [{
+      code: 'leaderboard-top',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [728, 90],
+            [970, 90],
+            [970, 250],
+            [2000, 1400]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 26460,
+          zoneId: 549816,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [2, 55, 57, 230]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 25692,
+          zoneId: 549784,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [2, 55, 57, 230]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: 3245623
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 569568,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 15624111
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'VLM_Kafe_Leaderboard-top'
+        }
+      }]
+    }, {
+      code: 'skyscraper-2',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [480, 300],
+            [300, 600],
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 26460,
+          zoneId: 389840,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83, 10, 15]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 25692,
+          zoneId: 389892,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83, 10, 15]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: 3245624
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 569576,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 15624112
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'VLM_Kafe_Skyscraper-2'
+        }
+      }]
+    }, {
+      code: 'm-leaderboard-top',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [320, 100],
+            [300, 100]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 46270,
+          zoneId: 651274,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [117, 19]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 46272,
+          zoneId: 643722,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [117, 19]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: 3245627
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 569572,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 15624113
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'VLM_Kafe_Leaderboard-top_Mobile'
+        }
+      }]
+    }, {
+      code: 'm-square-1',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [300, 300],
+            [480, 820]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 46270,
+          zoneId: 857042,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 48, 256]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 46272,
+          zoneId: 857124,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 48, 256]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: 3245628
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 581832,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 15624119
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'VLM_Kafe_Square-1_Mobile'
+        }
+      }]
+    }, {
+      code: 'leaderboard-bottom',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [728, 90],
+            [970, 90],
+            [970, 250],
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 26460,
+          zoneId: 1008786,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [2, 55, 57, 15]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 25692,
+          zoneId: 1008788,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [2, 55, 57, 15]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: 3245632
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 569570,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 15624114
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'VLM_Kafe_Leaderboard-bottom'
+        }
+      }]
+    }, {
+      code: 'skyscraper-1',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [300, 250],
+            [160, 600],
+            [120, 600]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 26460,
+          zoneId: 884754,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 15, 9, 8]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 25692,
+          zoneId: 104782,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 15, 9, 8]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: 3245635
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 569569,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 15624115
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'VLM_Kafe_Skyscraper-1'
+        }
+      }]
+    }, {
+      code: 'm-leaderboard-bottom',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [300, 100],
+            [300, 300]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 46270,
+          zoneId: 945064,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 19, 48]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 46272,
+          zoneId: 945060,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 19, 48]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: 3245638
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 581830,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 15624116
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'VLM_Kafe_Leaderboard-bottom_Mobile'
+        }
+      }]
+    }, {
+      code: 'commercial-article-native-detail',
+      mediaTypes: {
+        native: {}
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 144588,
+          zoneId: 1370784,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [90]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 144590,
+          zoneId: 1389436,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [90]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'VLM_Kafe_commercial-article-native-detail'
+        }
+      }]
+    }, {
+      code: 'commercial-article-2-ga-hp',
+      mediaTypes: {
+        native: {}
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 144588,
+          zoneId: 1370784,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [90]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 144590,
+          zoneId: 1389436,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [90]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'VLM_Kafe_commercial-article-native-detail'
+        }
+      }]
+    }, {
+      code: 'commercial-article-2-hp',
+      mediaTypes: {
+        native: {}
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 144588,
+          zoneId: 1370784,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [90]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 144590,
+          zoneId: 1389436,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [90]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'VLM_Kafe_commercial-article-native-detail'
+        }
+      }]
+    }, {
+      code: 'm-square-2',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 300],
+            [300, 250],
+            [336, 280]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 46270,
+          zoneId: 1036438,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [48, 15, 16]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 46272,
+          zoneId: 1036440,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [48, 15, 16]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: 4055811
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 15783980
+        }
+      }]
+    }, {
+      code: 'm-commercial-article-native-detail',
+      mediaTypes: {
+        native: {}
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 144588,
+          zoneId: 1370784,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [90]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 144590,
+          zoneId: 1389436,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [90]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'VLM_Kafe_commercial-article-native-detail'
+        }
+      }]
+    }, {
+      code: 'square-1',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [300, 300]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 26460,
+          zoneId: 2183454,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 48]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 25692,
+          zoneId: 2183474,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 48]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: 4080492
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 15783978
+        }
+      }]
+    }]
+  },
+  formats: {
+    skin: {
+      enabled: true,
+      contentEl: (document.getElementsByClassName('page')[0])
+    },
+    native: {
+      enabled: true,
+      templates: {
+        generic: "<article class='box-article'>\n  <header class='box-article-header'>\n    <div class='box-article-image'>\n      <a href='${link}'><div class='box-article-img'><img src='${img}'></div></a>\n    </div>\n    <div class='box-article-header-bottom'>\n      <div class='box-article-title'>\n        <a href='${link}'><h2>${title}</h2></a>\n      </div>\n    </div>\n  </header>\n  <blockquote class='box-article-excerpt'>${desc}</blockquote>\n</article>"
+      }
+    },
+    vignette: {
+      enabled: true,
+      adUnit: 'm-vignette'
+    }
+  }
+}
