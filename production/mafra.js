f252436661450d7e640abc0dba27f914f443f36e
CPEx Dev <dev@cpex.cz>
2022-04-08 14:48:53 +0200

    David: Nastaven adserver

--- production/mafra.js
+++ production/mafra.js
@@ -1,6 +1,29 @@
 window.cpexPublisherSettings = {
   publisher: {
+    name: 'MAFRA, a.s.',
+    sellerId: '003',
     code: 'mafra'
+  },
+  cmp: {
+    enabled: false
+  },
+  adserver: {
+    enabled: true,
+    adapter: 'paticka',
+    bidderTable: {
+      appnexus: 'xandr_hb',
+      pubmatic: 'pubmatic_hb',
+      rubicon: 'magnite_hb',
+      adform: 'adform_hb',
+      r2b2: 'r2b2_hb',
+      ix: 'index_hb'
+    },
+    allowedSSPs: {
+      xandr: '10001',
+      magnite: '10000',
+      pubmatic: '10002',
+      index: '10003'
+    }
   },
   aam: {
     settings: {
