1d591aef9c8be70cd89ce4bf0345b3b519902c35
CPEx Dev <dev@cpex.cz>
2022-04-11 13:40:07 +0200

    Frantisek: hb - skin test

---
+++ stage/eco/aktualne.cz.js
@@ -0,0 +1,45 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'aktualne.cz'
+  },
+  aam: {
+    settings: {
+      publisher: 'eco'
+    }
+  },
+  headerbidding: {
+    adUnits: [{
+      code: 'leader',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [728, 90],
+            [970, 100],
+            [970, 210],
+            [2000, 1400]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 353314,
+          zoneId: 2186666,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [2, 95, 96, 230]
+        }
+      }]
+    }],
+    currency: 'USD'
+  },
+  formats: {
+    skin: {
+      enabled: true,
+      contentEl: (document.getElementsByClassName('page')[0])
+    },
+    interscroller: {
+      enabled: true
+    }
+  }
+}
