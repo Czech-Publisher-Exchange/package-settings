61bcdf1aec9abbf354570851dcfa5b82347537c1
CPEx Dev <dev@cpex.cz>
2022-04-12 14:36:18 +0200

    silvie: publisher settings impuls

---
+++ production/impuls.js
@@ -0,0 +1,17 @@
+window.cpexPublisherSettings = {
+  publisher: {
+    code: 'impuls',
+    sellerId: '18'
+  },
+  cmp: {
+    enabled: false
+  },
+  aam: {
+    settings: {
+      publisher: 'impuls'
+    }
+  },
+  adserver: {
+    adapter: 'googletag'
+  }
+}
