4dbf2ac78686695de0f3f5d2e5e80a1195adeab9
CPEx Dev <dev@cpex.cz>
2022-04-16 11:42:36 +0200

    frantisek: test bez adunits

--- stage/eco/aktualne.cz.js
+++ stage/eco/aktualne.cz.js
@@ -8,29 +8,6 @@ window.cpexWebsiteSettings = {
     }
   },
   headerbidding: {
-    adUnits: [{
-      code: 'leader',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [728, 90],
-            [970, 100],
-            [970, 210],
-            [2000, 1400]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'rubicon',
-        params: {
-          siteId: 353314,
-          zoneId: 2186666,
-          accountId: '10900',
-          position: 'atf',
-          sizes: [2, 95, 96, 230]
-        }
-      }]
-    }],
     currency: 'USD'
   },
   formats: {
