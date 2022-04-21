27ea5afe428603899fa4f85e9194baee13f8021c
CPEx Dev <dev@cpex.cz>
2022-04-21 14:58:41 +0200

    frantisek: hn hb

--- production/eco/hn.cz.js
+++ production/eco/hn.cz.js
@@ -1,13 +1,234 @@
 window.cpexWebsiteSettings = {
-  website: {
-    name: 'hn.cz'
-  },
-  cmp: {
-    enabled: false
-  },
   aam: {
     settings: {
       publisher: 'eco'
     }
+  },
+  headerbidding: {
+    adUnits: [{
+      code: 'leader',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [970, 250],
+            [970, 210],
+            [970, 100],
+            [728, 90]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 401314,
+          zoneId: 2250698,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [57, 96, 95, 2]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 401308,
+          zoneId: 2250696,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [57, 96, 95, 2]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 23566747
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'ihned.cz',
+          p: '728x90',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'halfpage-1',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [160, 600],
+            [300, 300],
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 401314,
+          zoneId: 2250714,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 9, 48, 15]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 401308,
+          zoneId: 2250712,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 9, 48, 15]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 23566750
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'ihned.cz',
+          p: '300x600_1',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'halfpage-2',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [160, 600],
+            [300, 300],
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 401314,
+          zoneId: 2250718,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 9, 48, 15]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 401308,
+          zoneId: 2250716,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 9, 48, 15]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 23566751
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'ihned.cz',
+          p: '300x600_2',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'wallpaper',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [480, 300],
+            [300, 300],
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 401314,
+          zoneId: 2250710,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83, 48, 15]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 401308,
+          zoneId: 2250708,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83, 48, 15]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 23566749
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'ihned.cz',
+          p: '480x300',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'megaboard',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [970, 310],
+            [970, 250],
+            [970, 210],
+            [728, 90]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 401314,
+          zoneId: 2250706,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [94, 57, 96, 2]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 401308,
+          zoneId: 2250704,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [94, 57, 96, 2]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 23566748
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'ihned.cz',
+          p: '990x300',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }]
   }
 }
