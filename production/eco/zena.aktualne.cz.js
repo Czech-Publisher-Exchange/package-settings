428cb4466267c0e910f2bb342cd99ee0f360ae40
CPEx Dev <dev@cpex.cz>
2022-04-21 15:20:51 +0200

    frantisek: hb settings

--- production/eco/zena.aktualne.cz.js
+++ production/eco/zena.aktualne.cz.js
@@ -99,6 +99,323 @@ window.cpexWebsiteSettings = {
           g: 'hb'
         }
       }]
+    }, {
+      code: 'halfpage-2',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [300, 250],
+            [300, 300],
+            [160, 600],
+            [120, 600]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 379392,
+          zoneId: 2100110,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 15, 48, 9, 8]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 379386,
+          zoneId: 2100086,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 15, 48, 9, 8]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 21873795
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'zena.cz',
+          p: 'x',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'halfpage-3',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [300, 250],
+            [300, 300],
+            [160, 600],
+            [120, 600]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 379392,
+          zoneId: 2100112,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 15, 48, 9, 8]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 379386,
+          zoneId: 2100088,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 15, 48, 9, 8]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 21873809
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'zena.cz',
+          p: 'x',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'halfpage-4',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [300, 250],
+            [300, 300],
+            [160, 600],
+            [120, 600]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 379392,
+          zoneId: 2100114,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 15, 48, 9, 8]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 379386,
+          zoneId: 2100090,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 15, 48, 9, 8]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 21873811
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'zena.cz',
+          p: 'x',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'leader',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [970, 250],
+            [970, 210],
+            [970, 100],
+            [728, 90]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 379392,
+          zoneId: 2100100,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [57, 96, 95, 2]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 379386,
+          zoneId: 2100076,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [57, 96, 95, 2]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 21873712
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'zena.cz',
+          p: '970x210_2',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'megaboard',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [970, 310],
+            [970, 250],
+            [970, 210],
+            [970, 100],
+            [728, 90]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 379392,
+          zoneId: 2100102,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [94, 57, 96, 95, 2]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 379386,
+          zoneId: 2100078,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [94, 57, 96, 95, 2]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 21873767
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'zena.cz',
+          p: '970x310_2',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'wallpaper-1',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [480, 300],
+            [300, 300],
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 379392,
+          zoneId: 2100104,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83, 48, 15]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 379386,
+          zoneId: 2100080,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83, 48, 15]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 21873768
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'zena.cz',
+          p: '480x300',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'wallpaper-2',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [480, 300],
+            [300, 300],
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 379392,
+          zoneId: 2100106,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83, 48, 15]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 379386,
+          zoneId: 2100082,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83, 48, 15]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 21873769
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'zena.cz',
+          p: 'x',
+          m: 0,
+          g: 'hb'
+        }
+      }]
     }]
   }
 }
