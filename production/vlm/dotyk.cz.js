2bddfe2a70cb315cdd4c81ab2e34a90429e29624
CPEx Dev <dev@cpex.cz>
2022-04-25 12:54:06 +0200

    Frantisek: vymaz allowed ssp, jsou na urovni publishera

--- production/vlm/dotyk.cz.js
+++ production/vlm/dotyk.cz.js
@@ -446,12 +446,6 @@ window.cpexWebsiteSettings = {
     }
   },
   adserver: {
-    adapter: 'sastracker',
-    allowedSSPs: {
-      pubmatic: 1,
-      index: 3,
-      magnite: 4,
-      xandr: 5
-    }
+    adapter: 'sastracker'
   }
 }
