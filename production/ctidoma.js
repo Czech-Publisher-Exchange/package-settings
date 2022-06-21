1ccbb1a429c8836436e020faaf007c9c632511e9
CPEx Dev <dev@cpex.cz>
2022-06-21 13:48:31 +0200

    Silvie: publisher settings

---
+++ production/ctidoma.js
@@ -0,0 +1,17 @@
+window.cpexPublisherSettings = {
+  publisher: {
+    code: 'ctidoma',
+    sellerId: '026'
+  },
+  cmp: {
+    enabled: false
+  },
+  aam: {
+    settings: {
+      publisher: 'ctidoma'
+    }
+  },
+  adserver: {
+    adapter: 'googletag'
+  }
+}
