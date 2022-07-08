78481c72819f25852c3738602998a268c21cb77a
CPEx Dev <dev@cpex.cz>
2022-07-08 10:57:30 +0200

    Silvie: cnews.cz

---
+++ production/iinfo/cnews.cz.js
@@ -0,0 +1,299 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'cnews.cz'
+  },
+  aam: {
+    settings: {
+      publisher: 'iinfo'
+    }
+  },
+  adserver: {
+    adapter: 'sastracker'
+  },
+  headerbidding: {
+    adUnits: [{
+      code: 'iinfo-area-halfpagead-horni',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [160, 600],
+            [120, 600],
+            [300, 250],
+            [300, 300]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 265202,
+          zoneId: 2317568,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 9, 8, 15, 48]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 265182,
+          zoneId: 2317590,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 9, 8, 15, 48]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 24001672
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'ii.cnews.cz',
+          p: '300x600_1',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'iinfo-area-halfpagead-spodni',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [160, 600],
+            [120, 600],
+            [300, 250],
+            [300, 300]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 265202,
+          zoneId: 2317570,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 9, 8, 15, 48]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 265182,
+          zoneId: 2317592,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 9, 8, 15, 48]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 24001674
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'ii.cnews.cz',
+          p: '300x600_2',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'iinfo-area-leaderboard',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [728, 90],
+            [970, 210],
+            [998, 200],
+            [2000, 1400]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 265202,
+          zoneId: 2317564,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [2, 96, 259, 230]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 265182,
+          zoneId: 2317586,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [2, 96, 259, 230]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 24001670
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'ii.cnews.cz',
+          p: 'branding',
+          m: 0,
+          g: 'hb'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'ii.cnews.cz',
+          p: '998x200',
+          m: 0,
+          g: 'hb'
+        }
+      }],
+      filter: {
+        minWidth: 1000
+      }
+    }, {
+      code: 'iinfo-area-big-rectangle',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [970, 310],
+            [970, 210],
+            [970, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 265202,
+          zoneId: 2317566,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [94, 96, 57]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 265182,
+          zoneId: 2317588,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [94, 96, 57]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 24001671
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'ii.cnews.cz',
+          p: '970x310',
+          m: 0,
+          g: 'hb'
+        }
+      }],
+      filter: {
+        minWidth: 1000
+      }
+    }, {
+      code: 'iinfo-area-rectangle',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [480, 300],
+            [300, 250],
+            [300, 300]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 265202,
+          zoneId: 2317574,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83, 15, 48]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 265182,
+          zoneId: 2317596,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83, 15, 48]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 24353125
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'ii.cnews.cz',
+          p: '480x300',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'iinfo-area-spodni-square-banner',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 300],
+            [300, 250],
+            [250, 250],
+            [200, 200]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 265202,
+          zoneId: 2317572,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [48, 15, 14, 13]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 265182,
+          zoneId: 2317594,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [48, 15, 14, 13]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 24353121
+        }
+      }]
+    }]
+  },
+  formats: {
+    skin: {
+      enabled: true,
+      contentEl: (document.getElementsByClassName('parter-cpex-branding-wrapper')[0]),
+      contentCSS: 'width: 1060px; margin: 200px auto;'
+    }
+  }
+}
