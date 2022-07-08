0d96cb6e13344cff16dd9cb3a44ef297f62ec40f
CPEx Dev <dev@cpex.cz>
2022-07-08 10:48:58 +0200

    Silvie: rodicov.cz - základní nastavení

---
+++ production/iinfo/rodicov.cz.js
@@ -0,0 +1,295 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'rodicov.cz'
+  },
+  aam: {
+    settings: {
+      publisher: 'iinfo'
+    }
+  },
+  adserver: {
+    adapter: 'sastracker'
+  },
+  headerbidding: {
+    adUnits: [{
+      code: 'iinfo-area-halfpagead-horni',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [160, 600],
+            [120, 600],
+            [300, 250],
+            [300, 300]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 265202,
+          zoneId: 2317580,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 9, 8, 15, 48]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 265182,
+          zoneId: 2317602,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 9, 8, 15, 48]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4264901'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 24001683
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'ii.rodicov.cz',
+          p: '300x600_1',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'iinfo-area-halfpagead-spodni',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [160, 600],
+            [120, 600],
+            [300, 250],
+            [300, 300]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 265202,
+          zoneId: 2317582,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 9, 8, 15, 48]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 265182,
+          zoneId: 2317604,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 9, 8, 15, 48]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4264902'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 24001684
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'ii.rodicov.cz',
+          p: '300x600_2',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'iinfo-area-leaderboard',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [728, 90],
+            [970, 210],
+            [998, 200],
+            [2000, 1400]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 265202,
+          zoneId: 2317576,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [2, 96, 259, 230]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 265182,
+          zoneId: 2317598,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [2, 96, 259, 230]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4264899'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 24001676
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'ii.rodicov.cz',
+          p: 'branding',
+          m: 0,
+          g: 'hb'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'ii.rodicov.cz',
+          p: '998x200',
+          m: 0,
+          g: 'hb'
+        }
+      }],
+      filter: {
+        minWidth: 1000
+      }
+    }, {
+      code: 'iinfo-area-big-rectangle',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [970, 310],
+            [970, 210],
+            [970, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 265202,
+          zoneId: 2317578,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [94, 96, 57]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 265182,
+          zoneId: 2317600,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [94, 96, 57]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4264900'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 24001677
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'ii.rodicov.cz',
+          p: '970x310',
+          m: 0,
+          g: 'hb'
+        }
+      }],
+      filter: {
+        minWidth: 1000
+      }
+    }, {
+      code: 'iinfo-area-spodni-square-banner',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 300],
+            [300, 250],
+            [250, 250],
+            [200, 200]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 265202,
+          zoneId: 2317584,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [48, 15, 14, 13]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 265182,
+          zoneId: 2317606,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [48, 15, 14, 13]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4305025'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 24353382
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'ii.rodicov.cz',
+          p: '480x300',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }]
+  },
+  formats: {
+    skin: {
+      enabled: true,
+      contentEl: (document.getElementsByClassName('parter-cpex-branding-wrapper')[0]),
+      contentCSS: 'width: 1060px; margin: 200px auto;',
+      offset: null,
+      offsetScroll: false
+    }
+  }
+}
