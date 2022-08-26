fa98905d8c5e2113036d90f948a6f120040cf8e4
CPEx Dev <dev@cpex.cz>
2022-08-26 15:13:41 +0200

    Frantisek: zatim stopuju vignette v hb

--- production/vlm/denik.cz.js
+++ production/vlm/denik.cz.js
@@ -1272,30 +1272,6 @@ window.cpexWebsiteSettings = {
           inventoryCode: 'VLM_denikcz_m_square_gallery_9'
         }
       }]
-    }, {
-      code: 'centerVignette',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [480, 480],
-            [300, 300],
-            [300, 250]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'rubicon',
-        params: {
-          siteId: 46270,
-          zoneId: 2522988,
-          accountId: '10900',
-          position: 'atf',
-          sizes: [48, 15]
-        }
-      }],
-      filter: {
-        maxWidth: 1000
-      }
     }]
   },
   formats: {
