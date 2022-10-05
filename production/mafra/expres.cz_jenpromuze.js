4ddbe39a2ffd9398f5f8c2bf85b9888e707f64ca
CPEx Dev <dev@cpex.cz>
2022-10-05 08:50:10 +0200

    Martina: nasadenie jenpromuze

---
+++ production/mafra/expres.cz_jenpromuze.js
@@ -0,0 +1,263 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'expres.cz_jenpromuze'
+  },
+  aam: {
+    settings: {
+      publisher: 'mafra'
+    }
+  },
+  headerbidding: {
+    adUnits: [{
+      code: 'branding',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [998, 200],
+            [728, 90],
+            [970, 210],
+            [970, 90]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4641259'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 27157266
+        }
+      }]
+    }, {
+      code: 'halfpagead',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [300, 300],
+            [300, 250],
+            [160, 600],
+            [250, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4641260'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 27157269
+        }
+      }]
+    }, {
+      code: 'halfpagead_spodni',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [300, 300],
+            [300, 250],
+            [160, 600],
+            [250, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4641264'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 27157280
+        }
+      }]
+    }, {
+      code: 'halfpagead_rubrika',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [300, 300],
+            [300, 250],
+            [160, 600],
+            [250, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4641262'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 27157271
+        }
+      }]
+    }, {
+      code: 'halfpagead_rubrika_spodni',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [300, 300],
+            [300, 250],
+            [160, 600],
+            [250, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4641263'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 27157272
+        }
+      }]
+    }, {
+      code: 'halfpagead_fotogalerie',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [300, 300],
+            [300, 250],
+            [160, 600],
+            [250, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4641261'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 27157270
+        }
+      }]
+    }, {
+      code: 'm_300x250',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [250, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4641265'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 27157281
+        }
+      }]
+    }, {
+      code: 'm_branding',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [250, 250],
+            [480, 820],
+            [480, 480],
+            [300, 600]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4641266'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 27157283
+        }
+      }]
+    }, {
+      code: 'm_paticka',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4641267'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 27157285
+        }
+      }]
+    }, {
+      code: 'widesquare',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [480, 300],
+            [300, 300],
+            [250, 250],
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4641268'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 27157286
+        }
+      }]
+    }]
+  },
+  formats: {
+    interscroller: {
+      enabled: true
+    },
+    slideup: {
+      enabled: true,
+      adUnit: 'm_sticky'
+    }
+  }
+}
