cf91b8a547f4a76e63d64cbd3147b177b6784c64
CPEx Dev <dev@cpex.cz>
2022-10-05 09:06:38 +0200

    Martina: nasadenie onlajny

---
+++ production/mafra/onlajny.com.js
@@ -0,0 +1,68 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'onlajny.com'
+  },
+  aam: {
+    settings: {
+      publisher: 'mafra'
+    }
+  },
+  headerbidding: {
+    adUnits: [{
+      code: '750x100',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [750, 100],
+            [728, 90]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 144582,
+          zoneId: 1228786,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [39, 2]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 144584,
+          zoneId: 1228788,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [39, 2]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4641434'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 878601
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 27158083
+        }
+      }]
+    }]
+  },
+  formats: {
+    interscroller: {
+      enabled: true
+    },
+    slideup: {
+      enabled: true,
+      adUnit: 'm_sticky'
+    }
+  }
+}
