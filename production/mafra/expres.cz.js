4bf4a123e693739c97372805a075fd0973163de5
CPEx Dev <dev@cpex.cz>
2022-10-20 15:36:58 +0200

    Jana: zapojení nové pozice

--- production/mafra/expres.cz.js
+++ production/mafra/expres.cz.js
@@ -748,6 +748,44 @@ window.cpexWebsiteSettings = {
           adSlot: '4641199'
         }
       }]
+    }, {
+      code: 'halfpagead_lazyload',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [300, 300],
+            [300, 250],
+            [160, 600]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 144582,
+          zoneId: 2607338,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 48, 15, 9]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 144584,
+          zoneId: 2607342,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 48, 15, 9]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4740504'
+        }
+      }]
     }]
   },
   formats: {
