9c0e95ad2bc923167d9c70459e19e8f5a510f372
CPEx Dev <dev@cpex.cz>
2022-10-20 15:57:45 +0200

    Jana: zapojení nové pozice

--- production/mafra/lidovky.cz.js
+++ production/mafra/lidovky.cz.js
@@ -1350,6 +1350,49 @@ window.cpexWebsiteSettings = {
           placementId: 27157584
         }
       }]
+    }, {
+      code: 'halfpagead_lazyload',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [300, 300],
+            [300, 250],
+            [160, 600]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 144582,
+          zoneId: 2607340,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 48, 15, 9]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 144584,
+          zoneId: 2607344,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 48, 15, 9]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4740502'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 27707883
+        }
+      }]
     }]
   },
   formats: {
