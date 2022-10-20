2086c797f0b775cf46975ae5b38f07fbe0e9ba2f
CPEx Dev <dev@cpex.cz>
2022-10-20 16:18:10 +0200

    Jana: zapojení nové pozice

--- production/mafra/idnes.cz_hobby.js
+++ production/mafra/idnes.cz_hobby.js
@@ -1431,6 +1431,54 @@ window.cpexWebsiteSettings = {
       filter: {
         maxWidth: 1000
       }
+    }, {
+      code: 'halfpagead_lazyload',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [300, 300],
+            [300, 250],
+            [160, 600]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 144582,
+          zoneId: 2574508,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 48, 15, 9]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 144584,
+          zoneId: 2574572,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 48, 15, 9]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4746279'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 889444
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 27738528
+        }
+      }]
     }]
   },
   formats: {
