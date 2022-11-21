79cd5aa9d4fcdc8995a3f7092ef3becd04b9061e
CPEx Dev <dev@cpex.cz>
2022-11-21 13:49:37 +0100

    Jana: zapojení pozice m_branding_foto2 na bonusweb

--- production/mafra/idnes.cz_hry.js
+++ production/mafra/idnes.cz_hry.js
@@ -1790,6 +1790,57 @@ window.cpexWebsiteSettings = {
           g: 'hb'
         }
       }]
+    }, {
+      code: 'm_branding_foto2',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [250, 250],
+            [480, 820],
+            [480, 480]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 157330,
+          zoneId: 2653044,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 14, 256, 261]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 157332,
+          zoneId: 2653088,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 14, 256, 261]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4800011'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 919002
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 28185829
+        }
+      }],
+      filter: {
+        maxWidth: 1000
+      }
     }],
     analytics: ['cpexAnalytics', 'id5Analytics']
   }
