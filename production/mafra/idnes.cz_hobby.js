b26cbf29e19e579ddc0bacc6af674624a7a8c676
CPEx Dev <dev@cpex.cz>
2022-11-25 14:39:55 +0100

    Jana: zapojení widesquare_clankovy2 na hobby.idnes.cz

--- production/mafra/idnes.cz_hobby.js
+++ production/mafra/idnes.cz_hobby.js
@@ -1738,6 +1738,47 @@ window.cpexWebsiteSettings = {
       filter: {
         maxWidth: 1000
       }
+    }, {
+      code: 'widesquare_clankovy2',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [480, 300],
+            [300, 300],
+            [250, 250],
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 144582,
+          zoneId: 2672418,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83, 48, 14, 15]
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4830498'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 28291627
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'hobby.idnes.cz',
+          p: '480x300_clankovy_2',
+          m: 0,
+          g: 'hb'
+        }
+      }]
     }]
   },
   formats: {
