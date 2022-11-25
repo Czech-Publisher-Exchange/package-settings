3632b7b7a6dbb3c707dc019b31bc13c86d625747
CPEx Dev <dev@cpex.cz>
2022-11-25 14:22:53 +0100

    Jana: zapojení widesquare_clankovy2 na auto.idnes.cz

--- production/mafra/idnes.cz_auto.js
+++ production/mafra/idnes.cz_auto.js
@@ -1689,6 +1689,47 @@ window.cpexWebsiteSettings = {
       filter: {
         maxWidth: 1000
       }
+    }, {
+      code: 'widesquare_clankovy2',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [480, 300],
+            [300, 300],
+            [250, 250],
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 144582,
+          zoneId: 2672386,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83, 48, 14, 15]
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4830482'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 28291622
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'auto.idnes.cz',
+          p: '480x300_clankovy_2',
+          m: 0,
+          g: 'hb'
+        }
+      }]
     }]
   },
   formats: {
