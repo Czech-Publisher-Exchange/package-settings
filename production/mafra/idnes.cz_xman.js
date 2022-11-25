ece56fa27e0a672120f21031e64acd34249e9fdd
CPEx Dev <dev@cpex.cz>
2022-11-25 15:07:08 +0100

    Jana: zapojení widesquare_clankovy2 na xman.idnes.cz

--- production/mafra/idnes.cz_xman.js
+++ production/mafra/idnes.cz_xman.js
@@ -1745,6 +1745,47 @@ window.cpexWebsiteSettings = {
       filter: {
         maxWidth: 1000
       }
+    }, {
+      code: 'widesquare_clankovy2',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [480, 300],
+            [300, 300],
+            [250, 250],
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 144582,
+          zoneId: 2672394,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83, 48, 14, 15]
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4830486'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 28291636
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'xman.idnes.cz',
+          p: '480x300_clankovy_2',
+          m: 0,
+          g: 'hb'
+        }
+      }]
     }]
   },
   formats: {
