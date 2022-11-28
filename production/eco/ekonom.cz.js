efd807bbcada3584f2168f4308d4bdbf32f951cd
CPEx Dev <dev@cpex.cz>
2022-11-28 11:03:33 +0100

    František: ekonom.cz

---
+++ production/eco/ekonom.cz.js
@@ -0,0 +1,26 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'ekonom.cz'
+  },
+  aam: {
+    settings: {
+      publisher: 'eco'
+    }
+  },
+  headerbidding: {
+    currency: 'USD',
+    enabled: false
+  },
+  formats: {
+    skin: {
+      enabled: false,
+      offsetScroll: false
+    },
+    interscroller: {
+      enabled: false
+    },
+    slideup: {
+      enabled: false
+    }
+  }
+}
