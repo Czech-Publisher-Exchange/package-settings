ca6877afd2eed80fa6ebedaf7485b353a3c0b1fe
CPEx Dev <dev@cpex.cz>
2022-12-05 13:19:27 +0100

    Iveta: vrácení produkčního prebidu

--- production/csfd/csfd.sk.js
+++ production/csfd/csfd.sk.js
@@ -1489,8 +1489,7 @@ window.cpexWebsiteSettings = {
       filter: {
         minWidth: 1000
       }
-    }],
-    prebidPath: 'https://cdn.cpex.cz/stage/hb/prebid/v7/prebid.js'
+    }]
   },
   formats: {
     skin: {
