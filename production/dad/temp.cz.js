692bb5076d3d40ec01ef1f1d05d78bcd0e0ae670
CPEx Dev <dev@cpex.cz>
2022-12-09 16:09:54 +0100

    David: Test duplikace

---
+++ production/dad/temp.cz.js
@@ -0,0 +1,879 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'temp.cz'
+  },
+  aam: {
+    settings: {
+      publisher: 'dad'
+    }
+  },
+  adserver: {
+    adapter: 'googletag'
+  },
+  headerbidding: {
+    adUnits: [{
+      code: 'da_hokej_desktop_top_banner_998x200_2x1',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [728, 90],
+            [970, 90],
+            [970, 210],
+            [2000, 1400]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 278450,
+          zoneId: 2516566,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [2, 55, 96, 230]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 278452,
+          zoneId: 2518136,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [2, 55, 96, 230]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4608482'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 26821622
+        }
+      }]
+    }, {
+      code: 'da_hokej_desktop_skyscraper_velky_300x600_2x3',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 278450,
+          zoneId: 2516568,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 278452,
+          zoneId: 2518138,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4608483'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 26821631
+        }
+      }]
+    }, {
+      code: 'da_hokej_desktop_skycraper_maly_300x250_1x4',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 278450,
+          zoneId: 2516570,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 278452,
+          zoneId: 2518140,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4608484'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 26821632
+        }
+      }]
+    }, {
+      code: 'da_hokej_desktop_paticka_cela_sirka_998x200_1x15',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [970, 250],
+            [970, 210],
+            [970, 90]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 278450,
+          zoneId: 2516572,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [57, 96, 55]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 278452,
+          zoneId: 2518142,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [57, 96, 55]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4608485'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 26821633
+        }
+      }]
+    }, {
+      code: 'da_hokej_desktop_timeout_vpravo_300x300_1x9',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 300],
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 278450,
+          zoneId: 2516574,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [48, 15]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 278452,
+          zoneId: 2518148,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [48, 15]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4608486'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 26821635
+        }
+      }]
+    }, {
+      code: 'da_hokej_desktop_timeout_vpravo_2_300x300_1x21',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 300],
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 278450,
+          zoneId: 2516576,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [48, 15]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 278452,
+          zoneId: 2518150,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [48, 15]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4608487'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 26821637
+        }
+      }]
+    }, {
+      code: 'da_hokej_desktop_paticka_left_468x60_1x13',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [480, 300]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 278450,
+          zoneId: 2516578,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 278452,
+          zoneId: 2518152,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4608488'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 26821640
+        }
+      }]
+    }, {
+      code: 'da_hokej_desktop_paticka_right_468x60_1x14',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [480, 300]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 278450,
+          zoneId: 2516580,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 278452,
+          zoneId: 2518154,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4608489'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 26821642
+        }
+      }]
+    }, {
+      code: 'da_hokej_desktop_top_banner_998x200_1x1',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [970, 90],
+            [970, 210],
+            [728, 90]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 278450,
+          zoneId: 2516566,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [55, 96, 2]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 278452,
+          zoneId: 2518136,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [55, 96, 2]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4608482'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 26821622
+        }
+      }]
+    }, {
+      code: 'da_hokej_desktop_skyscraper_velky_300x600_1x3',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 278450,
+          zoneId: 2516568,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 278452,
+          zoneId: 2518138,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4608483'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 26821631
+        }
+      }]
+    }, {
+      code: 'da_hokej_desktop_paticka_cela_sirka_998x200_1x15',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [970, 210],
+            [970, 90],
+            [998, 200],
+            [970, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 278450,
+          zoneId: 2516572,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [96, 55, 259, 57]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 278452,
+          zoneId: 2518142,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [96, 55, 259, 57]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4608485'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 26821633
+        }
+      }]
+    }, {
+      code: 'da_hokej_desktop_velky_pod_obsahem_750x100_1x5',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [480, 300]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 278450,
+          zoneId: 2516578,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 278452,
+          zoneId: 2518152,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4608488'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 26821640
+        }
+      }]
+    }, {
+      code: 'da_hokej_desktop_skycraper_maly_300x250_1x4',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 278450,
+          zoneId: 2516570,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 278452,
+          zoneId: 2518140,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4608484'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 26821632
+        }
+      }]
+    }, {
+      code: 'da_hokej_mobile_top_banner_300x50_1x17',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [320, 100],
+            [320, 50]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 280842,
+          zoneId: 2516582,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [117, 43]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 280840,
+          zoneId: 2518156,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [117, 43]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4608490'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 26821643
+        }
+      }]
+    }, {
+      code: 'da_hokej_mobile_pod_scoreboardem_300x250_1x18',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [300, 300]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 280842,
+          zoneId: 2516584,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 48]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 280840,
+          zoneId: 2518158,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 48]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4608491'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 26821645
+        }
+      }]
+    }, {
+      code: 'da_hokej_mobile_skyscraper_velky_300x250_1x24',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [300, 300]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 280842,
+          zoneId: 2516586,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 48]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 280840,
+          zoneId: 2518160,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 48]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4608492'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 26821648
+        }
+      }]
+    }, {
+      code: 'da_hokej_mobile_skyscraper_maly_300x250_1x25',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [300, 300]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 280842,
+          zoneId: 2518126,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 48]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 280840,
+          zoneId: 2518162,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 48]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158733',
+          adSlot: '4608493'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 26821649
+        }
+      }]
+    }, {
+      code: 'da_hokej_mobile_velky_pod_obsahem_300x250_1x19',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [300, 300]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 280842,
+          zoneId: 2518128,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 48]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 280840,
+          zoneId: 2518164,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 48]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158734',
+          adSlot: '4608494'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 26821652
+        }
+      }]
+    }, {
+      code: 'da_hokej_mobile_paticka_left_300x50_1x26',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [320, 100],
+            [320, 50]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 280842,
+          zoneId: 2518130,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [117, 43]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 280840,
+          zoneId: 2518166,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [117, 43]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158735',
+          adSlot: '4608495'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 26821654
+        }
+      }]
+    }, {
+      code: 'da_hokej_mobile_paticka_right_300x50_1x27',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [320, 100],
+            [320, 50]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 280842,
+          zoneId: 2518132,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [117, 43]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 280840,
+          zoneId: 2518168,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [117, 43]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158736',
+          adSlot: '4608496'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 26821661
+        }
+      }]
+    }, {
+      code: 'da_hokej_mobile_paticka_cela_sirka_300x250',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [300, 300]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 280842,
+          zoneId: 2518134,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 48]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 280840,
+          zoneId: 2518170,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 48]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158737',
+          adSlot: '4608497'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 26821667
+        }
+      }]
+    }]
+  },
+  cmp: {
+    enabled: false
+  }
+}
