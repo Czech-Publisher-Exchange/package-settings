0fde84645428f9e48decec50ee97b7a1d34b79fb
CPEx Dev <dev@cpex.cz>
2022-12-09 09:39:08 +0100

    Aleš: nastaven cmp timeout na 2s

--- production/csfd.js
+++ production/csfd.js
@@ -18,7 +18,8 @@ window.cpexPublisherSettings = {
     enabled: true
   },
   headerbidding: {
-    analytics: ['id5Analytics', 'cpexAnalytics']
+    analytics: ['id5Analytics', 'cpexAnalytics'],
+    cmpLoadTimeoutMs: '2000'
   },
   general: {
     errorApiKey: 'hbp_tCjxz6JmkE7RI1QnA7qLBjDFb4ItP23y9OCs'
