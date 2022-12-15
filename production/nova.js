cf248432957e3c4145d795f625875084495af980
CPEx Dev <dev@cpex.cz>
2022-12-15 09:46:49 +0100

    Aleš: Vytvoření nového publishera Nova

---
+++ production/nova.js
@@ -0,0 +1,18 @@
+window.cpexPublisherSettings = {
+  publisher: {
+    code: 'nova',
+    name: 'TV Nova s.r.o.',
+    sellerId: '25'
+  },
+  cmp: {
+    enabled: false
+  },
+  aam: {
+    settings: {
+      publisher: 'nova'
+    }
+  },
+  adserver: {
+    adapter: 'googletag'
+  }
+}
