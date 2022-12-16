34ea5ef707dd2f0a4e9d310a363f4e7feea4f445
CPEx Dev <dev@cpex.cz>
2022-12-16 15:10:12 +0100

    Jana: zapoj. IX u widesquare_clankovy_2 na na ekonomika.idnes.cz

--- production/mafra/idnes.cz_ekonomika.js
+++ production/mafra/idnes.cz_ekonomika.js
@@ -1773,6 +1773,11 @@ window.cpexWebsiteSettings = {
           adSlot: '4830497'
         }
       }, {
+        bidder: 'ix',
+        params: {
+          siteId: 927757
+        }
+      }, {
         bidder: 'appnexus',
         params: {
           placementId: 28291626
