fd9e40d42cb6204e71c94b247b7a157bd8d6c68a
CPEx Dev <dev@cpex.cz>
2023-01-02 14:51:50 +0100

    Jana: zapoj. sizy 500x200 u m_sticky na akcniceny.cz

--- production/mafra/akcniceny.cz.js
+++ production/mafra/akcniceny.cz.js
@@ -345,7 +345,8 @@ window.cpexWebsiteSettings = {
         banner: {
           sizes: [
             [320, 150],
-            [300, 250]
+            [300, 250],
+            [500, 200]
           ]
         }
       },
@@ -356,7 +357,7 @@ window.cpexWebsiteSettings = {
           zoneId: 1139400,
           accountId: '10900',
           position: 'atf',
-          sizes: [60, 15]
+          sizes: [60, 15, 258]
         }
       }, {
         bidder: 'rubicon-mask',
@@ -365,7 +366,7 @@ window.cpexWebsiteSettings = {
           zoneId: 1139398,
           accountId: '10900',
           position: 'atf',
-          sizes: [60, 15]
+          sizes: [60, 15, 258]
         },
         bidderModuleName: 'rubicon'
       }, {
