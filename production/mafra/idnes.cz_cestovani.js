dcf810168120b8f31897f990066e5d6c7934714b
CPEx Dev <dev@cpex.cz>
2023-01-31 10:14:46 +0100

    Jana: zapoj. R2B2 na m_branding_foto2

--- production/mafra/idnes.cz_cestovani.js
+++ production/mafra/idnes.cz_cestovani.js
@@ -1882,6 +1882,14 @@ window.cpexWebsiteSettings = {
         params: {
           networkId: 11172
         }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'cestovani.idnes.cz',
+          p: 'interscroller_foto_scroll_2',
+          m: 1,
+          g: 'hb'
+        }
       }],
       filter: {
         maxWidth: 1000
