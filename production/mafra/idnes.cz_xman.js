624210dc9bbfb1c170dfcbb957ccf6848fd76a00
CPEx Dev <dev@cpex.cz>
2023-01-31 10:25:08 +0100

    Jana: zapoj. R2B2 na m_branding_foto2

--- production/mafra/idnes.cz_xman.js
+++ production/mafra/idnes.cz_xman.js
@@ -1885,6 +1885,14 @@ window.cpexWebsiteSettings = {
         params: {
           networkId: 11172
         }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'xman.idnes.cz',
+          p: 'interscroller_foto_scroll_2',
+          m: 1,
+          g: 'hb'
+        }
       }],
       filter: {
         maxWidth: 1000
