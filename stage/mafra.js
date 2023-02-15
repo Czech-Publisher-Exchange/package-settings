5360f02c5c01e82bfd22d673e75e0afba27a8820
CPEx Dev <dev@cpex.cz>
2023-02-15 21:04:30 +0100

    David: Slideup style polish

--- stage/mafra.js
+++ stage/mafra.js
@@ -20,7 +20,7 @@ window.cpexPublisherSettings = {
   },
   formats: {
     slideup: {
-      css: '#cpex-slideup {\n  background-color: transparent;\n  z-index: 9999;\n}\n#cpex-slideup-close {\n  box-sizing: border-box;\n  display: inline-block;\n  cursor: pointer;\n  position: absolute;\n  top: -40px;\n  right: 0px;\n  height: 40px;\n  padding: 7px 9px;\n  font-family: Arial, sans-serif;\n  background-color: #454545;\n  font-size: 14px;\n  color: #fff;\n}\n#cpex-slideup-close:hover {\n  background-color: #000;\n}',
+      css: '#cpex-slideup {\n  background-color: transparent;\n  z-index: 9999;\n}\n#cpex-slideup-close {\n  box-sizing: border-box;\n  display: inline-block;\n  cursor: pointer;\n  position: absolute;\n  top: -32px;\n  right: 0px;\n  height: 32px;\n  padding: 7px 9px;\n  font-family: Arial, sans-serif;\n  background-color: #454545;\n  font-size: 14px;\n  color: #fff;\n}\n#cpex-slideup-close:hover {\n  background-color: #000;\n}',
       closeTextHTML: 'zavřít reklamu'
     }
   }
