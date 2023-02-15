d9bb893d7e66116115cd86fb4022f72ea883a80f
CPEx Dev <dev@cpex.cz>
2023-02-15 21:00:57 +0100

    David: Testing slideup style

--- stage/mafra.js
+++ stage/mafra.js
@@ -1,7 +1,8 @@
 window.cpexPublisherSettings = {
   publisher: {
     name: 'MAFRA, a.s.',
-    sellerId: '003'
+    sellerId: '003',
+    code: 'mafra'
   },
   cmp: {
     enabled: false
@@ -11,5 +12,16 @@ window.cpexPublisherSettings = {
   },
   headerbidding: {
     enabled: false
+  },
+  aam: {
+    settings: {
+      publisher: 'mafra'
+    }
+  },
+  formats: {
+    slideup: {
+      css: '#cpex-slideup {\n  background-color: transparent;\n  z-index: 9999;\n}\n#cpex-slideup-close {\n  box-sizing: border-box;\n  display: inline-block;\n  cursor: pointer;\n  position: absolute;\n  top: -40px;\n  right: 0px;\n  height: 40px;\n  padding: 7px 9px;\n  font-family: Arial, sans-serif;\n  background-color: #454545;\n  font-size: 14px;\n  color: #fff;\n}\n#cpex-slideup-close:hover {\n  background-color: #000;\n}',
+      closeTextHTML: 'zavřít reklamu'
+    }
   }
 }
