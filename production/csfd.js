48d27cef366c00e34c8b58627c1dad688ad0850b
CPEx Dev <dev@cpex.cz>
2023-02-20 14:58:03 +0100

    Aleš: úprava cmpLoadTimeoutMs na 2500ms

--- production/csfd.js
+++ production/csfd.js
@@ -28,7 +28,7 @@ window.cpexPublisherSettings = {
   },
   headerbidding: {
     analytics: ['id5Analytics', 'cpexAnalytics'],
-    cmpLoadTimeoutMs: 1500
+    cmpLoadTimeoutMs: 2500
   },
   general: {
     errorApiKey: 'hbp_tCjxz6JmkE7RI1QnA7qLBjDFb4ItP23y9OCs'
