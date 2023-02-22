e97a01d7a4a5d40a39f6669e843c58f82b0038ea
CPEx Dev <dev@cpex.cz>
2023-02-22 15:59:41 +0100

    Frantisek: test editoru

---
+++ production/iinfo/test.cz.js
@@ -0,0 +1,674 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'root.cz',
+    note: 'notenote'
+  },
+  aam: {
+    settings: {
+      publisher: 'iinfo'
+    }
+  },
+  adserver: {
+    adapter: 'sastracker'
+  },
+  headerbidding: {
+    adUnits: [{
+      code: 'iinfo-area-halfpagead-horni',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [160, 600],
+            [120, 600],
+            [300, 250],
+            [300, 300]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 265202,
+          zoneId: 1335570,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 9, 8, 15, 48]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 265182,
+          zoneId: 1335568,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 9, 8, 15, 48]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: 3951701
+        }
+      }, {
+        bidder: 'im-adform',
+        params: {
+          mid: 690511,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'im-adform',
+        params: {
+          mid: 690633,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 727286,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 16315583
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'iInfo_Root_Halfpagead'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'root.cz',
+          p: '300x600',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'iinfo-area-halfpagead-spodni',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [160, 600],
+            [120, 600],
+            [300, 250],
+            [300, 300]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 265202,
+          zoneId: 2088338,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 9, 8, 15, 48]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 265182,
+          zoneId: 2088340,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 9, 8, 15, 48]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: 3735839
+        }
+      }, {
+        bidder: 'im-adform',
+        params: {
+          mid: 1057918,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'im-adform',
+        params: {
+          mid: 1057919,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 727292,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 21786341
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'iInfo_Root_Vrchni_Square'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'root.cz',
+          p: '300x300_1',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'iinfo-area-leaderboard',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [728, 90],
+            [970, 210],
+            [998, 200],
+            [2000, 1400]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 265202,
+          zoneId: 1335560,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [2, 96, 259, 230]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 265182,
+          zoneId: 1335558,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [2, 96, 259, 230]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: 3951700
+        }
+      }, {
+        bidder: 'im-adform',
+        params: {
+          mid: 690507,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'im-adform',
+        params: {
+          mid: 690629,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 727287,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 727288,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 16315579
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'iInfo_Root_Leaderboard'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'root.cz',
+          p: 'branding',
+          m: 0,
+          g: 'hb'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'root.cz',
+          p: '998x200',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'iinfo-area-leaderboard',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [320, 100]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 265202,
+          zoneId: 1335560,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [117]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 265182,
+          zoneId: 1335558,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [117]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: 3951700
+        }
+      }, {
+        bidder: 'im-adform',
+        params: {
+          mid: 690507,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'im-adform',
+        params: {
+          mid: 690629,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 727287,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 727288,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 16315579
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'iInfo_Root_Leaderboard_Mobile'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'root.cz',
+          p: '998x200',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'iinfo-area-big-rectangle',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [970, 310],
+            [970, 210],
+            [970, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 265202,
+          zoneId: 1336454,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [94, 96, 57]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 265182,
+          zoneId: 1336452,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [94, 96, 57]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: 3951702
+        }
+      }, {
+        bidder: 'im-adform',
+        params: {
+          mid: 690512,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'im-adform',
+        params: {
+          mid: 690634,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 727284,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 727285,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 16315584
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'iInfo_Root_Big_Rectangle'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'root.cz',
+          p: '970x310',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'iinfo-area-big-rectangle',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [300, 300]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 265202,
+          zoneId: 1336454,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 48]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 265182,
+          zoneId: 1336452,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 48]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: 3951702
+        }
+      }, {
+        bidder: 'im-adform',
+        params: {
+          mid: 690512,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'im-adform',
+        params: {
+          mid: 690634,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 727284,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 727285,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 16315584
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'iInfo_Root_Big_Rectangle_Mobile'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'root.cz',
+          p: '970x310',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'iinfo-area-rectangle',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [480, 300],
+            [300, 250],
+            [300, 300]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 265202,
+          zoneId: 1335574,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83, 15, 48]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 265182,
+          zoneId: 1335572,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83, 15, 48]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: 3951703
+        }
+      }, {
+        bidder: 'im-adform',
+        params: {
+          mid: 690508,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'im-adform',
+        params: {
+          mid: 690630,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 727289,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 727290,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 16315580
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'Root_homepage_2'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'root.cz',
+          p: '480x300',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'iinfo-area-rectangle',
+      mediaTypes: {
+        native: {}
+      },
+      bids: []
+    }, {
+      code: 'iinfo-area-spodni-square-banner',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 300],
+            [300, 250],
+            [250, 250],
+            [200, 200]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 265202,
+          zoneId: 1335566,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [48, 15, 14, 13]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 265182,
+          zoneId: 1335564,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [48, 15, 14, 13]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: 3951704
+        }
+      }, {
+        bidder: 'im-adform',
+        params: {
+          mid: 690510,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'im-adform',
+        params: {
+          mid: 690632,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 727291,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 16315582
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'iInfo_Root_Spodni_Square'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'root.cz',
+          p: '300x300_2',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }]
+  },
+  formats: {
+    skin: {
+      enabled: true,
+      contentEl: (document.getElementsByClassName('parter-cpex-branding-wrapper')[0]),
+      contentCSS: 'width: 1060px; margin: 200px auto;',
+      offset: 25,
+      offsetScroll: true,
+      hide: [(document.getElementsByClassName('design-advert-placeholder--leaderboard')[0])]
+    }
+  }
+}
