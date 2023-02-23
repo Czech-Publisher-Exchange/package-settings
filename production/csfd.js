b94cfeb3866345b3dc7b0fff96d12ad7fd8ab3f2
CPEx Dev <dev@cpex.cz>
2023-02-23 09:49:11 +0100

    Aleš: Úprava CMP timeoutů

--- production/csfd.js
+++ production/csfd.js
@@ -27,7 +27,9 @@ window.cpexPublisherSettings = {
     enabled: true
   },
   headerbidding: {
-    analytics: ['id5Analytics', 'cpexAnalytics']
+    analytics: ['id5Analytics', 'cpexAnalytics'],
+    cmpLoadTimeoutMs: '1000',
+    cmpActionTimeoutMs: '1000'
   },
   general: {
     errorApiKey: 'hbp_tCjxz6JmkE7RI1QnA7qLBjDFb4ItP23y9OCs'
