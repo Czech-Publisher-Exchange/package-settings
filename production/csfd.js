a9aac0ab16c9244bfcfdcc2131741be06261d0da
CPEx Dev <dev@cpex.cz>
2023-02-23 14:09:04 +0100

    Aleš: uložení cmpActionTimeoutMs na číslo

--- production/csfd.js
+++ production/csfd.js
@@ -29,7 +29,7 @@ window.cpexPublisherSettings = {
   headerbidding: {
     analytics: ['id5Analytics', 'cpexAnalytics'],
     cmpLoadTimeoutMs: '1000',
-    cmpActionTimeoutMs: '1000'
+    cmpActionTimeoutMs: 1000
   },
   general: {
     errorApiKey: 'hbp_tCjxz6JmkE7RI1QnA7qLBjDFb4ItP23y9OCs'
