0cbf7da9002c29c244f5cf4e639343a0785e2fe7
CPEx Dev <dev@cpex.cz>
2023-03-23 09:40:35 +0100

    Frantisek: uvodni nastaveni

---
+++ production/eco/vyber.centrum.cz.js
@@ -0,0 +1,586 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'vyber.centrum.cz'
+  },
+  cmp: {
+    enabled: false
+  },
+  aam: {
+    settings: {
+      publisher: 'eco'
+    }
+  },
+  headerbidding: {
+    adUnits: [{
+      code: 'leader',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [970, 250],
+            [970, 210],
+            [728, 90],
+            [2000, 1400],
+            [970, 100]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 385178,
+          zoneId: 2614894,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [57, 96, 2, 230, 95]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 385180,
+          zoneId: 2142928,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [57, 96, 2, 230, 95]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic_eco_hb',
+        params: {
+          publisherId: '161532',
+          adSlot: '4344936'
+        },
+        bidderModuleName: 'pubmatic'
+      }, {
+        bidder: 'adform_im_hb',
+        params: {
+          mid: 1334582,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform_im_hb-mask',
+        params: {
+          mid: 1334584,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1148666,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 759674
+        }
+      }, {
+        bidder: 'teads',
+        params: {
+          placementId: 158416,
+          pageId: 144524
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 22213120
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'ECO_centrumcz_leader'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'centrum.cz',
+          p: '728x90_2',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'halfpage',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [160, 600],
+            [120, 600],
+            [300, 300],
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 385178,
+          zoneId: 2142942,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 9, 8, 48, 15]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 385180,
+          zoneId: 2142944,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 9, 8, 48, 15]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic_eco_hb',
+        params: {
+          publisherId: '161532',
+          adSlot: '4344935'
+        },
+        bidderModuleName: 'pubmatic'
+      }, {
+        bidder: 'adform_im_hb',
+        params: {
+          mid: 1334586,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform_im_hb-mask',
+        params: {
+          mid: 1334588,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1148675,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 759673
+        }
+      }, {
+        bidder: 'teads',
+        params: {
+          placementId: 153776,
+          pageId: 140099
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 22213128
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'ECO_centrumcz_halfpage'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'centrum.cz',
+          p: '300x600',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'mediumrectangle',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 300],
+            [300, 250],
+            [250, 250],
+            [300, 50]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 385178,
+          zoneId: 2142934,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [48, 15, 14, 44]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 385180,
+          zoneId: 2142936,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [48, 15, 14, 44]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic_eco_hb',
+        params: {
+          publisherId: '161532',
+          adSlot: '4344934'
+        },
+        bidderModuleName: 'pubmatic'
+      }, {
+        bidder: 'adform_im_hb',
+        params: {
+          mid: 1334589,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform_im_hb-mask',
+        params: {
+          mid: 1334592,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1148708,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 759675
+        }
+      }, {
+        bidder: 'teads',
+        params: {
+          placementId: 158417,
+          pageId: 144525
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 22213123
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'ECO_centrumcz_mediumrectangle'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'centrum.cz',
+          p: '300x250',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'strip',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [728, 90],
+            [320, 50],
+            [300, 50]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 385182,
+          zoneId: 2142958,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [2, 43, 44]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 385184,
+          zoneId: 2142960,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [2, 43, 44]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic_eco_hb',
+        params: {
+          publisherId: '161532',
+          adSlot: '4426138'
+        },
+        bidderModuleName: 'pubmatic'
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1421075,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 809614
+        }
+      }, {
+        bidder: 'teads',
+        params: {
+          placementId: 165641,
+          pageId: 151406
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 22213136
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'ECO_centrumcz_strip'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'centrum.cz',
+          p: '320x50',
+          m: 1,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'mediumrectangle-mob-pos1',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 300],
+            [300, 250],
+            [250, 250],
+            [300, 50],
+            [480, 480]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 385182,
+          zoneId: 2142966,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [48, 15, 14, 44, 261]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 385184,
+          zoneId: 2142968,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [48, 15, 14, 44, 261]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic_eco_hb',
+        params: {
+          publisherId: '161532',
+          adSlot: '4426129'
+        },
+        bidderModuleName: 'pubmatic'
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1421069,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 809612
+        }
+      }, {
+        bidder: 'teads',
+        params: {
+          placementId: 150211,
+          pageId: 136835
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 22213138
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'ECO_centrumcz_mediumrectangle_mob_pos1'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'centrum.cz',
+          p: '300x250_1',
+          m: 1,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'mediumrectangle-mob-pos2',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 300],
+            [300, 250],
+            [250, 250],
+            [300, 50],
+            [480, 480]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 385182,
+          zoneId: 2142970,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [48, 15, 14, 44, 261]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 385184,
+          zoneId: 2142972,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [48, 15, 14, 44, 261]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic_eco_hb',
+        params: {
+          publisherId: '161532',
+          adSlot: '4426131'
+        },
+        bidderModuleName: 'pubmatic'
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1421071,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 809613
+        }
+      }, {
+        bidder: 'teads',
+        params: {
+          placementId: 165642,
+          pageId: 151407
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 22213139
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'ECO_centrumcz_mediumrectangle_mob_pos2'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'centrum.cz',
+          p: '300x250_2',
+          m: 1,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'popup',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [500, 200]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 385182,
+          zoneId: 2669034,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 258]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 385184,
+          zoneId: 2669036,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 258]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 28273071
+        }
+      }]
+    }],
+    currency: 'USD',
+    analytics: ['id5Analytics', 'cpexAnalytics']
+  },
+  formats: {
+    skin: {
+      enabled: true,
+      contentEl: (document.getElementsByClassName('sc-1cb3an8-0')[0])
+    },
+    native: {
+      enabled: true
+    },
+    slideup: {
+      adUnit: 'reklama-popupmobile',
+      backgroundEl: (document.getElementById('reklama-popupmobile-wrapper')),
+      enabled: true
+    }
+  }
+}
