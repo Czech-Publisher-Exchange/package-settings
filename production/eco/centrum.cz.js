c672fda9cee8e818b4e3fe1fdbd284e1ca0812c0
CPEx Dev <dev@cpex.cz>
2023-03-23 17:34:45 +0100

    David: Odebrání komentáře

--- production/eco/centrum.cz.js
+++ production/eco/centrum.cz.js
@@ -567,7 +567,8 @@ window.cpexWebsiteSettings = {
       }]
     }],
     currency: 'USD',
-    analytics: ['id5Analytics', 'cpexAnalytics']
+    analytics: ['id5Analytics', 'cpexAnalytics'],
+    enabled: false
   },
   formats: {
     skin: {
@@ -584,6 +585,11 @@ window.cpexWebsiteSettings = {
     }
   },
   general: {
-    errorPath: 'https://73f2bd72d0d2477ab2f976d6098fe246@o530000.ingest.sentry.io/4504531846365184'
+    errorPath: 'https://73f2bd72d0d2477ab2f976d6098fe246@o530000.ingest.sentry.io/4504531846365184',
+    beforeLoad: (() => {
+      if (!!['www.centrum.cz', 'atlas.centrum.cz', 'pocasi.centrum.cz', 'volny.centrum.cz'].find(str => window.location.href.indexOf(str) !== -1)) {
+        window.cpexPackage.settings.headerbidding.enabled = true
+      }
+    })
   }
 }
