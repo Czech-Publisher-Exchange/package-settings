5bcab7266c70437a82ad25ae0b970583619dc2e3
CPEx Dev <dev@cpex.cz>
2023-04-18 12:26:56 +0200

    David: Added passback for testing

---
+++ stage/incorp/eurozpravy.cz.js
@@ -0,0 +1,75 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'eurozpravy.cz'
+  },
+  aam: {
+    settings: {
+      publisher: 'incorp'
+    }
+  },
+  adserver: {
+    enabled: false
+  },
+  headerbidding: {
+    adUnits: [{
+      code: 'leaderboard',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [998, 200],
+            [728, 90],
+            [970, 210],
+            [2000, 1400]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 363124,
+          zoneId: 2831580,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [259, 2, 96, 230]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 363126,
+          zoneId: 2831582,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [259, 2, 96, 230]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '5083139'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 21054355
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 93608
+        }
+      }]
+    }],
+    passbacks: {
+      leaderboard: "const src = 'https://eurozpravy.cz/passback.html'\nconst elementId = 'main'\nconst type = 'skin'\nconst size = { width: 2000, height: 1400 }\n\ncpexPackage.main.formats.prepareIframe(type, elementId, size.width, size.height, src)\ncpexPackage.main.customAds['main'] = { type, elementId }\ncpexPackage.headerbidding.prepareMetaData(elementId, { type: 'passback' })"
+    }
+  },
+  formats: {
+    skin: {
+      enabled: true,
+      contentEl: (document.getElementsByClassName('main__content')[0]),
+      offset: 60,
+      contentCSS: 'position: relative; margin-top: -26px;'
+    }
+  }
+}
