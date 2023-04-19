ce59b991ce52dc04dcc5bdf7865d88d671e58aff
CPEx Dev <dev@cpex.cz>
2023-04-19 12:28:04 +0200

    David: Re-saved to stage

---
+++ stage/incorp.js
@@ -0,0 +1,15 @@
+window.cpexPublisherSettings = {
+  publisher: {
+    code: 'incorp',
+    name: 'INCORP a.s.',
+    sellerId: '22'
+  },
+  cmp: {
+    enabled: false
+  },
+  aam: {
+    settings: {
+      publisher: 'incorp'
+    }
+  }
+}
