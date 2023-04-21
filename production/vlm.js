a42e1a34a155e212d126fa17ca65e3b40150cb07
CPEx Dev <dev@cpex.cz>
2023-04-21 15:27:53 +0200

    David: Fixed vignette style

--- production/vlm.js
+++ production/vlm.js
@@ -51,5 +51,15 @@ window.cpexPublisherSettings = {
   },
   cmp: {
     enabled: false
+  },
+  formats: {
+    vignette: {
+      cookieDuration: 3600,
+      cookieName: 'vignette',
+      closeTextHTML: "<svg width='32' height='32' viewBox='29 15 26 22'><circle cx='42' cy='26' r='12' stroke='white' stroke-width='1.5'></circle><path d='M 36,20 l 12,12 M 36,32 l 12,-12' stroke='white' stroke-width='2'></path></svg>",
+      css: '#cpex-vignette {\n    background-color: #000;\n    z-index: 2147483642;\n}\n#cpex-vignette-header {\n    margin: 0 18px 18px auto;\n    font-family: arial, sans-serif;\n    font-size: 12px;\n    color: #FFFFFF;\n    text-align: right;\n    padding: 4px 8px;\n    background: black;\n    position: absolute;\n    bottom: 0;\n    right: 0;\n    z-index: 1;\n    font-weight: 700;\n}\n#cpex-vignette-close {\n    position: absolute;\n    right: 0px;\n    top: 0px;\n}',
+      adUnits: ['centerVignette', 'm-vignette'],
+      enabled: true
+    }
   }
 }
