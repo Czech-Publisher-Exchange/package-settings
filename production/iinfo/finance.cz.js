419f00ad9ad1018c0fe31581bc73ac04a21dd89a
CPEx Dev <dev@cpex.cz>
2023-04-23 23:51:10 +0200

    Frantisek: uprava skin zpet

--- production/iinfo/finance.cz.js
+++ production/iinfo/finance.cz.js
@@ -394,9 +394,7 @@ window.cpexWebsiteSettings = {
   formats: {
     skin: {
       enabled: true,
-      contentEl: (document.getElementsByClassName('parter-cpex-branding-wrapper')[0]),
-      hide: [(document.getElementsByClassName('leaderboard-top')[0])],
-      contentCSS: 'width: 1060px; margin: 200px auto; position: relative;'
+      contentEl: (document.getElementsByClassName('parter-cpex-branding-wrapper')[0])
     }
   }
 }
