15e26b509dd08a95d5f4d9542153da120ed4ddc4
CPEx Dev <dev@cpex.cz>
2023-05-05 09:23:30 +0200

    Aleš: Nastavení adapterů pro SAS

---
+++ stage/nova.js
@@ -0,0 +1,25 @@
+window.cpexPublisherSettings = {
+  publisher: {
+    code: 'nova',
+    name: 'TV Nova s.r.o.',
+    sellerId: '25'
+  },
+  cmp: {
+    enabled: false
+  },
+  aam: {
+    settings: {
+      publisher: 'nova'
+    }
+  },
+  adserver: {
+    adapter: 'sastracker',
+    loadPrerequisites: true
+  },
+  general: {
+    onLoad: (() => {
+      const lightTheme = document.documentElement.classList.contains('-light-theme');
+      document.querySelector(':root').style.setProperty('--theme-bg', lightTheme ? 'white' : window.getComputedStyle(document.body).backgroundColor)
+    })
+  }
+}
