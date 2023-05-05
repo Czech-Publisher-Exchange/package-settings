80ee94f1d8aa4f9314192cf3a12ebca53a6e0d44
David Spohr <spohr.david@gmail.com>
2023-05-05 15:48:06 +0200

    Oprava skinu

--- production/nova/sport.js
+++ production/nova/sport.js
@@ -705,7 +705,7 @@ window.cpexWebsiteSettings = {
       enabled: true,
       contentEl: (document.getElementsByClassName('c-content')[0]),
       offset: 78,
-      contentCSS: 'position: relative; margin: auto; margin-top: 280px; background-color: var`--theme-bg, white`;'
+      contentCSS: 'position: relative; margin: auto; margin-top: 280px; background-color: var(--theme-bg, white);'
     },
     interscroller: {
       enabled: true
