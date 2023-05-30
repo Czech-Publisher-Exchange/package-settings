539d2d7f66565eaa3e10c65386b633d162c249fc
CPEx Dev <dev@cpex.cz>
2023-05-30 11:40:14 +0200

    Aleš: publikování do produkce (nemají zatím nasazeno)

---
+++ production/nova/tv_nova.cz.js
@@ -0,0 +1,30 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'tv.nova.cz'
+  },
+  aam: {
+    settings: {
+      publisher: 'nova'
+    }
+  },
+  adserver: {
+    adapter: 'sastracker',
+    gamId: '27415282',
+    gamPrefix: 'TN_'
+  },
+  headerbidding: {
+    enabled: false
+  },
+  formats: {
+    interscroller: {
+      enabled: true
+    },
+    skin: {
+      enabled: true,
+      contentEl: /*S*/ document.getElementsByClassName('c-content')[0] /*E*/ ,
+      offset: 80,
+      offsetScroll: true,
+      contentCSS: 'position: relative; margin: auto; margin-top: 280px; padding-top: 0px; background-color: var(--theme-bg, white);'
+    }
+  }
+}
