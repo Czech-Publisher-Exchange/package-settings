4eb8e0c4d482ac7728c366134c95a9e01ab3bef6
CPEx Dev <dev@cpex.cz>
2023-06-05 13:19:22 +0200

    Frantisek: pocatecni nastaveni

---
+++ production/eco/default.js
@@ -0,0 +1,14 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'default',
+    note: 'defaultni nastaveni bez prebidu'
+  },
+  aam: {
+    settings: {
+      publisher: 'eco'
+    }
+  },
+  headerbidding: {
+    enabled: false
+  }
+}
