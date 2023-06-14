2ecad282fdbf380abbb24a8aafc938c394bceabd
CPEx Dev <dev@cpex.cz>
2023-06-14 12:31:12 +0200

    Frantisek: uprava responsive

--- production/mafra/penize.cz.js
+++ production/mafra/penize.cz.js
@@ -1372,9 +1372,12 @@ window.cpexWebsiteSettings = {
     },
     skin: {
       enabled: true,
-      backgroundEl: /*S*/document.getElementById('main')/*E*/,
-      contentEl: /*S*/document.getElementsByClassName('m-bg-4')[0]/*E*/,
+      backgroundEl: /*S*/ document.getElementById('main') /*E*/ ,
+      contentEl: /*S*/ document.getElementsByClassName('m-bg-4')[0] /*E*/ ,
       contentCSS: ' '
+    },
+    responsive: {
+      containerEl: /*S*/ document.getElementById('content') /*E*/
     }
   }
 }
