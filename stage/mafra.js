4c20ded4ed3cd978997ed830bf3b4fc8fc55dd99
CPEx Dev <dev@cpex.cz>
2023-06-20 14:30:31 +0200

    David: Added first native templates

--- stage/mafra.js
+++ stage/mafra.js
@@ -2,26 +2,74 @@ window.cpexPublisherSettings = {
   publisher: {
     name: 'MAFRA, a.s.',
     sellerId: '003',
-    code: 'mafra'
+    code: 'mafra',
+    note: '_'
   },
   cmp: {
     enabled: false
   },
   adserver: {
-    enabled: false
-  },
-  headerbidding: {
-    enabled: false
+    enabled: true,
+    adapter: 'paticka',
+    bidderTable: {
+      appnexus: 'xandr_hb',
+      pubmatic: 'pubmatic_hb',
+      rubicon: 'magnite_hb',
+      adform: 'adform_hb',
+      r2b2: 'r2b2_hb',
+      ix: 'index_hb',
+      'omg-adform': 'OMG-Adform_HB',
+      'fragile-adform': 'Fragile-Adform_HB',
+      criteo: 'Criteo_HB',
+      teads: 'Teads_HB'
+    }
   },
   aam: {
     settings: {
       publisher: 'mafra'
     }
   },
+  headerbidding: {
+    cmpLoadTimeoutMs: 1000,
+    bidderSettings: {
+      rubicon: {
+        bidCpmAdjustment: /*S*/ (bidCpm, bid) => {
+          return bid.dealId ? bidCpm * 0.93 : bidCpm * 0.88
+        } /*E*/
+      },
+      'rubicon-mask': {
+        bidCpmAdjustment: /*S*/ (bidCpm, bid) => {
+          return bid.dealId ? bidCpm * 0.93 : bidCpm * 0.88
+        } /*E*/
+      },
+      invibes: {
+        params: {
+          customEndpoint: 'https://bid5.videostep.com/Bid/VideoAdContent'
+        }
+      }
+    }
+  },
   formats: {
     slideup: {
-      css: '#cpex-slideup {\n  background-color: transparent;\n  z-index: 9999;\n}\n#cpex-slideup-close {\n  box-sizing: border-box;\n  display: inline-block;\n  cursor: pointer;\n  position: absolute;\n  top: -32px;\n  right: 0px;\n  height: 32px;\n  padding: 7px 9px;\n  font-family: Arial, sans-serif;\n  background-color: #454545;\n  font-size: 14px;\n  color: #fff;\n}\n#cpex-slideup-close:hover {\n  background-color: #000;\n}',
-      closeTextHTML: 'zavřít reklamu'
+      closeTextHTML: 'zavřít reklamu',
+      css: '#cpex-slideup {  background-color: transparent;  z-index: 9999;}#cpex-slideup-close {  box-sizing: border-box;  display: inline-block;  cursor: pointer;  position: absolute;  top: -32px;  right: 0px;  height: 32px;  padding: 7px 9px;  font-family: Arial, sans-serif;  background-color: #454545;  font-size: 14px;  color: #fff;}#cpex-slideup-close:hover {  background-color: #000;}'
+    },
+    responsive: {
+      enabled: true,
+      adUnits: ['m_branding', 'm_branding_foto', 'm_branding_foto2', 'm_300x250', 'm_300x250_clankovy', 'm_300x250_clankovy_opak1', 'm_300x250_clankovy_opak2', 'm_300x250_foto_end', 'm_300x250_foto_mid', 'm_300x250_foto_mid2', 'm_300x250_foto_start', 'm_300x250_infinite'],
+      width: 480,
+      height: 480,
+      containerEl: /*S*/ document.getElementById('text-out-box') /*E*/
+    },
+    vignette: {
+      css: "#cpex-vignette {    background-color: #000E;    z-index: 2147483642;}#cpex-vignette-close {    position: absolute;    z-index: 999;    right: 20px;    top: 10px;    box-sizing: border-box;    display: inline-block;    cursor: pointer;    width: 30px;    height: 30px;    padding: 10px;    font-size: 0pt;    border: none;    background-image: url('https://trackad.cz/icon.php?icon=closer2&front=cacaca&back=000000');    background-size: contain;    border-radius: 30px;    box-shadow: 0 0 9px 3px #cacaca;}#cpex-vignette-header {    position: absolute;    right: 10px;    bottom: 20px;    font-size: 12px;    background: rgba`40, 40, 40, 0.7`;    color: #d1d1d1;    padding: 1px 3px;    text-transform: capitalize;}",
+      closeTextHTML: ' '
+    },
+    native: {
+      templates: {
+        nativ: "<div class='art ads-mockup'>\n  <a href='${link}' target='_blank' class='art-link'>\n    <h3>${title}</h3>\n    <div class='art-img w230'>\n      <img src='${img}'>\n      <span></span>\n    </div>\n  </a>\n  <p class='perex'>${desc}</p>\n</div>",
+        m_nativ: "<div class='art ads-mockup'>\n  <a rel='sponsored' href='${link}' class='art-link' target='_blank'>\n    <img title='' alt='' src='${img}' class='art-img sp5 ' width='100' height='75'>\n    <h3>${title}</h3>\n  </a>\n</div>"
+      }
     }
   }
 }
