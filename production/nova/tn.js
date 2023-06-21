696b341eaedb8e23582a97b3c398568bfd6e7f17
CPEx Dev <dev@cpex.cz>
2023-06-21 13:36:52 +0200

    Aleš: úprava pro skin - schování leaderboard el

--- production/nova/tn.js
+++ production/nova/tn.js
@@ -1254,7 +1254,8 @@ window.cpexWebsiteSettings = {
       contentEl: /*S*/ document.getElementsByClassName('c-content')[0] /*E*/ ,
       offset: 80,
       offsetScroll: true,
-      contentCSS: 'position: relative; margin: auto; margin-top: 280px; padding-top: 0px; background-color: var(--theme-bg, white);'
+      contentCSS: 'position: relative; margin: auto; margin-top: 280px; padding-top: 0px; background-color: var(--theme-bg, white);',
+      hide: [ /*S*/ document.getElementsByClassName('c-leaderboard')[0] /*E*/ ]
     }
   }
 }
