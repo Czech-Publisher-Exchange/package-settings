ce1bf59eba2828f2b5601b21aca388ca774b7c69
CPEx Dev <dev@cpex.cz>
2023-06-26 11:29:01 +0200

    Aleš: úprava pro stylování skinů

--- production/nova/sport.js
+++ production/nova/sport.js
@@ -1167,7 +1167,8 @@ window.cpexWebsiteSettings = {
       contentEl: /*S*/ document.getElementsByClassName('c-content')[0] /*E*/ ,
       offset: 78,
       contentCSS: 'position: relative; margin: auto; margin-top: 280px; background-color: var(--theme-bg, white);',
-      hide: [ /*S*/ document.getElementsByClassName('c-leaderboard')[0] /*E*/ ]
+      hide: [ /*S*/ document.getElementsByClassName('c-leaderboard')[0] /*E*/ ],
+      generalCSS: ' .c-branding .c-leaderboard {\n   height: 0px;\n   max-height: 0px\n }'
     },
     interscroller: {
       enabled: true
