2c52e30ca9a3be2b54137b0d18a857fc6bb687d9
CPEx Dev <dev@cpex.cz>
2023-06-27 12:59:57 +0200

    Aleš: Úprava contentCSS pro skin

--- production/nova/sport.js
+++ production/nova/sport.js
@@ -1166,8 +1166,8 @@ window.cpexWebsiteSettings = {
       enabled: true,
       contentEl: /*S*/ document.getElementsByClassName('c-content')[0] /*E*/ ,
       offset: 78,
-      contentCSS: 'position: relative; margin: auto; margin-top: 280px; background-color: var(--theme-bg, white);',
-      generalCSS: '.c-branding .c-leaderboard {\n   height: 0px;\n   min-height: 0px\n }\n\nbody .c-leaderboard #div-ad-leaderboard {\n   width: 1920px;\n    height: 0;\n    min-height: 0;\n    position: fixed;\n    left: 50%;\n    top: 83px;\n    transform: translateX(-50%);\n}\nbody .c-leaderboard #div-ad-leaderboard .banner-highlight {\n    height: 1400px;\n}'
+      contentCSS: 'position: relative;\nmargin: auto;\nmargin-top: 280px;\npadding-top: 0px;\nbackground-color: var(--theme-bg, white);',
+      generalCSS: '.c-branding .c-leaderboard {   height: 0px;   min-height: 0px }body .c-leaderboard #div-ad-leaderboard {   width: 1920px;    height: 0;    min-height: 0;    position: fixed;    left: 50%;    top: 83px;    transform: translateX(-50%);}body .c-leaderboard #div-ad-leaderboard .banner-highlight {    height: 1400px;}'
     },
     interscroller: {
       enabled: true
