54b417862f0b52265787e23a38b0f4b88e8497a4
CPEx Dev <dev@cpex.cz>
2023-06-27 14:00:05 +0200

    Jana: vytvoření publisher settings pro echo24

---
+++ production/echo24.js
@@ -0,0 +1,29 @@
+window.cpexPublisherSettings = {
+  publisher: {
+    name: 'Echo Media a.s.',
+    sellerId: '029',
+    code: 'echo24'
+  },
+  adserver: {
+    adapter: 'sastracker',
+    bidderTable: {
+      criteo: 'Criteo',
+      rubicon: 'Magnite_HB',
+      appnexus: 'Xandr_HB',
+      pubmatic: 'Pubmatic_HB',
+      ix: 'Index_HB'
+    }
+  },
+  aam: {
+    settings: {
+      publisher: 'echo24'
+    },
+    enabled: true
+  },
+  headerbidding: {
+    analytics: ['id5Analytics', 'cpexAnalytics']
+  },
+  general: {
+    errorApiKey: 'hbp_tCjxz6JmkE7RI1QnA7qLBjDFb4ItP23y9OCs'
+  }
+}
