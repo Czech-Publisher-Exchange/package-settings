cef8aa49374e6ed74d3cc2a646916fc709ccee52
CPEx Dev <dev@cpex.cz>
2023-06-28 08:59:36 +0200

    Markéta: přidání R2B2 a Invibes

--- production/mafra/idnes.cz_cestovani.js
+++ production/mafra/idnes.cz_cestovani.js
@@ -1533,6 +1533,12 @@ window.cpexWebsiteSettings = {
           m: 0,
           g: 'hb'
         }
+      }, {
+        bidder: 'invibes',
+        params: {
+          placementId: 'idnes.cz/cestovani',
+          site: 'invibes_cestovani'
+        }
       }]
     }, {
       code: 'branding_fotogalerie',
@@ -2469,6 +2475,12 @@ window.cpexWebsiteSettings = {
           m: 0,
           g: 'hb'
         }
+      }, {
+        bidder: 'invibes',
+        params: {
+          placementId: 'idnes.cz/cestovani',
+          site: 'invibes_cestovani2'
+        }
       }]
     }, {
       code: 'm_vignete',
@@ -2581,6 +2593,14 @@ window.cpexWebsiteSettings = {
         bidder: 'criteo',
         params: {
           networkId: 11172
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'cestovani.idnes.cz',
+          p: '300x600_rubrika_lazyload',
+          m: 0,
+          g: 'hb'
         }
       }]
     }, {
