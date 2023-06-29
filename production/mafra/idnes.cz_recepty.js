793118f5b058cd3f5ec329a5f881ab8867e1a576
CPEx Dev <dev@cpex.cz>
2023-06-29 10:53:53 +0200

    Markéta: oprava Invibes

--- production/mafra/idnes.cz_recepty.js
+++ production/mafra/idnes.cz_recepty.js
@@ -1539,8 +1539,8 @@ window.cpexWebsiteSettings = {
       }, {
         bidder: 'invibes',
         params: {
-          placementId: 'idnes.cz/recepty',
-          site: 'invibes_recepty'
+          placementId: 'invibes_recepty',
+          site: 'idnes.cz/recepty'
         }
       }]
     }, {
