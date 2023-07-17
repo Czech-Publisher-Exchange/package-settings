29334618a0e6de4acf392c216e63ef76686cf4fc
CPEx Dev <dev@cpex.cz>
2023-07-17 17:24:38 +0200

    Iveta: přidání size 300x250 na m-sticky

--- production/vlm/kondice.cz.js
+++ production/vlm/kondice.cz.js
@@ -882,7 +882,8 @@ window.cpexWebsiteSettings = {
           sizes: [
             [320, 150],
             [300, 120],
-            [500, 200]
+            [500, 200],
+            [300, 250]
           ]
         }
       },
@@ -893,7 +894,7 @@ window.cpexWebsiteSettings = {
           zoneId: 2805946,
           accountId: '10900',
           position: 'atf',
-          sizes: [60, 85, 258]
+          sizes: [60, 85, 258, 15]
         }
       }, {
         bidder: 'rubicon-mask',
@@ -902,7 +903,7 @@ window.cpexWebsiteSettings = {
           zoneId: 2806660,
           accountId: '10900',
           position: 'atf',
-          sizes: [60, 85, 258]
+          sizes: [60, 85, 258, 15]
         },
         bidderModuleName: 'rubicon'
       }, {
