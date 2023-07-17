8e7866e400709bb6416c593ae5b5cf07c148bca7
CPEx Dev <dev@cpex.cz>
2023-07-17 16:18:45 +0200

    Iveta: zapnutí responsive funkce

--- production/vlm.js
+++ production/vlm.js
@@ -33,19 +33,19 @@ window.cpexPublisherSettings = {
     analytics: ['id5Analytics', 'cpexAnalytics'],
     bidderSettings: {
       rubicon: {
-        bidCpmAdjustment: /*S*/(bidCpm, bid) => {
+        bidCpmAdjustment: /*S*/ (bidCpm, bid) => {
           return bid.dealId ? bidCpm * 0.93 : bidCpm * 0.88
-        }/*E*/
+        } /*E*/
       },
       'rubicon-mask': {
-        bidCpmAdjustment: /*S*/(bidCpm, bid) => {
+        bidCpmAdjustment: /*S*/ (bidCpm, bid) => {
           return bid.dealId ? bidCpm * 0.93 : bidCpm * 0.88
-        }/*E*/
+        } /*E*/
       },
       adform: {
-        bidCpmAdjustment: /*S*/(bidCpm, bid) => {
+        bidCpmAdjustment: /*S*/ (bidCpm, bid) => {
           return bidCpm * 0.9
-        }/*E*/
+        } /*E*/
       }
     }
   },
@@ -57,9 +57,14 @@ window.cpexPublisherSettings = {
       cookieDuration: 3600,
       cookieName: 'vignette',
       closeTextHTML: "<svg width='32' height='32' viewBox='29 15 26 22'><circle cx='42' cy='26' r='12' stroke='white' stroke-width='1.5'></circle><path d='M 36,20 l 12,12 M 36,32 l 12,-12' stroke='white' stroke-width='2'></path></svg>",
-      css: '#cpex-vignette {\n    background-color: #000;\n    z-index: 2147483642;\n}\n#cpex-vignette-header {\n    margin: 0 18px 18px auto;\n    font-family: arial, sans-serif;\n    font-size: 12px;\n    color: #FFFFFF;\n    text-align: right;\n    padding: 4px 8px;\n    background: black;\n    position: absolute;\n    bottom: 0;\n    right: 0;\n    z-index: 1;\n    font-weight: 700;\n}\n#cpex-vignette-close {\n    position: absolute;\n    right: 0px;\n    top: 0px;\n    background: none;\n    border: none;\n}',
+      css: '#cpex-vignette {    background-color: #000;    z-index: 2147483642;}#cpex-vignette-header {    margin: 0 18px 18px auto;    font-family: arial, sans-serif;    font-size: 12px;    color: #FFFFFF;    text-align: right;    padding: 4px 8px;    background: black;    position: absolute;    bottom: 0;    right: 0;    z-index: 1;    font-weight: 700;}#cpex-vignette-close {    position: absolute;    right: 0px;    top: 0px;    background: none;    border: none;}',
       adUnits: ['centerVignette', 'm-vignette'],
       enabled: true
+    },
+    responsive: {
+      enabled: true,
+      width: 480,
+      height: 480
     }
   }
 }
