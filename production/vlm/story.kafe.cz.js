0a372f1c9352adde3866322ecc8c6e19659a0868
CPEx Dev <dev@cpex.cz>
2023-07-24 16:32:12 +0200

    Iveta: zapnutí responsive funkce

--- production/vlm/story.kafe.cz.js
+++ production/vlm/story.kafe.cz.js
@@ -817,6 +817,9 @@ window.cpexWebsiteSettings = {
     slideup: {
       enabled: false,
       adUnits: ['m-sticky']
+    },
+    responsive: {
+      adUnits: ['m-square-1', 'm-square-2', 'm-square-3', 'm-square-4', 'm-square-5']
     }
   },
   cmp: {
