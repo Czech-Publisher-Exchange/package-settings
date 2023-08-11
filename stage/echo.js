db563ee0436ce8c66635ed680d10fbfea9bc0797
CPEx Dev <dev@cpex.cz>
2023-08-11 14:05:55 +0200

    Aleš: Vytvoření stage publisher settings

---
+++ stage/echo.js
@@ -0,0 +1,39 @@
+window.cpexPublisherSettings = {
+  publisher: {
+    name: 'Echo Media a.s.',
+    sellerId: '029',
+    code: 'echo'
+  },
+  adserver: {
+    adapter: 'sastracker',
+    bidderTable: {
+      criteo: 'Criteo',
+      rubicon: 'Magnite_HB',
+      appnexus: 'Xandr_HB',
+      pubmatic: 'Pubmatic_HB',
+      ix: 'Index_HB'
+    },
+    allowedSSPs: {
+      pubmatic: 1,
+      index: 2,
+      magnite: 4,
+      xandr: 5
+    },
+    areaKey: 'divId',
+    enabled: true
+  },
+  headerbidding: {
+    analytics: ['id5Analytics', 'cpexAnalytics']
+  },
+  general: {
+    errorApiKey: 'hbp_tCjxz6JmkE7RI1QnA7qLBjDFb4ItP23y9OCs'
+  },
+  cmp: {
+    enabled: false
+  },
+  aam: {
+    settings: {
+      publisher: 'echo'
+    }
+  }
+}
