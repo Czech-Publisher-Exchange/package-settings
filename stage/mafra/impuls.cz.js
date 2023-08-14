82caeda2895998fa2638998358f10958c463acb3
CPEx Dev <dev@cpex.cz>
2023-08-14 10:59:03 +0200

    Aleš: úprava skinu pro impuls

--- stage/mafra/impuls.cz.js
+++ stage/mafra/impuls.cz.js
@@ -334,7 +334,7 @@ window.cpexWebsiteSettings = {
       backgroundEl: /*S*/ document.getElementById('main') /*E*/ ,
       contentEl: /*S*/ document.getElementsByClassName('m-bg-4')[0] /*E*/ ,
       contentCSS: ' ',
-      offset: 50
+      generalCSS: '.r-leaderboardhp {\n  height: 200px\n}'
     }
   }
 }
