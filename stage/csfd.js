c2c33e60b8c17329b14a197cade8140de4ef8e9e
CPEx Dev <dev@cpex.cz>
2023-08-18 18:07:05 +0200

    David: Update to cookie reading

--- stage/csfd.js
+++ stage/csfd.js
@@ -27,12 +27,20 @@ window.cpexPublisherSettings = {
     errorApiKey: 'hbp_tCjxz6JmkE7RI1QnA7qLBjDFb4ItP23y9OCs',
     onLoad: /*S*/ () => {
       window.__tcfapi('addEventListener', 2, (data, success) => {
-        if (success === false) {
-          return;
-        };
+        if (success === false) return;
         if (data.vendor.consents[570] && Didomi.getUserConsentStatusForVendor('c:pomomedia-HZQX3YWL')) {
-          window.dataLayer = window.dataLayer || {};
-          window.dataLayer.seg = window.cpexPackage.getCookie('exc');
+          window.addEventListener('message', (event) => {
+            if (event.origin !== 'https://cdn.cpex.cz') return;
+            if (event.data.type === 'cpexRead') {
+              window.dataLayer = window.dataLayer || {};
+              window.dataLayer.seg = event.data.value
+            }
+          }, false);
+          cpexPackage.utils.addElement('iframe', document.body, {
+            src: 'https://cdn.cpex.cz/cookies/read.html?name=exc&time=1209600&data=' + encodeURIComponent(segments.toString()),
+            width: 0,
+            height: 0
+          })
         }
       })
     } /*E*/
