66613e0fb4ac13cf1722d6a7bdb39bfb07bc673f
CPEx Dev <dev@cpex.cz>
2023-08-18 11:24:38 +0200

    Jana: vytvoření website settings stars24.cz

---
+++ production/incorp/stars24.cz.js
@@ -0,0 +1,20 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'stars24.cz'
+  },
+  adserver: {
+    enabled: false
+  },
+  formats: {
+    skin: {
+      contentEl: /*S*/ document.getElementsByClassName('main__content')[0] /*E*/ ,
+      offset: 60,
+      contentCSS: 'position: relative; margin-top: -26px;'
+    }
+  },
+  aam: {
+    settings: {
+      publisher: 'incorp'
+    }
+  }
+}
