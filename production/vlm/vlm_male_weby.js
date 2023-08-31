1e88a58dd12d0c7cd101f30608228aa4425f61d3
CPEx Dev <dev@cpex.cz>
2023-08-31 14:53:30 +0200

    Iveta: settings pro malé VLM weby bez HB

---
+++ production/vlm/vlm_male_weby.js
@@ -0,0 +1,23 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'vlm_male_weby'
+  },
+  headerbidding: {
+    enabled: false
+  },
+  formats: {
+    interscroller: {
+      enabled: true
+    },
+    slideup: {
+      enabled: true,
+      adUnits: ['m-sticky']
+    },
+    vignette: {
+      enabled: false
+    },
+    responsive: {
+      adUnits: ['m-square-1', 'm-square-2', 'm-square-3', 'm-square-4', 'm-square-5', 'm-square-bottom-1', 'm-square-gallery-1', 'm-square-gallery-2', 'm-square-gallery-3', 'm-square-gallery-4', 'm-square-gallery-5', 'm-square-gallery-6', 'm-square-gallery-7', 'm-square-gallery-8']
+    }
+  }
+}
