ab84c6aa96240a70794663d6ed3f087c3a9495d8
CPEx Dev <dev@cpex.cz>
2023-09-04 11:48:33 +0200

    Frantisek: pridani predavani segmentu z cookie do DL

--- production/csfd.js
+++ production/csfd.js
@@ -24,6 +24,27 @@ window.cpexPublisherSettings = {
     analytics: ['id5Analytics', 'cpexAnalytics']
   },
   general: {
-    errorApiKey: 'hbp_tCjxz6JmkE7RI1QnA7qLBjDFb4ItP23y9OCs'
+    errorApiKey: 'hbp_tCjxz6JmkE7RI1QnA7qLBjDFb4ItP23y9OCs',
+    onLoad: /*S*/ () => {
+      window.__tcfapi('addEventListener', 2, (data, success) => {
+        if (success === false) return;
+        if (data.vendor.consents[570] && Didomi.getUserConsentStatusForVendor('c:pomomedia-HZQX3YWL')) {
+          window.addEventListener('message', (event) => {
+            if (event.origin !== 'https://cdn.cpex.cz') return;
+            if (event.data.type === 'cpexRead') {
+              window.dataLayer = window.dataLayer || [];
+              dataLayer.push({
+                seg: event.data.value
+              })
+            }
+          }, false);
+          cpexPackage.utils.addElement('iframe', document.body, {
+            src: 'https://cdn.cpex.cz/cookies/read.html?name=exc',
+            width: 0,
+            height: 0
+          })
+        }
+      })
+    } /*E*/
   }
 }
