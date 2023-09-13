b790ada2862b4547e4c36fc30c030d19627f50d8
CPEx Dev <dev@cpex.cz>
2023-09-13 08:04:35 +0200

    Markéta: native enabled

--- production/mafra/idnes.cz_auto.js
+++ production/mafra/idnes.cz_auto.js
@@ -2768,6 +2768,28 @@ window.cpexWebsiteSettings = {
           g: 'hb'
         }
       }]
+    }, {
+      code: 'nativ',
+      mediaTypes: {
+        native: {}
+      },
+      bids: [{
+        bidder: 'appnexus',
+        params: {
+          placementId: 30878653
+        }
+      }]
+    }, {
+      code: 'm_nativ',
+      mediaTypes: {
+        native: {}
+      },
+      bids: [{
+        bidder: 'appnexus',
+        params: {
+          placementId: 30878654
+        }
+      }]
     }]
   },
   formats: {
