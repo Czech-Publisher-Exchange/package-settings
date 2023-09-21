e0c48594ae20436e8c752d57f9bf405fb717481f
CPEx Dev <dev@cpex.cz>
2023-09-21 09:04:17 +0200

    Markéta: přidání widesq_clankovy_3

--- production/mafra/arome.cz.js
+++ production/mafra/arome.cz.js
@@ -398,6 +398,44 @@ window.cpexWebsiteSettings = {
           g: 'hb'
         }
       }]
+    }, {
+      code: 'widesquare_clankovy3',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [480, 300],
+            [300, 300],
+            [250, 250],
+            [300, 250],
+            [480, 480]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 144582,
+          zoneId: 3039000,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83, 48, 14, 15, 261]
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1038407
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 30850660
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172
+        }
+      }]
     }]
   },
   formats: {
