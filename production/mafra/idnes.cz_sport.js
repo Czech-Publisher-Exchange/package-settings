abc858cf92be7369f8a0532001c2cb81ed13c670
CPEx Dev <dev@cpex.cz>
2023-09-21 09:18:40 +0200

    Markéta: přidání widesq_clankovy_3

--- production/mafra/idnes.cz_sport.js
+++ production/mafra/idnes.cz_sport.js
@@ -2849,6 +2849,50 @@ window.cpexWebsiteSettings = {
           placementId: 30878850
         }
       }]
+    }, {
+      code: 'widesquare_clankovy3',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [480, 300],
+            [300, 300],
+            [250, 250],
+            [300, 250],
+            [480, 480]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 144582,
+          zoneId: 3038970,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83, 48, 14, 15, 261]
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '5409055'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1038392
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 30850617
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172
+        }
+      }]
     }]
   },
   formats: {
