c753775f2e21edb13da9c879f97447bcdf5921a5
CPEx Dev <dev@cpex.cz>
2023-09-21 09:05:25 +0200

    Markéta: přidání widesq_clankovy_3

--- production/mafra/expres.cz.js
+++ production/mafra/expres.cz.js
@@ -1930,12 +1930,51 @@ window.cpexWebsiteSettings = {
         }
       },
       bids: [{
+        bidder: 'criteo',
+        params: {
+          networkId: 11172
+        }
+      }, {
         bidder: 'r2b2',
         params: {
           d: 'expres.cz',
           p: '300x250',
           m: 0,
           g: 'hb'
+        }
+      }]
+    }, {
+      code: 'widesquare_clankovy3',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [480, 300],
+            [300, 300],
+            [250, 250],
+            [300, 250],
+            [480, 480]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 144582,
+          zoneId: 3039002,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83, 48, 14, 15, 261]
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '5409071'
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172
         }
       }]
     }]
