f2f665bcfc866208cf79954fb147de5e5685a4e1
CPEx Dev <dev@cpex.cz>
2023-09-21 14:41:06 +0200

    Aleš: Příprava pro extrasimo.cz

---
+++ production/incorp/extrasimo.cz.js
@@ -0,0 +1,107 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'extrasimo.cz'
+  },
+  adserver: {
+    enabled: true,
+    adapter: 'googletag',
+    loadPrerequisites: true,
+    defineSlots: [{
+      elementId: 'dhp-1-2000x1400',
+      sizes: [
+        [998, 200],
+        [970, 210],
+        [2000, 1400]
+      ],
+      path: '/22996079844/extrasimo.cz_dhp-1-2000x1400'
+    }, {
+      elementId: 'dsa-1-2000x1400',
+      sizes: [
+        [998, 200],
+        [970, 210],
+        [2000, 1400]
+      ],
+      path: '/22996079844/extrasimo.cz_dsa-1-2000x1400'
+    }],
+    gamId: '22996079844',
+    gamPrefix: 'extrasimo.cz_'
+  },
+  headerbidding: {
+    adUnits: [{
+      code: 'dhp-1-2000x1400',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [998, 200],
+            [970, 210],
+            [2000, 1400]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 363124,
+          zoneId: 3046826,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [259, 96, 230]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 363126,
+          zoneId: 3046828,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [259, 96, 230]
+        },
+        bidderModuleName: 'rubicon'
+      }],
+      filter: {
+        minWidth: 750
+      }
+    }, {
+      code: 'dsa-1-2000x1400',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [998, 200],
+            [970, 210],
+            [2000, 1400]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 363124,
+          zoneId: 3046826,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [259, 96, 230]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 363126,
+          zoneId: 3046828,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [259, 96, 230]
+        },
+        bidderModuleName: 'rubicon'
+      }],
+      filter: {
+        minWidth: 750
+      }
+    }]
+  },
+  formats: {
+    skin: {
+      enabled: true,
+      contentEl: /*S*/ document.getElementsByClassName('main__content')[0] /*E*/ ,
+      offset: 60
+    }
+  }
+}
