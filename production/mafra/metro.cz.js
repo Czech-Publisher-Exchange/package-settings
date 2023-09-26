08fd79142ca8302ec0ec4fe89fe1be9a8440c1d2
CPEx Dev <dev@cpex.cz>
2023-09-26 13:00:57 +0200

    Markéta: zapojení webu

--- production/mafra/metro.cz.js
+++ production/mafra/metro.cz.js
@@ -555,7 +555,6 @@ window.cpexWebsiteSettings = {
   },
   formats: {
     skin: {
-      enabled: true,
       contentEl: /*S*/ document.getElementsByClassName('m-bg-4')[0] /*E*/ ,
       backgroundEl: /*S*/ document.getElementById('main') /*E*/ ,
       contentCSS: ' ',
