2b1b5135e880e106d92490919a11a016b487bfc0
CPEx Dev <dev@cpex.cz>
2023-10-05 16:34:58 +0200

    Markéta: Adform native a wide_cl_3

--- production/mafra/idnes.cz_xman.js
+++ production/mafra/idnes.cz_xman.js
@@ -2786,6 +2786,12 @@ window.cpexWebsiteSettings = {
         native: {}
       },
       bids: [{
+        bidder: 'adform',
+        params: {
+          mid: 1782850,
+          rcur: 'USD'
+        }
+      }, {
         bidder: 'appnexus',
         params: {
           placementId: 30878851
@@ -2797,9 +2803,59 @@ window.cpexWebsiteSettings = {
         native: {}
       },
       bids: [{
+        bidder: 'adform',
+        params: {
+          mid: 1782849,
+          rcur: 'USD'
+        }
+      }, {
         bidder: 'appnexus',
         params: {
           placementId: 30878852
+        }
+      }]
+    }, {
+      code: 'widesquare_clankovy3',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [480, 300],
+            [300, 300],
+            [250, 250],
+            [300, 250],
+            [480, 480]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 144582,
+          zoneId: 3038966,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83, 48, 14, 15, 261]
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '5409053'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1038390
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 30850613
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172
         }
       }]
     }]
