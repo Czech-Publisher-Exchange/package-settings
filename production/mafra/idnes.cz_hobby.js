a91f730517a1f5a3cfc6d7a95f278e50466ecbcb
CPEx Dev <dev@cpex.cz>
2023-10-09 11:55:11 +0200

    Markéta: Adform wide_cl_3

--- production/mafra/idnes.cz_hobby.js
+++ production/mafra/idnes.cz_hobby.js
@@ -2843,6 +2843,12 @@ window.cpexWebsiteSettings = {
           adSlot: '5409063'
         }
       }, {
+        bidder: 'adform',
+        params: {
+          mid: 1781497,
+          rcur: 'USD'
+        }
+      }, {
         bidder: 'ix',
         params: {
           siteId: 1038400
