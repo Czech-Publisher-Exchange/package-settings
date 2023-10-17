909177e9b1e42dbbd314fa11c223ebefbb2283c9
CPEx Dev <dev@cpex.cz>
2023-10-17 12:24:51 +0200

    Markéta: wide_cl_3 r2b2

--- production/mafra/idnes.cz_recepty.js
+++ production/mafra/idnes.cz_recepty.js
@@ -2800,6 +2800,14 @@ window.cpexWebsiteSettings = {
         params: {
           networkId: 11172
         }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'recepty.idnes.cz',
+          p: '480x300_clankovy_3',
+          m: 0,
+          g: 'hb'
+        }
       }]
     }]
   },
