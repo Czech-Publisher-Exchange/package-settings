c1fbb9389e7e39b27af8e92aa373cd339d65c473
CPEx Dev <dev@cpex.cz>
2023-10-23 17:16:49 +0200

    Markéta: wide_cl_3 r2b2

--- production/mafra/idnes.cz_hry.js
+++ production/mafra/idnes.cz_hry.js
@@ -2896,6 +2896,14 @@ window.cpexWebsiteSettings = {
         params: {
           networkId: 11172
         }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'bonusweb.idnes.cz',
+          p: '480x300_clankovy_3',
+          m: 0,
+          g: 'hb'
+        }
       }]
     }]
   }
