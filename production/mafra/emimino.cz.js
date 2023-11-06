41067287cf7f9cdd0502b4be2030dfb6c6f16937
CPEx Dev <dev@cpex.cz>
2023-11-06 16:57:16 +0100

    David: Updated m_nativ

--- production/mafra/emimino.cz.js
+++ production/mafra/emimino.cz.js
@@ -1187,8 +1187,8 @@ window.cpexWebsiteSettings = {
     },
     native: {
       templates: {
-        nativ: "<div class='advert-topic'>  <div class='advert-topic'>    <div class='advert-topic__content'> <a rel='sponsored' href='${link}' target='_blank' class='color--default'> <span>${title} - ${desc}</span> </a> </div>    <div class='advert-topic__image'> <a rel='sponsored' href='${link}' target='_blank'>        <picture><img class='image-response' src='${img}'></picture>      </a> </div>  </div></div>",
-        m_nativ: 'nativ'
+        nativ: "<div class='advert-topic'>\n  <div class='advert-topic'>\n    <div class='advert-topic__content'>\n      <a rel='sponsored' href='${link}' target='_blank' class='color--default'><span>${title} - ${desc}</span></a>\n    </div>\n    <div class='advert-topic__image'>\n      <a rel='sponsored' href='${link}' target='_blank'>\n        <picture><img class='image-response' src='${img}'></picture>\n      </a>\n    </div>\n  </div>\n</div>",
+        m_nativ: "<div class='advert-topic'>\n  <div class='advert-topic'>\n    <div class='advert-topic__content'>\n      <a rel='sponsored' href='${link}' target='_blank' class='color--default'><span>${title}</span></a>\n    </div>\n    <div class='advert-topic__image' style='width:50%'>\n      <a rel='sponsored' href='${link}' target='_blank'>\n        <picture><img class='image-response' src='${img}'></picture>\n      </a>\n    </div>\n  </div>\n</div>"
       },
       enabled: true
     }
