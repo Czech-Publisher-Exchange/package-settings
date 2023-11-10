a27afb4043f220d7b094b305b18a965adaf78cac
CPEx Dev <dev@cpex.cz>
2023-11-10 09:17:44 +0100

    Frantisek: uprava native

--- production/csfd.js
+++ production/csfd.js
@@ -44,5 +44,13 @@ window.cpexPublisherSettings = {
         }
       })
     } /*E*/
+  },
+  formats: {
+    native: {
+      enabled: true,
+      templates: {
+        generic: "<style>\n  .cpex-native a {\n    display: flex;\n    text-decoration: none;\n    color: inherit\n  }\n\n  .cpex-native img {\n    width: 200px;\n    object-fit: cover\n  }\n\n  .cpex-native .text {\n    padding-left: 20px\n  }\n</style>\n<div id='native'> <a href='${link}' target='_blank'>\n    <div class='native-content'>\n      <div class='native-content-img'> <img src='${img}' alt=''> </div>\n      <div class='native-content-desc'>\n        <div class='native-content-desc-text'>\n          <p class='native-title'>${title}</p>\n          <p id='nativeText'>${desc}</p>\n        </div>\n        <div class='native-info'> <span class='info'>reklama</span> </div>\n      </div>\n    </div>\n  </a></div>"
+      }
+    }
   }
 }
