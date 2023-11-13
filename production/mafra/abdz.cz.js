1744aefd9fca690f956e2a9b599ad0871b9e3e11
CPEx Dev <dev@cpex.cz>
2023-11-13 11:28:08 +0100

    Markéta: nový web

---
+++ production/mafra/abdz.cz.js
@@ -0,0 +1,18 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'abdz.cz'
+  },
+  formats: {
+    interscroller: {
+      enabled: true
+    },
+    slideup: {
+      enabled: true,
+      adUnits: ['m_sticky']
+    },
+    skin: {
+      generalCSS: '#r-leaderboardhp {  height: 200px;  margin-bottom: 0px;}#page {  z-index:1;}',
+      zIndex: 1
+    }
+  }
+}
