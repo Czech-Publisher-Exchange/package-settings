11e09c7caed890c6d6478bae7284feec99c73b47
CPEx Dev <dev@cpex.cz>
2023-11-13 11:29:33 +0100

    Markéta: nový web

---
+++ production/mafra/autozive.cz.js
@@ -0,0 +1,19 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'autozive.cz'
+  },
+  formats: {
+    interscroller: {
+      enabled: true
+    },
+    slideup: {
+      enabled: true,
+      adUnits: ['m_sticky']
+    },
+    skin: {
+      enabled: true,
+      generalCSS: '#r-leaderboardhp {  height: 200px;  margin-bottom: 0px;}#page {  z-index:1;}',
+      zIndex: 1
+    }
+  }
+}
