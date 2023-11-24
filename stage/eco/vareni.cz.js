ba1422287729e985e81f9ebe81e35daaa46e3a2b
CPEx Dev <dev@cpex.cz>
2023-11-24 14:30:30 +0100

    Frantisek: test skin offset

--- stage/eco/vareni.cz.js
+++ stage/eco/vareni.cz.js
@@ -1,5 +1,654 @@
 window.cpexWebsiteSettings = {
   website: {
     name: 'vareni.cz'
+  },
+  headerbidding: {
+    adUnits: [{
+      code: 'leader',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [2000, 1400],
+            [970, 310],
+            [970, 250],
+            [970, 210],
+            [970, 100],
+            [728, 90]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 361048,
+          zoneId: 1965808,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [230, 94, 57, 96, 95, 2]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 361464,
+          zoneId: 1965810,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [230, 94, 57, 96, 95, 2]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic_eco_hb',
+        params: {
+          publisherId: '161532',
+          adSlot: '4344939'
+        },
+        bidderModuleName: 'pubmatic'
+      }, {
+        bidder: 'adform_im_hb',
+        params: {
+          mid: 1334484,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform_im_hb-mask',
+        params: {
+          mid: 1334488,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1109659,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 759661
+        }
+      }, {
+        bidder: 'teads',
+        params: {
+          placementId: 158423,
+          pageId: 144531
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 21011671
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'ECO_varenicz_leader'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'vareni.cz',
+          p: '728x90',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'megaboard',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [970, 310],
+            [970, 250],
+            [970, 210],
+            [970, 100],
+            [728, 90]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 361048,
+          zoneId: 1965820,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [94, 57, 96, 95, 2]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 361464,
+          zoneId: 1965822,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [94, 57, 96, 95, 2]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic_eco_hb',
+        params: {
+          publisherId: '161532',
+          adSlot: '4344940'
+        },
+        bidderModuleName: 'pubmatic'
+      }, {
+        bidder: 'adform_im_hb',
+        params: {
+          mid: 1334499,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform_im_hb-mask',
+        params: {
+          mid: 1334501,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1109665,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 759665
+        }
+      }, {
+        bidder: 'teads',
+        params: {
+          placementId: 158424,
+          pageId: 144532
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 21011717
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'ECO_varenicz_megaboard'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'vareni.cz',
+          p: '970x210',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'wallpaper-1',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [480, 300],
+            [300, 300],
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 361048,
+          zoneId: 1965824,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83, 48, 15]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 361464,
+          zoneId: 1965826,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83, 48, 15]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic_eco_hb',
+        params: {
+          publisherId: '161532',
+          adSlot: '4344941'
+        },
+        bidderModuleName: 'pubmatic'
+      }, {
+        bidder: 'adform_knwl_hb',
+        params: {
+          mid: 1693298,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform_im_hb',
+        params: {
+          mid: 1334495,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform_im_hb-mask',
+        params: {
+          mid: 1334496,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1109660,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 759667
+        }
+      }, {
+        bidder: 'teads',
+        params: {
+          placementId: 153680,
+          pageId: 140016
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 21011721
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'ECO_varenicz_wallpaper_1'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'vareni.cz',
+          p: '480x300_pos1',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'wallpaper-x',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [480, 300],
+            [300, 300],
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 361048,
+          zoneId: 1965828,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83, 48, 15]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 361464,
+          zoneId: 1965830,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83, 48, 15]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic_eco_hb',
+        params: {
+          publisherId: '161532',
+          adSlot: '4344942'
+        },
+        bidderModuleName: 'pubmatic'
+      }, {
+        bidder: 'adform_knwl_hb',
+        params: {
+          mid: 1693299,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform_im_hb',
+        params: {
+          mid: 1334497,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform_im_hb-mask',
+        params: {
+          mid: 1334498,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1109662,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 759668
+        }
+      }, {
+        bidder: 'teads',
+        params: {
+          placementId: 158428,
+          pageId: 144536
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 21011729
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'ECO_varenicz_wallpaper_x'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'vareni.cz',
+          p: '480x300_pos2',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'halfpage-1',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [160, 600],
+            [300, 300],
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 361048,
+          zoneId: 1958638,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 9, 48, 15]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 361464,
+          zoneId: 1958640,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 9, 48, 15]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic_eco_hb',
+        params: {
+          publisherId: '161532',
+          adSlot: '4344937'
+        },
+        bidderModuleName: 'pubmatic'
+      }, {
+        bidder: 'adform_knwl_hb',
+        params: {
+          mid: 1693300,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform_im_hb',
+        params: {
+          mid: 1334489,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform_im_hb-mask',
+        params: {
+          mid: 1334490,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1109663,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 759659
+        }
+      }, {
+        bidder: 'teads',
+        params: {
+          placementId: 153681,
+          pageId: 140017
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 21010492
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'ECO_varenicz_halfpage_1'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'vareni.cz',
+          p: '300x600_pos1',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'halfpage-2',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [160, 600],
+            [300, 300],
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 361048,
+          zoneId: 1958642,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 9, 48, 15]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 361464,
+          zoneId: 1958644,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 9, 48, 15]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic_eco_hb',
+        params: {
+          publisherId: '161532',
+          adSlot: '4344938'
+        },
+        bidderModuleName: 'pubmatic'
+      }, {
+        bidder: 'adform_knwl_hb',
+        params: {
+          mid: 1693301,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform_im_hb',
+        params: {
+          mid: 1334491,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform_im_hb-mask',
+        params: {
+          mid: 1334494,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1109664,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 759660
+        }
+      }, {
+        bidder: 'teads',
+        params: {
+          placementId: 158426,
+          pageId: 144534
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 21011627
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'ECO_varenicz_halfpage_1'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'vareni.cz',
+          p: '300x600_pos2',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'popup',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [500, 200]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 361516,
+          zoneId: 2669022,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 258]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 361522,
+          zoneId: 2669024,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 258]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 28273058
+        }
+      }]
+    }],
+    currency: 'USD'
+  },
+  formats: {
+    skin: {
+      enabled: true,
+      contentEl: /*S*/ document.getElementsByClassName('bhWtgq')[0] /*E*/ ,
+      contentCSS: ' ',
+      zIndex: null,
+      backgroundEl: /*S*/ document.getElementById('brand-c') /*E*/
+    },
+    interscroller: {
+      enabled: true
+    },
+    slideup: {
+      adUnits: ['sas-special'],
+      enabled: true,
+      backgroundEl: /*S*/ document.getElementById('sas-special-wrapper') /*E*/
+    }
+  },
+  general: {
+    errorPath: 'https://73f2bd72d0d2477ab2f976d6098fe246@o530000.ingest.sentry.io/4504531846365184',
+    onLoad: /*S*/ dynamické nastavení offsetu v settings do general.onLoad: () => {
+      window.cpexPackage.settings.format.skin.offset = window._ecohec ? (window._ecohec.desktopNavigationHeight || 0) : 0
+    } /*E*/
   }
 }
