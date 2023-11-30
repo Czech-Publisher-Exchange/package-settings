b53d375eb9a65a7afd65033dbfe5934c900d6e4c
CPEx Dev <dev@cpex.cz>
2023-11-30 08:00:37 +0100

    Aleš: úprava slideup z-index na 999

--- stage/mafra.js
+++ stage/mafra.js
@@ -21,7 +21,8 @@ window.cpexPublisherSettings = {
       'omg-adform': 'OMG-Adform_HB',
       'fragile-adform': 'Fragile-Adform_HB',
       criteo: 'Criteo_HB',
-      teads: 'Teads_HB'
+      teads: 'Teads_HB',
+      invibes: 'Invibes_HB'
     }
   },
   headerbidding: {
@@ -47,14 +48,13 @@ window.cpexPublisherSettings = {
   formats: {
     slideup: {
       closeTextHTML: 'zavřít reklamu',
-      css: '#cpex-slideup {  background-color: transparent;  z-index: 9999;}#cpex-slideup-close {  box-sizing: border-box;  display: inline-block;  cursor: pointer;  position: absolute;  top: -32px;  right: 0px;  height: 32px;  padding: 7px 9px;  font-family: Arial, sans-serif;  background-color: #454545;  font-size: 14px;  color: #fff;}#cpex-slideup-close:hover {  background-color: #000;}'
+      css: '#cpex-slideup {\n  background-color: transparent;\n  z-index: 999;\n}\n\n#cpex-slideup-close {\n  box-sizing: border-box;\n  display: inline-block;\n  cursor: pointer;\n  position: absolute;\n  top: -32px;\n  right: 0px;\n  height: 32px;\n  padding: 7px 9px;\n  font-family: Arial, sans-serif;\n  background-color: #454545;\n  font-size: 14px;\n  color: #fff;\n}\n\n#cpex-slideup-close:hover {\n  background-color: #000;\n}'
     },
     responsive: {
       enabled: true,
       adUnits: ['m_branding', 'm_branding_foto', 'm_branding_foto2', 'm_300x250', 'm_300x250_clankovy', 'm_300x250_clankovy_opak1', 'm_300x250_clankovy_opak2', 'm_300x250_foto_end', 'm_300x250_foto_mid', 'm_300x250_foto_mid2', 'm_300x250_foto_start', 'm_300x250_infinite'],
       width: 480,
-      height: 480,
-      containerEl: /*S*/ document.getElementById('text-out-box') /*E*/
+      height: 480
     },
     vignette: {
       css: "#cpex-vignette {    background-color: #000E;    z-index: 2147483642;}#cpex-vignette-close {    position: absolute;    z-index: 999;    right: 20px;    top: 10px;    box-sizing: border-box;    display: inline-block;    cursor: pointer;    width: 30px;    height: 30px;    padding: 10px;    font-size: 0pt;    border: none;    background-image: url('https://trackad.cz/icon.php?icon=closer2&front=cacaca&back=000000');    background-size: contain;    border-radius: 30px;    box-shadow: 0 0 9px 3px #cacaca;}#cpex-vignette-header {    position: absolute;    right: 10px;    bottom: 20px;    font-size: 12px;    background: rgba`40, 40, 40, 0.7`;    color: #d1d1d1;    padding: 1px 3px;    text-transform: capitalize;}",
@@ -62,10 +62,9 @@ window.cpexPublisherSettings = {
     },
     native: {
       templates: {
-        nativ: "<div class='art'> <a href='${link}' target='_blank' class='art-link'>\n    <h3>${title}</h3>\n    <div class='art-img w230'> <img src='${img}'> <span></span> </div>\n  </a>\n  <p class='perex'>${desc}</p>\n</div>",
-        m_nativ: "<div class='art'> <a rel='sponsored' href='${link}' class='art-link' target='_blank'> <img title='' alt='' src='${img}' class='art-img sp5 ' width='100' height='75'>\n    <h3>${title}</h3>\n  </a></div>"
-      },
-      enabled: true
+        nativ: "<div class='art'> <a href='${link}' target='_blank' class='art-link'>    <h3>${title}</h3>    <div class='art-img w230'><img src='${img}' style='object-fit:cover'><span></span></div>    <p class='perex'>${desc}</p>  </a></div>",
+        m_nativ: "<div class='art'>  <a rel='sponsored' href='${link}' class='art-link' target='_blank'>    <img title='' alt='' src='${img}' class='art-img sp5 ' style='object-fit:cover' width='100' height='75'>    <h3 style='text-align:left'>${title}</h3>  </a></div>"
+      }
     }
   }
 }
