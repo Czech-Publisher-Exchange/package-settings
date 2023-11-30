818b24de3a6e93e58441e40be9f50efaa82c9a50
CPEx Dev <dev@cpex.cz>
2023-11-30 12:23:36 +0100

    Markéta: nový web

--- production/mafra/adbz.cz.js
+++ production/mafra/adbz.cz.js
@@ -1,5 +1,102 @@
 window.cpexWebsiteSettings = {
   website: {
     name: 'adbz.cz'
+  },
+  headerbidding: {
+    adUnits: [{
+      code: 'branding',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [998, 200],
+            [728, 90],
+            [970, 210],
+            [970, 90]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 144582,
+          zoneId: 3134316,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [259, 2, 96, 55]
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1815172,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1053063
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 31440172
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'mafra.adbz.cz',
+          p: 'branding',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'm_branding',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [250, 250],
+            [480, 820],
+            [300, 600],
+            [768, 1230],
+            [480, 480]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 157330,
+          zoneId: 3134318,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 14, 256, 10, 261]
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1815174,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1053064
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 31440170
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'mafra.adbz.cz',
+          p: 'interscroller',
+          m: 1,
+          g: 'hb'
+        }
+      }]
+    }]
   }
 }
