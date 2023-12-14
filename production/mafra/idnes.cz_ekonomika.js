dfbba7be468096e4b22ccc8d92d7afddb27338ac
CPEx Dev <dev@cpex.cz>
2023-12-14 09:02:01 +0100

    Markéta: Pub a IX do m_sticky

--- production/mafra/idnes.cz_ekonomika.js
+++ production/mafra/idnes.cz_ekonomika.js
@@ -1152,6 +1152,12 @@ window.cpexWebsiteSettings = {
         },
         bidderModuleName: 'rubicon'
       }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '5604802'
+        }
+      }, {
         bidder: 'adform',
         params: {
           mid: 1634345,
@@ -1171,6 +1177,11 @@ window.cpexWebsiteSettings = {
           rcur: 'USD'
         },
         bidderModuleName: 'adform'
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1064294
+        }
       }, {
         bidder: 'teads',
         params: {
@@ -2945,7 +2956,17 @@ window.cpexWebsiteSettings = {
       adUnits: ['m_vignete']
     },
     native: {
-      enabled: true
+      enabled: true,
+      adUnitConfig: {
+        nativ: {
+          titleLength: 56,
+          descLength: 133
+        },
+        m_nativ: {
+          titleLength: 56,
+          descLength: 133
+        }
+      }
     }
   }
 }
