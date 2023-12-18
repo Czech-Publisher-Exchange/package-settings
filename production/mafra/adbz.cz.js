6703d6979e6a56d08bbfcbd4f3ed8f798c0bbcdb
CPEx Dev <dev@cpex.cz>
2023-12-18 11:27:26 +0100

    Markéta: odebrání branding pozice

--- production/mafra/adbz.cz.js
+++ production/mafra/adbz.cz.js
@@ -4,52 +4,6 @@ window.cpexWebsiteSettings = {
   },
   headerbidding: {
     adUnits: [{
-      code: 'branding',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [998, 200],
-            [728, 90],
-            [970, 210],
-            [970, 90]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'rubicon',
-        params: {
-          siteId: 144582,
-          zoneId: 3134316,
-          accountId: '10900',
-          position: 'atf',
-          sizes: [259, 2, 96, 55]
-        }
-      }, {
-        bidder: 'adform',
-        params: {
-          mid: 1815172,
-          rcur: 'USD'
-        }
-      }, {
-        bidder: 'ix',
-        params: {
-          siteId: 1053063
-        }
-      }, {
-        bidder: 'appnexus',
-        params: {
-          placementId: 31440172
-        }
-      }, {
-        bidder: 'r2b2',
-        params: {
-          d: 'mafra.adbz.cz',
-          p: 'branding',
-          m: 0,
-          g: 'hb'
-        }
-      }]
-    }, {
       code: 'm_branding',
       mediaTypes: {
         banner: {
