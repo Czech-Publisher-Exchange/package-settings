57070f85756852e988707283bd1acd4d042eeb3d
CPEx Dev <dev@cpex.cz>
2024-01-02 15:21:46 +0100

    Markéta: náhrada IDs Teads

--- production/mafra/idos.idnes.cz.js
+++ production/mafra/idos.idnes.cz.js
@@ -225,8 +225,8 @@ window.cpexWebsiteSettings = {
       }, {
         bidder: 'teads',
         params: {
-          placementId: 185860,
-          pageId: 170969
+          placementId: 211349,
+          pageId: 195815
         }
       }, {
         bidder: 'appnexus',
@@ -391,8 +391,8 @@ window.cpexWebsiteSettings = {
       }, {
         bidder: 'teads',
         params: {
-          placementId: 144326,
-          pageId: 131425
+          placementId: 211358,
+          pageId: 195824
         }
       }, {
         bidder: 'appnexus',
@@ -478,8 +478,8 @@ window.cpexWebsiteSettings = {
       }, {
         bidder: 'teads',
         params: {
-          placementId: 144326,
-          pageId: 131425
+          placementId: 211358,
+          pageId: 195824
         }
       }, {
         bidder: 'appnexus',
@@ -557,8 +557,8 @@ window.cpexWebsiteSettings = {
       }, {
         bidder: 'teads',
         params: {
-          placementId: 144326,
-          pageId: 131425
+          placementId: 211358,
+          pageId: 195824
         }
       }, {
         bidder: 'appnexus',
