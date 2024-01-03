081932a8485f5172e19e8c283b4fb534a0d1ceb9
CPEx Dev <dev@cpex.cz>
2024-01-03 11:24:08 +0100

    Markéta: založení settings

---
+++ production/mafra/udalosti247.cz.js
@@ -0,0 +1,21 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'udalosti247.cz'
+  },
+  headerbidding: {
+    adUnits: [{
+      code: 'branding',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [998, 200],
+            [728, 90],
+            [970, 210],
+            [970, 90]
+          ]
+        }
+      },
+      bids: []
+    }]
+  }
+}
