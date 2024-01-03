8ea4d44211577d3f6c47ff8e83b8e088166bd687
CPEx Dev <dev@cpex.cz>
2024-01-03 11:21:07 +0100

    Markéta: založení settings

---
+++ production/mafra/armadnizpravodaj.cz.js
@@ -0,0 +1,21 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'armadnizpravodaj.cz'
+  },
+  headerbidding: {
+    adUnits: [{
+      code: 'branding',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [998, 200],
+            [728, 90],
+            [970, 210],
+            [970, 90]
+          ]
+        }
+      },
+      bids: []
+    }]
+  }
+}
