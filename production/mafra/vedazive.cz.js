62fe6120bbd13449d6e8389a987b757e7b401ff7
CPEx Dev <dev@cpex.cz>
2024-01-10 12:08:42 +0100

    Markéta: přidání bidderů

--- production/mafra/vedazive.cz.js
+++ production/mafra/vedazive.cz.js
@@ -15,7 +15,34 @@ window.cpexWebsiteSettings = {
           ]
         }
       },
-      bids: []
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 144582,
+          zoneId: 3178136,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [259, 2, 96, 55]
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 31700236
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'mafra.vedazive.cz',
+          p: 'branding',
+          m: 0,
+          g: 'hb'
+        }
+      }]
     }]
   }
 }
