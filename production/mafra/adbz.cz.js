6df05ff3a0e591c82605d1aae9cb61703c27650b
CPEx Dev <dev@cpex.cz>
2024-01-10 12:09:44 +0100

    Markéta: přidání bidderů

--- production/mafra/adbz.cz.js
+++ production/mafra/adbz.cz.js
@@ -4,50 +4,53 @@ window.cpexWebsiteSettings = {
   },
   headerbidding: {
     adUnits: [{
-      code: 'm_branding',
+      code: 'branding',
       mediaTypes: {
         banner: {
           sizes: [
-            [300, 250],
-            [250, 250],
-            [480, 820],
-            [300, 600],
-            [768, 1230],
-            [480, 480]
+            [998, 200],
+            [728, 90],
+            [970, 210],
+            [970, 90]
           ]
         }
       },
       bids: [{
         bidder: 'rubicon',
         params: {
-          siteId: 157330,
-          zoneId: 3134318,
+          siteId: 144582,
+          zoneId: 3134316,
           accountId: '10900',
           position: 'atf',
-          sizes: [15, 14, 256, 10, 261]
+          sizes: [259, 2, 96, 55]
         }
       }, {
         bidder: 'adform',
         params: {
-          mid: 1815174,
+          mid: 1815172,
           rcur: 'USD'
         }
       }, {
         bidder: 'ix',
         params: {
-          siteId: 1053064
+          siteId: 1053063
         }
       }, {
         bidder: 'appnexus',
         params: {
-          placementId: 31440170
+          placementId: 31440172
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172
         }
       }, {
         bidder: 'r2b2',
         params: {
           d: 'mafra.adbz.cz',
-          p: 'interscroller',
-          m: 1,
+          p: 'branding',
+          m: 0,
           g: 'hb'
         }
       }]
