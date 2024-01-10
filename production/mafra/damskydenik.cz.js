4c4365abca67919f5a99910631214bf75b6acdf4
CPEx Dev <dev@cpex.cz>
2024-01-10 12:07:25 +0100

    Markéta: přidání bidderů

--- production/mafra/damskydenik.cz.js
+++ production/mafra/damskydenik.cz.js
@@ -15,7 +15,45 @@ window.cpexWebsiteSettings = {
           ]
         }
       },
-      bids: []
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 144582,
+          zoneId: 3178140,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [259, 2, 96, 55]
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '5652822'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1065813
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 31700237
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'mafra.damskydenik.cz',
+          p: 'branding',
+          m: 0,
+          g: 'hb'
+        }
+      }]
     }]
   }
 }
