7261cc3258f36209d84ea17490859df6f0bd3cb9
CPEx Dev <dev@cpex.cz>
2024-01-15 15:58:04 +0100

    Frantisek: uprava offset

--- production/eco/zena.aktualne.cz.js
+++ production/eco/zena.aktualne.cz.js
@@ -924,9 +924,7 @@ window.cpexWebsiteSettings = {
     skin: {
       enabled: true,
       contentEl: /*S*/ document.getElementsByClassName('page')[0] /*E*/ ,
-      offset: /*S*/ () => {
-        return 64
-      } /*E*/ ,
+      offset: /*S*/ 64 /*E*/ ,
       zIndex: 0,
       contentCSS: 'z-index:2; margin-top: 200px',
       hide: [ /*S*/ document.getElementsByClassName('leaderboard')[0] /*E*/ ]
