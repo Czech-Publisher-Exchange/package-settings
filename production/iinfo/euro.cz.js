2affcd65fb32eb2521298f9dc0b567c52a0c92c6
CPEx Dev <dev@cpex.cz>
2024-01-18 09:51:01 +0100

    Aleš: Zapnutí dynamického skin offsetu

--- production/iinfo/euro.cz.js
+++ production/iinfo/euro.cz.js
@@ -434,8 +434,8 @@ window.cpexWebsiteSettings = {
       enabled: true,
       contentEl: /*S*/ document.getElementsByClassName('parter-cpex-branding-wrapper')[0] /*E*/ ,
       contentCSS: 'width: 1060px; margin: 200px auto; position: relative;',
-      hide: [ /*S*/ document.getElementsByClassName('js-advert-position-mark-leaderboard js-advert')[0] /*E*/, /*S*/ document.getElementsByClassName('leaderboard-dynamic-height-wrapper')[0] /*E*/ ],
-      offset: 54
+      hide: [ /*S*/ document.getElementsByClassName('js-advert-position-mark-leaderboard js-advert')[0] /*E*/ , /*S*/ document.getElementsByClassName('leaderboard-dynamic-height-wrapper')[0] /*E*/ ],
+      offset: /*S*/ () => document.getElementsByClassName('js_o-header-bar')[0]?.offsetHeight ?? 0 /*E*/
     }
   }
 }
