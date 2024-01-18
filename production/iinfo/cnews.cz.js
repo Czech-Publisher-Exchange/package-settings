ef6e16c037f26522e3b7e566c1ffe2cd8635331b
CPEx Dev <dev@cpex.cz>
2024-01-18 09:40:29 +0100

    Aleš: Vypnutí marginu u skin

--- production/iinfo/cnews.cz.js
+++ production/iinfo/cnews.cz.js
@@ -290,7 +290,7 @@ window.cpexWebsiteSettings = {
     skin: {
       enabled: true,
       contentEl: /*S*/ document.getElementsByClassName('parter-cpex-branding-wrapper')[0] /*E*/ ,
-      contentCSS: 'width: 1060px; margin: 200px auto;'
+      contentCSS: 'width: 1060px;'
     }
   },
   general: {
