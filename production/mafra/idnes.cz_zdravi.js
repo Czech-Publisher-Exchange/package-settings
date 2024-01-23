d0a0076f3a47a43285e5cc12f24d0dbdd02ff485
CPEx Dev <dev@cpex.cz>
2024-01-23 10:40:54 +0100

    Markéta: přidání Fragile m_vignete

--- production/mafra/idnes.cz_zdravi.js
+++ production/mafra/idnes.cz_zdravi.js
@@ -2518,6 +2518,13 @@ window.cpexWebsiteSettings = {
         },
         bidderModuleName: 'adform'
       }, {
+        bidder: 'fragile-adform',
+        params: {
+          mid: 1841874,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
         bidder: 'ix',
         params: {
           siteId: 958829
