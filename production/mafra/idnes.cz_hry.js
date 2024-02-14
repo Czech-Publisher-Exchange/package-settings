9b6ebd8411d021c62ea8285fddfe9bebc1ae6aa0
CPEx Dev <dev@cpex.cz>
2024-02-14 10:20:27 +0100

    Markéta: přidání m_sticky_foto

--- production/mafra/idnes.cz_hry.js
+++ production/mafra/idnes.cz_hry.js
@@ -12,7 +12,7 @@ window.cpexWebsiteSettings = {
     },
     slideup: {
       enabled: true,
-      adUnits: ['m_sticky']
+      adUnits: ['m_sticky', 'm_sticky_foto']
     },
     vignette: {
       enabled: true,
@@ -2933,6 +2933,60 @@ window.cpexWebsiteSettings = {
           p: '480x300_clankovy_3',
           m: 0,
           g: 'hb'
+        }
+      }]
+    }, {
+      code: 'm_sticky_foto',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [320, 150],
+            [300, 250],
+            [500, 200]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 157330,
+          zoneId: 3210332,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [60, 15, 258]
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '5697974'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1848888,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1072424
+        }
+      }, {
+        bidder: 'teads',
+        params: {
+          placementId: 211358,
+          pageId: 195824
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 31818876
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172
         }
       }]
     }]
