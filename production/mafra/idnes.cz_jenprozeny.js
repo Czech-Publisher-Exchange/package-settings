4143742cc273d853ee4a64d2ade586174ca946f2
CPEx Dev <dev@cpex.cz>
2024-02-14 10:21:27 +0100

    Markéta: přidání m_sticky_foto

--- production/mafra/idnes.cz_jenprozeny.js
+++ production/mafra/idnes.cz_jenprozeny.js
@@ -740,6 +740,55 @@ window.cpexWebsiteSettings = {
           networkId: 11172
         }
       }]
+    }, {
+      code: 'm_sticky_foto',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [320, 150],
+            [300, 250],
+            [500, 200]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 157330,
+          zoneId: 3210348,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [60, 15, 258]
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '5697982'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1849417,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'teads',
+        params: {
+          placementId: 211324,
+          pageId: 195790
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 31818890
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172
+        }
+      }]
     }]
   },
   formats: {
@@ -755,7 +804,7 @@ window.cpexWebsiteSettings = {
     },
     slideup: {
       enabled: true,
-      adUnits: ['m_sticky']
+      adUnits: ['m_sticky', 'm_sticky_foto']
     }
   }
 }
