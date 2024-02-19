70fec46548dee630c3bb54b4b24eafa500d0edf4
CPEx Dev <dev@cpex.cz>
2024-02-19 16:14:35 +0100

    Frantisek: hb vignete - navrat zpet po testovani

--- production/mafra/idnes.cz_mobil.js
+++ production/mafra/idnes.cz_mobil.js
@@ -2484,7 +2484,9 @@ window.cpexWebsiteSettings = {
       mediaTypes: {
         banner: {
           sizes: [
-            [480, 820]
+            [720, 1280],
+            [480, 820],
+            [300, 600]
           ]
         }
       },
@@ -2495,7 +2497,7 @@ window.cpexWebsiteSettings = {
           zoneId: 2795938,
           accountId: '10900',
           position: 'atf',
-          sizes: [256]
+          sizes: [256, 10]
         }
       }, {
         bidder: 'rubicon-mask',
@@ -2504,7 +2506,7 @@ window.cpexWebsiteSettings = {
           zoneId: 2800870,
           accountId: '10900',
           position: 'atf',
-          sizes: [256]
+          sizes: [256, 10]
         },
         bidderModuleName: 'rubicon'
       }, {
