e608772d8ebedf9928f1ea7e133b0e017ac64934
CPEx Dev <dev@cpex.cz>
2024-03-05 10:30:37 +0100

    Markéta: doplnění Magnite mask

--- production/mafra/idnes.cz_technet.js
+++ production/mafra/idnes.cz_technet.js
@@ -2509,7 +2509,7 @@ window.cpexWebsiteSettings = {
           zoneId: 2795928,
           accountId: '10900',
           position: 'atf',
-          sizes: [256, 10]
+          sizes: [484, 256, 10]
         }
       }, {
         bidder: 'rubicon-mask',
@@ -2518,7 +2518,7 @@ window.cpexWebsiteSettings = {
           zoneId: 2800858,
           accountId: '10900',
           position: 'atf',
-          sizes: [256, 10]
+          sizes: [484, 256, 10]
         },
         bidderModuleName: 'rubicon'
       }, {
@@ -2590,6 +2590,16 @@ window.cpexWebsiteSettings = {
           position: 'atf',
           sizes: [10, 48, 15, 9]
         }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 144584,
+          zoneId: 3275012,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 48, 15, 9]
+        },
+        bidderModuleName: 'rubicon'
       }, {
         bidder: 'pubmatic',
         params: {
@@ -2854,6 +2864,16 @@ window.cpexWebsiteSettings = {
           position: 'atf',
           sizes: [83, 48, 14, 15, 261]
         }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 144584,
+          zoneId: 3274982,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83, 48, 14, 15, 261]
+        },
+        bidderModuleName: 'rubicon'
       }, {
         bidder: 'pubmatic',
         params: {
