456cacd129585f1dcc4ca2b9e5676b0c10a87aab
CPEx Dev <dev@cpex.cz>
2024-03-05 11:51:34 +0100

    Markéta: přidání Magnite mask

--- production/mafra/idnes.cz_kultura.js
+++ production/mafra/idnes.cz_kultura.js
@@ -2729,6 +2729,16 @@ window.cpexWebsiteSettings = {
           sizes: [10, 48, 15, 9]
         }
       }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 144584,
+          zoneId: 3275242,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 48, 15, 9]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
         bidder: 'pubmatic',
         params: {
           publisherId: '158732',
