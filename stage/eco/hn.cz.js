17084058ea59a56951df99c74f925914d74d6b4c
CPEx Dev <dev@cpex.cz>
2024-03-06 22:16:49 +0100

    Frantisek: test magnite piano

---
+++ stage/eco/hn.cz.js
@@ -0,0 +1,456 @@
+window.cpexWebsiteSettings = {
+  headerbidding: {
+    adUnits: [{
+      code: 'leader',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [970, 250],
+            [970, 210],
+            [970, 100],
+            [728, 90]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 401314,
+          zoneId: 2250698,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [57, 96, 95, 2]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 401308,
+          zoneId: 2250696,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [57, 96, 95, 2]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic_eco_hb',
+        params: {
+          publisherId: '161532',
+          adSlot: '4344918'
+        },
+        bidderModuleName: 'pubmatic'
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1219807,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 765419
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 23566747
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'ECO_hncz_leader'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'ihned.cz',
+          p: '728x90',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'halfpage-1',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [160, 600],
+            [300, 300],
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 401314,
+          zoneId: 2250714,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 9, 48, 15]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 401308,
+          zoneId: 2250712,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 9, 48, 15]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic_eco_hb',
+        params: {
+          publisherId: '161532',
+          adSlot: '4344916'
+        },
+        bidderModuleName: 'pubmatic'
+      }, {
+        bidder: 'knowlimits-adform',
+        params: {
+          mid: 1840860,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'omg-adform',
+        params: {
+          mid: 1869817,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1219808,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 763333
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 23566750
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'ECO_hncz_halfpage_1'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'ihned.cz',
+          p: '300x600_1',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'halfpage-2',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [160, 600],
+            [300, 300],
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 401314,
+          zoneId: 2250718,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 9, 48, 15]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 401308,
+          zoneId: 2250716,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 9, 48, 15]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic_eco_hb',
+        params: {
+          publisherId: '161532',
+          adSlot: '4344917'
+        },
+        bidderModuleName: 'pubmatic'
+      }, {
+        bidder: 'knowlimits-adform',
+        params: {
+          mid: 1840861,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'omg-adform',
+        params: {
+          mid: 1869818,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1219809,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 765418
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 23566751
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'ECO_hncz_halfpage_2'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'ihned.cz',
+          p: '300x600_2',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'wallpaper',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [480, 300],
+            [300, 300],
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 401314,
+          zoneId: 2250710,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83, 48, 15]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 401308,
+          zoneId: 2250708,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83, 48, 15]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic_eco_hb',
+        params: {
+          publisherId: '161532',
+          adSlot: '4344920'
+        },
+        bidderModuleName: 'pubmatic'
+      }, {
+        bidder: 'knowlimits-adform',
+        params: {
+          mid: 1840862,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'omg-adform',
+        params: {
+          mid: 1869819,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1219810,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 765425
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 23566749
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'ECO_hncz_wallpaper'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'ihned.cz',
+          p: '480x300',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'megaboard',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [970, 310],
+            [970, 250],
+            [970, 210],
+            [728, 90]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 401314,
+          zoneId: 2250706,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [94, 57, 96, 2]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 401308,
+          zoneId: 2250704,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [94, 57, 96, 2]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic_eco_hb',
+        params: {
+          publisherId: '161532',
+          adSlot: '4251419'
+        },
+        bidderModuleName: 'pubmatic'
+      }, {
+        bidder: 'knowlimits-adform',
+        params: {
+          mid: 1840863,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'omg-adform',
+        params: {
+          mid: 1869820,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1219811,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 765423
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 23566748
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'ECO_hncz_megaboard'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'ihned.cz',
+          p: '990x300',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }],
+    prebidPath: 'https://cdn.cpex.cz/stage/hb/prebid/v8.12/prebid.min.js'
+  },
+  website: {
+    name: 'hn.cz'
+  },
+  formats: {
+    skin: {
+      enabled: true,
+      contentEl: /*S*/ document.getElementById('brand-a') /*E*/ ,
+      offset: 130,
+      offsetScroll: false
+    },
+    native: {
+      enabled: true
+    }
+  },
+  general: {
+    errorPath: 'https://73f2bd72d0d2477ab2f976d6098fe246@o530000.ingest.sentry.io/4504531846365184',
+    onLoad: /*S*/ (function() {
+      var gdprCmpStub = document.createElement('script');
+      gdprCmpStub.src = 'resources/gdpr_cmp_stub.js';
+      gdprCmpStub.type = 'text/javascript';
+      document.head.appendChild(gdprCmpStub);
+      var cxScript = document.createElement('script');
+      cxScript.src = '//cdn.cxense.com/cx.js';
+      cxScript.type = 'text/javascript';
+      document.head.appendChild(cxScript);
+      cxScript.onload = function() {
+        cX.sync('rub');
+      };
+    })(); /*E*/
+  }
+}
