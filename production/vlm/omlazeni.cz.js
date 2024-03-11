aa7bf3847a190214ddfc92dd8b854b4e5f8e9f94
CPEx Dev <dev@cpex.cz>
2024-03-11 16:51:10 +0100

    Iveta: nasazení package na omlazeni.cz

---
+++ production/vlm/omlazeni.cz.js
@@ -0,0 +1,29 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'omlazeni.cz'
+  },
+  cmp: {
+    enabled: false
+  },
+  adserver: {
+    adapter: 'sastracker'
+  },
+  headerbidding: {
+    enabled: false
+  },
+  formats: {
+    interscroller: {
+      enabled: true
+    },
+    slideup: {
+      enabled: true,
+      adUnits: ['m-sticky']
+    },
+    responsive: {
+      enabled: true,
+      adUnits: ['m-square-1'],
+      width: 480,
+      height: 480
+    }
+  }
+}
