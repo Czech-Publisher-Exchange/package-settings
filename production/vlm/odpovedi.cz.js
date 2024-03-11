21dd30c339c3ce7952ae0f8bf1bc8f6105f647f2
CPEx Dev <dev@cpex.cz>
2024-03-11 16:13:32 +0100

    Iveta: nasazení package na odpovedi.cz

---
+++ production/vlm/odpovedi.cz.js
@@ -0,0 +1,29 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'odpovedi.cz'
+  },
+  cmp: {
+    enabled: false
+  },
+  adserver: {
+    adapter: 'sastracker'
+  },
+  headerbidding: {
+    enabled: false
+  },
+  formats: {
+    interscroller: {
+      enabled: true
+    },
+    slideup: {
+      enabled: true,
+      adUnits: ['m-sticky']
+    },
+    responsive: {
+      enabled: true,
+      adUnits: ['m-square-2'],
+      width: 480,
+      height: 480
+    }
+  }
+}
