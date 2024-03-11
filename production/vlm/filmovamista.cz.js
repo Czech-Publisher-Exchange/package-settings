fcb8db22221b3eb53ad6db9b7cdbca0decf8a3c8
CPEx Dev <dev@cpex.cz>
2024-03-11 16:30:11 +0100

    Iveta: nasazení package na filmovamista.cz

---
+++ production/vlm/filmovamista.cz.js
@@ -0,0 +1,29 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'filmovamista.cz'
+  },
+  cmp: {
+    enabled: false
+  },
+  adserver: {
+    adapter: 'sastracker'
+  },
+  headerbidding: {
+    enabled: false
+  },
+  formats: {
+    interscroller: {
+      enabled: true
+    },
+    slideup: {
+      enabled: true,
+      adUnits: ['m-sticky']
+    },
+    responsive: {
+      enabled: true,
+      adUnits: ['m-square-2', 'm-square-1', 'm-square-3'],
+      width: 480,
+      height: 480
+    }
+  }
+}
