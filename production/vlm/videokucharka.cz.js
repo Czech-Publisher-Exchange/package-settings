5fa8891ac05a3fb2404b0e79d7bc99956f5e7c91
CPEx Dev <dev@cpex.cz>
2024-03-12 12:12:24 +0100

    Iveta: nasazení package na videokucharka.cz

---
+++ production/vlm/videokucharka.cz.js
@@ -0,0 +1,24 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'videokucharka.cz'
+  },
+  cmp: {
+    enabled: false
+  },
+  adserver: {
+    adapter: 'sastracker'
+  },
+  headerbidding: {
+    enabled: false
+  },
+  formats: {
+    slideup: {
+      adUnits: ['m-sticky']
+    },
+    responsive: {
+      adUnits: ['m-square-1'],
+      width: 480,
+      height: 480
+    }
+  }
+}
