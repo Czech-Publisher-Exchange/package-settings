c6950cb90507b2d86ce16016886ce39795f2d91d
CPEx Dev <dev@cpex.cz>
2024-03-12 12:23:39 +0100

    Iveta: nasazení package na prima-receptar.cz

---
+++ production/vlm/prima-receptar.cz.js
@@ -0,0 +1,29 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'prima-receptar.cz'
+  },
+  cmp: {
+    enabled: false
+  },
+  adserver: {
+    adapter: 'sastracker'
+  },
+  headerbidding: {
+    enabled: false
+  },
+  formats: {
+    interscroller: {
+      enabled: true
+    },
+    slideup: {
+      enabled: false,
+      adUnits: ['m-sticky']
+    },
+    responsive: {
+      enabled: true,
+      adUnits: ['m-square-1'],
+      width: 480,
+      height: 480
+    }
+  }
+}
