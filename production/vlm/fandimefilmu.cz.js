403f138287be4f9d9df9da56468dff29b0e522a7
CPEx Dev <dev@cpex.cz>
2024-03-12 12:15:27 +0100

    Iveta: nasazení package na fandimefilmu.cz

---
+++ production/vlm/fandimefilmu.cz.js
@@ -0,0 +1,24 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'fandimefilmu.cz'
+  },
+  cmp: {
+    enabled: false
+  },
+  adserver: {
+    adapter: 'sastracker'
+  },
+  headerbidding: {
+    enabled: false
+  },
+  formats: {
+    slideup: {
+      adUnits: ['m-sticky']
+    },
+    responsive: {
+      adUnits: ['m-square-1'],
+      width: 480,
+      height: 480
+    }
+  }
+}
