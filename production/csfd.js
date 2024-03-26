4c549c5c467b321fe27cd2f10aa19e685eb28289
David Spohr <spohr.david@gmail.com>
2024-03-26 17:23:20 +0100

    Fixed piano export, removed where not applicable

--- production/csfd.js
+++ production/csfd.js
@@ -42,9 +42,7 @@ window.cpexPublisherSettings = {
             if (event.origin !== 'https://cdn.cpex.cz') return;
             if (event.data.type === 'cpexRead') {
               window.dataLayer = window.dataLayer || [];
-              dataLayer.push({
-                seg: event.data.value
-              })
+              dataLayer.push({ seg: event.data.value })
             }
           }, false);
           cpexPackage.utils.addElement('iframe', document.body, {
@@ -55,20 +53,6 @@ window.cpexPublisherSettings = {
           })
         }
       })
-      // piano segment export
-      window.cX = window.cX || {}
-      window.cX.callQueue = window.cX.callQueue || []
-      window.cX.callQueue.push(['invoke', () => {
-        window.__tcfapi('addEventListener', 2, (data, success) => {
-          if (success === false) { return }
-          if (data.vendor.consents[570] && window.Didomi.getUserConsentStatusForVendor('c:pomomedia-HZQX3YWL')) {
-            const segments = window.cX.getUserSegmentIds({ persistedQueryId: '51ff14b454af0cf4aedc891fee56b86c1aa69a31' })
-            if (isFilledArray(segments)) { // use html from cdn.cpex.cz to save it as a 3rd party cookie, for 14 days
-              addElement('iframe', document.body, { src: 'https://cdn.cpex.cz/cookies/save.html?name=exc&time=1209600&data=' + encodeURIComponent(segments.toString()), width: 0, height: 0, style: 'border: none; display: block' })
-            }
-          }
-        })
-      }])    
     } /*E*/
   },
   formats: {
