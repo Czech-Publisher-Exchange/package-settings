60dd3a7036549c772653d25b271c4369d034bdef
David Spohr <spohr.david@gmail.com>
2024-03-26 16:18:43 +0100

    Moved piano sync to publisher settings

--- production/eco.js
+++ production/eco.js
@@ -40,6 +40,22 @@ window.cpexPublisherSettings = {
     userIDs: ['id5Id', 'shareId', 'criteo', 'czechAdId']
   },
   general: {
-    errorApiKey: 'hbp_DAmvPZplPFWmFYRT4W16UT3Qs47LcW1iHniv'
+    errorApiKey: 'hbp_DAmvPZplPFWmFYRT4W16UT3Qs47LcW1iHniv',
+    onLoad: /*S*/ () => {
+      // piano segment export
+      window.cX = window.cX || {}
+      window.cX.callQueue = window.cX.callQueue || []
+      window.cX.callQueue.push(['invoke', () => {
+        window.__tcfapi('addEventListener', 2, (data, success) => {
+          if (success === false) { return }
+          if (data.vendor.consents[570] && window.Didomi.getUserConsentStatusForVendor('c:pomomedia-HZQX3YWL')) {
+            const segments = window.cX.getUserSegmentIds({ persistedQueryId: '51ff14b454af0cf4aedc891fee56b86c1aa69a31' })
+            if (isFilledArray(segments)) { // use html from cdn.cpex.cz to save it as a 3rd party cookie, for 14 days
+              addElement('iframe', document.body, { src: 'https://cdn.cpex.cz/cookies/save.html?name=exc&time=1209600&data=' + encodeURIComponent(segments.toString()), width: 0, height: 0, style: 'border: none; display: block' })
+            }
+          }
+        })
+      }])    
+    } /*E*/
   }
 }
--- production/iinfo.js
+++ production/iinfo.js
@@ -25,7 +25,23 @@ window.cpexPublisherSettings = {
     }
   },
   general: {
-    errorApiKey: 'hbp_lNk4ta9q55knF3cxCNbFUHJbOeI3gL0GVl1h'
+    errorApiKey: 'hbp_lNk4ta9q55knF3cxCNbFUHJbOeI3gL0GVl1h',
+    onLoad: /*S*/ () => {
+      // piano segment export
+      window.cX = window.cX || {}
+      window.cX.callQueue = window.cX.callQueue || []
+      window.cX.callQueue.push(['invoke', () => {
+        window.__tcfapi('addEventListener', 2, (data, success) => {
+          if (success === false) { return }
+          if (data.vendor.consents[570] && window.Didomi.getUserConsentStatusForVendor('c:pomomedia-HZQX3YWL')) {
+            const segments = window.cX.getUserSegmentIds({ persistedQueryId: '51ff14b454af0cf4aedc891fee56b86c1aa69a31' })
+            if (isFilledArray(segments)) { // use html from cdn.cpex.cz to save it as a 3rd party cookie, for 14 days
+              addElement('iframe', document.body, { src: 'https://cdn.cpex.cz/cookies/save.html?name=exc&time=1209600&data=' + encodeURIComponent(segments.toString()), width: 0, height: 0, style: 'border: none; display: block' })
+            }
+          }
+        })
+      }])    
+    } /*E*/
   },
   formats: {
     skin: {
