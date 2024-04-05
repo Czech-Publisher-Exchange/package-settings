149a6ad161ae5e137bf7d6d6771b8191337d851e
David Spohr <spohr.david@gmail.com>
2024-04-05 15:59:19 +0200

    Replaced line comments with block comments

--- production/eco.js
+++ production/eco.js
@@ -42,9 +42,9 @@ window.cpexPublisherSettings = {
   general: {
     errorApiKey: 'hbp_DAmvPZplPFWmFYRT4W16UT3Qs47LcW1iHniv',
     onLoad: /*S*/ () => {
-      // skin offset
+      /* skin offset */
       window.cpexPackage.settings.formats.skin.offset = window._ecohec ? (window._ecohec.desktopNavigationHeight || 0) : 0
-      // piano segment export
+      /* piano segment export */
       window.cX = window.cX || {}
       window.cX.callQueue = window.cX.callQueue || []
       window.cX.callQueue.push(['invoke', () => {
@@ -52,7 +52,7 @@ window.cpexPublisherSettings = {
           if (success === false) { return }
           if (data.vendor.consents[570] && window.Didomi.getUserConsentStatusForVendor('c:pomomedia-HZQX3YWL')) {
             const segments = window.cX.getUserSegmentIds({ persistedQueryId: '51ff14b454af0cf4aedc891fee56b86c1aa69a31' })
-            if (Array.isArray(segments) && segments.length) { // use html from cdn.cpex.cz to save it as a 3rd party cookie, for 14 days
+            if (Array.isArray(segments) && segments.length) { /* use html from cdn.cpex.cz to save it as a 3rd party cookie, for 14 days */
               window.cpexPackage.utils.addElement('iframe', document.body, { src: 'https://cdn.cpex.cz/cookies/save.html?name=exc&time=1209600&data=' + encodeURIComponent(segments.toString()), width: 0, height: 0, style: 'border: none; display: block' })
             }
           }
