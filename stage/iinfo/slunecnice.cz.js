1ff3321dc34416c28964784acc480edc62274267
CPEx Dev <dev@cpex.cz>
2024-04-08 10:18:59 +0200

    Aleš: Úprava stage settings pro slunecnice GAM

--- stage/iinfo/slunecnice.cz.js
+++ stage/iinfo/slunecnice.cz.js
@@ -1,11 +1,5 @@
 window.cpexWebsiteSettings = {
   adserver: {
-    adapter: 'sastracker',
-    allowedSSPs: {
-      pubmatic: 1,
-      index: 3,
-      magnite: 4,
-      xandr: 5
-    }
+    adapter: 'googletag (publisher)'
   }
 }
