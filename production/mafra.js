974b20f46c78570440d6e8ca162562ab3ac0734e
CPEx Dev <dev@cpex.cz>
2024-04-08 10:50:25 +0200

    Markéta: odebrán Invibes a přidán Smart v biddertable

--- production/mafra.js
+++ production/mafra.js
@@ -22,8 +22,8 @@ window.cpexPublisherSettings = {
       'fragile-adform': 'Fragile-Adform_HB',
       criteo: 'Criteo_HB',
       teads: 'Teads_HB',
-      invibes: 'Invibes_HB',
-      sspBC: 'WP_HB'
+      sspBC: 'WP_HB',
+      smart: 'Smart_HB'
     }
   },
   headerbidding: {
@@ -75,14 +75,22 @@ window.cpexPublisherSettings = {
     onLoad: /*S*/ () => {
       /* piano segment export */
       window.cX = window.cX || {}
-      window.cX.callQueue = window.cX.callQueue || []
-      window.cX.callQueue.push(['invoke', () => {
+      window.cX.callQueue = window.cX.callQueue || [] window.cX.callQueue.push(['invoke', () => {
         window.__tcfapi('addEventListener', 2, (data, success) => {
-          if (success === false) { return }
+          if (success === false) {
+            return
+          }
           if (data.vendor.consents[570] && window.Didomi.getUserConsentStatusForVendor('c:pomomedia-HZQX3YWL')) {
-            const segments = window.cX.getUserSegmentIds({ persistedQueryId: '51ff14b454af0cf4aedc891fee56b86c1aa69a31' })
-            if (Array.isArray(segments) && segments.length) { /* use html from cdn.cpex.cz to save it as a 3rd party cookie, for 14 days */
-              window.cpexPackage.utils.addElement('iframe', document.body, { src: 'https://cdn.cpex.cz/cookies/save.html?name=exc&time=1209600&data=' + encodeURIComponent(segments.toString()), width: 0, height: 0, style: 'border: none; display: block' })
+            const segments = window.cX.getUserSegmentIds({
+              persistedQueryId: '51ff14b454af0cf4aedc891fee56b86c1aa69a31'
+            }) if (Array.isArray(segments) && segments.length) {
+              /* use html from cdn.cpex.cz to save it as a 3rd party cookie, for 14 days */
+              window.cpexPackage.utils.addElement('iframe', document.body, {
+                src: 'https://cdn.cpex.cz/cookies/save.html?name=exc&time=1209600&data=' + encodeURIComponent(segments.toString()),
+                width: 0,
+                height: 0,
+                style: 'border: none; display: block'
+              })
             }
           }
         })
