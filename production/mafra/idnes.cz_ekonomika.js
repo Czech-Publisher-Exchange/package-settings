9deaa3e12a5c362336b1525936d5ed8a6f34956e
CPEx Dev <dev@cpex.cz>
2024-04-22 11:13:21 +0200

    Markéta: přidání Criteo do nativ

--- production/mafra/idnes.cz_ekonomika.js
+++ production/mafra/idnes.cz_ekonomika.js
@@ -2943,6 +2943,11 @@ window.cpexWebsiteSettings = {
         params: {
           placementId: 30878663
         }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172
+        }
       }]
     }, {
       code: 'm_nativ',
@@ -2964,6 +2969,11 @@ window.cpexWebsiteSettings = {
         bidder: 'appnexus',
         params: {
           placementId: 30878664
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172
         }
       }]
     }, {
