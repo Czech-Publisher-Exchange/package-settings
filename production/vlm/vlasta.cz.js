2856e8dc6194cc65eaae009fa973acd9741be91a
CPEx Dev <dev@cpex.cz>
2024-04-29 09:45:18 +0200

    Iveta: zapojení wallpaper-4 a 5

--- production/vlm/vlasta.cz.js
+++ production/vlm/vlasta.cz.js
@@ -2335,6 +2335,88 @@ window.cpexWebsiteSettings = {
       filter: {
         minWidth: 1000
       }
+    }, {
+      code: 'wallpaper-4',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [300, 300],
+            [336, 280],
+            [480, 300]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 26460,
+          zoneId: 3365136,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 48, 16, 83]
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '5906178'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 32592873
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172
+        }
+      }],
+      filter: {
+        minWidth: 1000
+      }
+    }, {
+      code: 'wallpaper-5',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [300, 300],
+            [336, 280],
+            [480, 300]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 26460,
+          zoneId: 3365158,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 48, 16, 83]
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '5906189'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 32593294
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172
+        }
+      }],
+      filter: {
+        minWidth: 1000
+      }
     }]
   },
   formats: {
