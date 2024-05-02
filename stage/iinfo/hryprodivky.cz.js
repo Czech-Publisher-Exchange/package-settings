09428376708cd396c46f246e18bbfc710efc1ffc
CPEx Dev <dev@cpex.cz>
2024-05-02 08:56:29 +0200

    Markéta: custom buckets

--- stage/iinfo/hryprodivky.cz.js
+++ stage/iinfo/hryprodivky.cz.js
@@ -377,7 +377,8 @@ window.cpexWebsiteSettings = {
       filter: {
         maxWidth: 1000
       }
-    }]
+    }],
+    customBuckets: [15, 18, 20, 22, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 80, 90, 100, 125, 150]
   },
   formats: {
     skin: {
