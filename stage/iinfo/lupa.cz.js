b760e04e8cf7ff3f9de96f28b78ada95db90816b
CPEx Dev <dev@cpex.cz>
2024-05-02 08:58:10 +0200

    Markéta: migrace GAM, doplnění IDs

---
+++ stage/iinfo/lupa.cz.js
@@ -0,0 +1,544 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'lupa.cz'
+  },
+  adserver: {
+    adapter: 'googletag'
+  },
+  headerbidding: {
+    adUnits: [{
+      code: 'iinfo-area-halfpagead_horni',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [160, 600],
+            [120, 600],
+            [300, 250],
+            [300, 300]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 265202,
+          zoneId: 1518746,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 9, 8, 15, 48]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 265182,
+          zoneId: 1518748,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 9, 8, 15, 48]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '2445469'
+        }
+      }, {
+        bidder: 'im-adform',
+        params: {
+          mid: 690503,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'im-adform',
+        params: {
+          mid: 690609,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 480784
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 16315569
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'lupa.cz',
+          p: '300x600',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'iinfo-area-halfpagead_spodni',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [160, 600],
+            [120, 600],
+            [300, 250],
+            [300, 300]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 265202,
+          zoneId: 2088326,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 9, 8, 15, 48]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 265182,
+          zoneId: 2088328,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 9, 8, 15, 48]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '3735834'
+        }
+      }, {
+        bidder: 'im-adform',
+        params: {
+          mid: 1057999,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'im-adform',
+        params: {
+          mid: 1058000,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 784923
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 21786325
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'lupa.cz',
+          p: '300x300_1',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'iinfo-area-leaderboard',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [728, 90],
+            [970, 210],
+            [998, 200],
+            [2000, 1400]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 265202,
+          zoneId: 1518756,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [2, 96, 259, 230]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 265182,
+          zoneId: 1518754,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [2, 96, 259, 230]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '2445472'
+        }
+      }, {
+        bidder: 'im-adform',
+        params: {
+          mid: 690497,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'im-adform',
+        params: {
+          mid: 690605,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 480785
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 30168998
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'lupa.cz',
+          p: 'branding',
+          m: 0,
+          g: 'hb'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'lupa.cz',
+          p: '998x200',
+          m: 0,
+          g: 'hb'
+        }
+      }],
+      filter: {
+        minWidth: 1000
+      }
+    }, {
+      code: 'iinfo-area-big-rectangle',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [970, 310],
+            [970, 210],
+            [970, 250],
+            [300, 250],
+            [300, 300]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 265202,
+          zoneId: 1518770,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [94, 96, 57, 15, 48]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 265182,
+          zoneId: 1518764,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [94, 96, 57, 15, 48]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '2445473'
+        }
+      }, {
+        bidder: 'im-adform',
+        params: {
+          mid: 690504,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'im-adform',
+        params: {
+          mid: 690610,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 430307
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 16315570
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'lupa.cz',
+          p: '970x310',
+          m: 0,
+          g: 'hb'
+        }
+      }],
+      filter: {
+        minWidth: 1000
+      }
+    }, {
+      code: 'iinfo-area-big-rectangle',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [300, 300]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 265202,
+          zoneId: 1518770,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 48]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 265182,
+          zoneId: 1518764,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 48]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '2445473'
+        }
+      }, {
+        bidder: 'im-adform',
+        params: {
+          mid: 690504,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'im-adform',
+        params: {
+          mid: 690610,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 430307
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 16315570
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'lupa.cz',
+          p: '970x310',
+          m: 0,
+          g: 'hb'
+        }
+      }],
+      filter: {
+        maxWidth: 1000
+      }
+    }, {
+      code: 'iinfo-area-rectangle',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [480, 300],
+            [300, 250],
+            [300, 300],
+            [336, 280]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 265202,
+          zoneId: 1518752,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83, 15, 48, 16]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 265182,
+          zoneId: 1518750,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [83, 15, 48, 16]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '5913151'
+        }
+      }, {
+        bidder: 'im-adform',
+        params: {
+          mid: 690498,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'im-adform',
+        params: {
+          mid: 690606,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 480786
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 16315566
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'lupa.cz',
+          p: '480x300',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'iinfo-area-interscroller',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [480, 820]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 364918,
+          zoneId: 3361156,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [256]
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '5913120'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1099718
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 32643567
+        }
+      }],
+      filter: {
+        maxWidth: 1000
+      }
+    }, {
+      code: 'iinfo-area-mobilni_branding',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [320, 100],
+            [320, 150],
+            [500, 200]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 364918,
+          zoneId: 3361158,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [117, 60, 258]
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '5913121'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1099719
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 32643570
+        }
+      }],
+      filter: {
+        maxWidth: 1000
+      }
+    }],
+    customBuckets: [15, 18, 20, 22, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 80, 90, 100, 125, 150]
+  },
+  formats: {
+    skin: {
+      contentEl: /*S*/ document.getElementsByClassName('parter-cpex-branding-wrapper')[0] /*E*/,
+      enabled: true,
+      hide: [/*S*/ document.getElementsByClassName('design-advert-placeholder--leaderboard design-advert-placeholder')[0] /*E*/, /*S*/ document.getElementsByClassName('leaderboard-dynamic-height-wrapper')[0] /*E*/]
+    },
+    native: {
+      enabled: true
+    },
+    slideup: {
+      adUnits: ['iinfo-area-sticky-board']
+    }
+  },
+  cmp: {
+    enabled: false
+  }
+}
