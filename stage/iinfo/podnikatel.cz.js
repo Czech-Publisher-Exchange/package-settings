3dd1b1ac7cfdb6a36ca237e7b0abf6aa8df5d37b
CPEx Dev <dev@cpex.cz>
2024-05-13 16:18:45 +0200

    Aleš: úprava názvu a mapování

--- stage/iinfo/podnikatel.cz.js
+++ stage/iinfo/podnikatel.cz.js
@@ -7,7 +7,7 @@ window.cpexWebsiteSettings = {
   },
   headerbidding: {
     adUnits: [{
-      code: 'iinfo-area-halfpagead-horni',
+      code: 'iinfo-area-halfpagead_horni',
       mediaTypes: {
         banner: {
           sizes: [
@@ -78,7 +78,7 @@ window.cpexWebsiteSettings = {
         }
       }]
     }, {
-      code: 'iinfo-area-halfpagead-spodni',
+      code: 'iinfo-area-halfpagead_spodni',
       mediaTypes: {
         banner: {
           sizes: [
@@ -381,7 +381,8 @@ window.cpexWebsiteSettings = {
           sizes: [
             [480, 300],
             [300, 250],
-            [300, 300]
+            [300, 300],
+            [336, 280]
           ]
         }
       },
@@ -392,7 +393,7 @@ window.cpexWebsiteSettings = {
           zoneId: 1336482,
           accountId: '10900',
           position: 'atf',
-          sizes: [83, 15, 48]
+          sizes: [83, 15, 48, 16]
         }
       }, {
         bidder: 'rubicon-mask',
@@ -401,7 +402,7 @@ window.cpexWebsiteSettings = {
           zoneId: 1336480,
           accountId: '10900',
           position: 'atf',
-          sizes: [83, 15, 48]
+          sizes: [83, 15, 48, 16]
         },
         bidderModuleName: 'rubicon'
       }, {
@@ -444,75 +445,83 @@ window.cpexWebsiteSettings = {
         }
       }]
     }, {
-      code: 'iinfo-area-spodni-square-banner',
+      code: 'iinfo-area-interscroller',
       mediaTypes: {
         banner: {
           sizes: [
-            [300, 300],
-            [300, 250],
-            [250, 250],
-            [200, 200]
+            [480, 820]
           ]
         }
       },
       bids: [{
         bidder: 'rubicon',
         params: {
-          siteId: 265202,
-          zoneId: 1336474,
+          siteId: 364918,
+          zoneId: 3361148,
           accountId: '10900',
           position: 'atf',
-          sizes: [48, 15, 14, 13]
+          sizes: [256]
         }
-      }, {
-        bidder: 'rubicon-mask',
-        params: {
-          siteId: 265182,
-          zoneId: 1336472,
-          accountId: '10900',
-          position: 'atf',
-          sizes: [48, 15, 14, 13]
-        },
-        bidderModuleName: 'rubicon'
       }, {
         bidder: 'pubmatic',
         params: {
           publisherId: '158732',
-          adSlot: '3951699'
+          adSlot: '5913116'
         }
-      }, {
-        bidder: 'im-adform',
-        params: {
-          mid: 690525,
-          rcur: 'USD'
-        },
-        bidderModuleName: 'adform'
-      }, {
-        bidder: 'im-adform',
-        params: {
-          mid: 690624,
-          rcur: 'USD'
-        },
-        bidderModuleName: 'adform'
       }, {
         bidder: 'ix',
         params: {
-          siteId: 480797
+          siteId: 1099716
         }
       }, {
         bidder: 'appnexus',
         params: {
-          placementId: 16315588
+          placementId: 32643559
+        }
+      }],
+      filter: {
+        maxWidth: 1000
+      }
+    }, {
+      code: 'iinfo-area-mobilni_branding',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [320, 100],
+            [320, 150],
+            [500, 200]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 364918,
+          zoneId: 3361150,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [117, 60, 258]
         }
       }, {
-        bidder: 'r2b2',
+        bidder: 'pubmatic',
         params: {
-          d: 'podnikatel.cz',
-          p: '300x300_2',
-          m: 0,
-          g: 'hb'
+          publisherId: '158732',
+          adSlot: '5913117'
         }
-      }]
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1099717
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 32643573
+        }
+      }],
+      filter: {
+        maxWidth: 1000
+      }
     }],
     userIDs: ['id5Id', 'sharedId', 'criteo', 'cpexId'],
     customBuckets: [15, 18, 20, 22, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 80, 90, 100, 125, 150]
