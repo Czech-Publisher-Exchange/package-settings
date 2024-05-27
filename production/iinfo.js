924e4060ae9f9a24120dcf914eae3e1f05f8de67
CPEx Dev <dev@cpex.cz>
2024-05-27 16:38:25 +0200

    Markéta: přidání biddera ConnectAd

--- production/iinfo.js
+++ production/iinfo.js
@@ -21,7 +21,8 @@ window.cpexPublisherSettings = {
       r2b2: 'r2b2',
       ix: 'index_hb',
       triplelift: 'triplelift_hb',
-      'fragile-adform': 'fragile-adform'
+      'fragile-adform': 'fragile-adform',
+      connectad: 'connectad_hb'
     }
   },
   general: {
@@ -29,7 +30,22 @@ window.cpexPublisherSettings = {
   },
   formats: {
     skin: {
-      generalCSS: "#cpex-skin::after {\n    position: absolute;\n    top: 0;\n    left: 0;\n    font-weight: 700;\n    text-shadow: 0.5px 0.866px 1px #0001;\n    pointer-events: none;\n    content: 'reklama';\n    display: block;\n    color: #fff;\n    line-height: 1.5rem;\n    font-size: 0.6875rem;\n    font-family: inherit;\n    width: 100%;\n    text-align: center;\n}"
+      generalCSS: "#cpex-skin::after {
+    position: absolute;
+    top: 0;
+    left: 0;
+    font-weight: 700;
+    text-shadow: 0.5px 0.866px 1px #0001;
+    pointer-events: none;
+    content: 'reklama';
+    display: block;
+    color: #fff;
+    line-height: 1.5rem;
+    font-size: 0.6875rem;
+    font-family: inherit;
+    width: 100%;
+    text-align: center;
+}"
     }
   }
 }
