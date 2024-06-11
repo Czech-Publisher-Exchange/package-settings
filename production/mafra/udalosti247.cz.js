067f530da256abe35bb56ef546bdf2b5ce97e653
CPEx Dev <dev@cpex.cz>
2024-06-11 14:57:37 +0200

    Markéta: přidání Criteo uid, doplnění sizes

--- production/mafra/udalosti247.cz.js
+++ production/mafra/udalosti247.cz.js
@@ -8,10 +8,13 @@ window.cpexWebsiteSettings = {
       mediaTypes: {
         banner: {
           sizes: [
+            [2000, 1400],
             [998, 200],
             [728, 90],
             [970, 210],
-            [970, 90]
+            [970, 90],
+            [2000, 1200],
+            [1920, 1080]
           ]
         }
       },
@@ -22,7 +25,7 @@ window.cpexWebsiteSettings = {
           zoneId: 3178142,
           accountId: '10900',
           position: 'atf',
-          sizes: [259, 2, 96, 55]
+          sizes: [230, 259, 2, 96, 55, 265]
         }
       }, {
         bidder: 'pubmatic',
@@ -44,7 +47,8 @@ window.cpexWebsiteSettings = {
       }, {
         bidder: 'criteo',
         params: {
-          networkId: 11172
+          networkId: 11172,
+          uid: 'mafra_banner_default'
         }
       }, {
         bidder: 'r2b2',
