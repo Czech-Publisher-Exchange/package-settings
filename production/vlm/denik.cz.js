ee67425128340b9d84afb84c2581ecd6d8547639
CPEx Dev <dev@cpex.cz>
2024-07-02 16:31:28 +0200

    Iveta: m-sticky-gallery adform

--- production/vlm/denik.cz.js
+++ production/vlm/denik.cz.js
@@ -2537,6 +2537,12 @@ window.cpexWebsiteSettings = {
           adSlot: '5204069'
         }
       }, {
+        bidder: 'adform',
+        params: {
+          mid: 1940156,
+          rcur: 'USD'
+        }
+      }, {
         bidder: 'ix',
         params: {
           siteId: 1088925
