2cc7c4294795a5fe537c805a290af7d1047b4eda
CPEx Dev <dev@cpex.cz>
2024-07-19 18:20:42 +0200

    David: Added adUnits

--- production/hyperia/letakomat.cz.js
+++ production/hyperia/letakomat.cz.js
@@ -1,5 +1,83 @@
 window.cpexWebsiteSettings = {
   website: {
     name: 'letakomat.cz'
+  },
+  headerbidding: {
+    adUnits: [{
+      code: 'post-anchor',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [300, 100],
+            [300, 50],
+            [300, 120]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 555452,
+          zoneId: 3481232,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 19, 44, 85]
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '6073719'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1123986
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 33360784
+        }
+      }]
+    }, {
+      code: 'div-gpt-ad-13-0',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [250, 250],
+            [336, 280]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 555452,
+          zoneId: 3487154,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 14, 16]
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '6105524'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1125596
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 33645351
+        }
+      }]
+    }]
   }
 }
