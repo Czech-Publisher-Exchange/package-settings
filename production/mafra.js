ad8166e369118fbe3801597942d91bac8774af0c
CPEx Dev <dev@cpex.cz>
2024-07-24 12:54:24 +0200

    Markéta: přidání 5 adunit do responsive

--- production/mafra.js
+++ production/mafra.js
@@ -49,7 +49,7 @@ window.cpexPublisherSettings = {
     },
     responsive: {
       enabled: true,
-      adUnits: ['m_branding', 'm_branding_foto', 'm_branding_foto2', 'm_300x250', 'm_300x250_clankovy', 'm_300x250_clankovy_opak1', 'm_300x250_clankovy_opak2', 'm_300x250_foto_end', 'm_300x250_foto_mid', 'm_300x250_foto_mid2', 'm_300x250_foto_start', 'm_300x250_infinite'],
+      adUnits: ['m_branding', 'm_branding_foto', 'm_branding_foto2', 'm_300x250', 'm_300x250_clankovy', 'm_300x250_clankovy_opak1', 'm_300x250_clankovy_opak2', 'm_300x250_foto_end', 'm_300x250_foto_mid', 'm_300x250_foto_mid2', 'm_300x250_foto_start', 'm_300x250_infinite', 'm_300x250_vybrali', 'm_300x250_souvisejici', 'm_300x250_foto_mid3', 'm_300x250_foto_mid4', 'm_300x250_clankovy_opak3'],
       width: 480,
       height: 480
     },
