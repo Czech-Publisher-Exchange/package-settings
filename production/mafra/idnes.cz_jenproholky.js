53d53a211c4ffbc0478b544a910b6a8741fce57f
CPEx Dev <dev@cpex.cz>
2024-07-30 13:15:48 +0200

    Markéta: offset 44 u interscrolleru

--- production/mafra/idnes.cz_jenproholky.js
+++ production/mafra/idnes.cz_jenproholky.js
@@ -1014,7 +1014,8 @@ window.cpexWebsiteSettings = {
       hide: [/*S*/ document.getElementById('r-leaderboard-hp') /*E*/]
     },
     interscroller: {
-      enabled: true
+      enabled: true,
+      offset: 44
     },
     slideup: {
       enabled: true,
