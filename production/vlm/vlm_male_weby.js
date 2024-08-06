242758bf8ee28ffd98a2f65fb78f0efb35e13bbb
CPEx Dev <dev@cpex.cz>
2024-08-06 16:35:08 +0200

    Iveta: zapnutí vignette   nové adunity pro responsive

--- production/vlm/vlm_male_weby.js
+++ production/vlm/vlm_male_weby.js
@@ -15,10 +15,10 @@ window.cpexWebsiteSettings = {
       adUnits: ['m-sticky']
     },
     vignette: {
-      enabled: false
+      enabled: true
     },
     responsive: {
-      adUnits: ['m-square-1', 'm-square-2', 'm-square-3', 'm-square-4', 'm-square-5', 'm-square-bottom-1', 'm-square-gallery-1', 'm-square-gallery-2', 'm-square-gallery-3', 'm-square-gallery-4', 'm-square-gallery-5', 'm-square-gallery-6', 'm-square-gallery-7', 'm-square-gallery-8']
+      adUnits: ['m-square-1', 'm-square-2', 'm-square-3', 'm-square-4', 'm-square-5', 'm-square-bottom-1', 'm-square-gallery-1', 'm-square-gallery-2', 'm-square-gallery-3', 'm-square-gallery-4', 'm-square-gallery-5', 'm-square-gallery-6', 'm-square-gallery-7', 'm-square-gallery-8', 'm-square-1-hp', 'm-square-2-hp', 'm-square-category-1', 'm-square-category-2', 'm-square-category-3', 'm-square-category-4', 'm-square-category-5']
     }
   }
 }
