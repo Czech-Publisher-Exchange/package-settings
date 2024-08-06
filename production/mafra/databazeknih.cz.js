9bdc2c17902406ed39b4e32671b9e3fa2cd7c547
CPEx Dev <dev@cpex.cz>
2024-08-06 13:10:26 +0200

    Markéta: přidání IDs WP, rozšíření sizes

--- production/mafra/databazeknih.cz.js
+++ production/mafra/databazeknih.cz.js
@@ -185,6 +185,8 @@ window.cpexWebsiteSettings = {
           m: 0,
           g: 'hb'
         }
+      }, {
+        bidder: 'sspBC'
       }]
     }, {
       code: 'halfpagead',
@@ -382,8 +384,13 @@ window.cpexWebsiteSettings = {
             [250, 250],
             [480, 820],
             [300, 600],
+            [720, 1280],
             [768, 1230],
-            [480, 480]
+            [480, 480],
+            [300, 300],
+            [160, 600],
+            [120, 600],
+            [338, 280]
           ]
         }
       },
@@ -394,7 +401,7 @@ window.cpexWebsiteSettings = {
           zoneId: 1649666,
           accountId: '10900',
           position: 'atf',
-          sizes: [15, 14, 256, 10, 261]
+          sizes: [15, 14, 256, 10, 484, 261, 48, 9, 8]
         }
       }, {
         bidder: 'rubicon-mask',
@@ -403,7 +410,7 @@ window.cpexWebsiteSettings = {
           zoneId: 1649686,
           accountId: '10900',
           position: 'atf',
-          sizes: [15, 14, 256, 10, 261]
+          sizes: [15, 14, 256, 10, 484, 261, 48, 9, 8]
         },
         bidderModuleName: 'rubicon'
       }, {
