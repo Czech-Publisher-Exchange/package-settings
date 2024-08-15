8e37726c9a90e0985462665467d805ecc5031207
CPEx Dev <dev@cpex.cz>
2024-08-15 16:19:29 +0200

    Iveta: nové settings pro nationalgeographic.cz

---
+++ production/vlm/nationalgeographic.cz.js
@@ -0,0 +1,24 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'nationalgeographic.cz'
+  },
+  headerbidding: {
+    enabled: false
+  },
+  formats: {
+    interscroller: {
+      enabled: true,
+      adUnits: ['m-square-1', 'm-square-category-1', 'm-square-category-2', 'm-square-gallery-1']
+    },
+    slideup: {
+      enabled: true,
+      adUnits: ['m-sticky', 'm-sticky-gallery']
+    },
+    vignette: {
+      enabled: true
+    },
+    responsive: {
+      adUnits: ['m-square-1', 'm-square-2', 'm-square-3', 'm-square-4', 'm-square-5', 'm-square-bottom-1', 'm-square-gallery-1', 'm-square-gallery-2', 'm-square-gallery-3', 'm-square-gallery-4', 'm-square-gallery-5', 'm-square-gallery-6', 'm-square-gallery-7', 'm-square-gallery-8', 'm-square-1-hp', 'm-square-2-hp', 'm-square-category-1', 'm-square-category-2', 'm-square-category-3', 'm-square-category-4', 'm-square-category-5']
+    }
+  }
+}
