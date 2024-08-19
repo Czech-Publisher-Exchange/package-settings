a867588afd067bda2f4af3b1973922f702e3eb68
CPEx Dev <dev@cpex.cz>
2024-08-19 12:39:12 +0200

    Aleš: Zapnutí AB testu (beze změny) pro měření reportingu

--- production/vlm/kondice.cz.js
+++ production/vlm/kondice.cz.js
@@ -3787,9 +3787,10 @@ window.cpexWebsiteSettings = {
         probability: 0.5
       },
       b: {
-        websiteSettings: 'https://cdn.cpex.cz/settings/production/vlm/kondice.cz.js',
+        websiteSettings: 'https://cdn.cpex.cz/settings/production/vlm/kondice.cz_b.js',
         probability: 0.5
       }
-    }
+    },
+    enabled: true
   }
 }
