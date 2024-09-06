d9e7d04834628c58ce7f9d5079ead91975c1e812
CPEx Dev <dev@cpex.cz>
2024-09-06 18:14:01 +0200

    David: Leaving only connectad for testing

--- stage/mafra/idnes.cz_zpravy.js
+++ stage/mafra/idnes.cz_zpravy.js
@@ -4,198 +4,6 @@ window.cpexWebsiteSettings = {
   },
   headerbidding: {
     adUnits: [{
-      code: 'branding',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [2000, 1400],
-            [998, 200],
-            [728, 90],
-            [970, 210],
-            [970, 90]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: '300x250',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [300, 250],
-            [250, 250]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'halfpagead',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [300, 600],
-            [300, 300],
-            [300, 250],
-            [160, 600],
-            [250, 250]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'halfpagead_spodni',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [300, 600],
-            [300, 300],
-            [300, 250],
-            [160, 600]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'halfpagead_rubrika',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [300, 600],
-            [300, 300],
-            [300, 250],
-            [160, 600]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'halfpagead_rubrika_spodni',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [300, 600],
-            [300, 300],
-            [300, 250],
-            [160, 600]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'halfpagead_fotogalerie',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [300, 600],
-            [300, 300],
-            [300, 250],
-            [160, 600]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'm_300x250',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [300, 250],
-            [250, 250],
-            [480, 480]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'm_300x250_clankovy',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [300, 250],
-            [250, 250],
-            [480, 480]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'm_300x250_vybrali',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [300, 250],
-            [250, 250]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'm_300x250_souvisejici',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [300, 250],
-            [250, 250]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
       code: 'm_branding',
       mediaTypes: {
         banner: {
@@ -204,241 +12,21 @@ window.cpexWebsiteSettings = {
             [250, 250],
             [480, 820],
             [300, 600],
+            [720, 1280],
             [768, 1230],
-            [480, 480]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'm_paticka',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [300, 250]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'm_sticky',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [320, 150],
-            [300, 250],
-            [500, 200]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'wallpaper',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [970, 310],
-            [728, 90],
-            [970, 250]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'widesquare',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [480, 300],
+            [480, 480],
             [300, 300],
-            [250, 250],
-            [300, 250],
-            [480, 480]
+            [160, 600],
+            [120, 600],
+            [338, 280]
           ]
         }
       },
       bids: [{
-        bidder: 'criteo',
+        bidder: 'connectad',
         params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'widesquare_spodni',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [480, 300],
-            [300, 300],
-            [250, 250],
-            [300, 250],
-            [480, 480]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'widesquare_clankovy',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [480, 300],
-            [300, 300],
-            [250, 250],
-            [300, 250],
-            [480, 480]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'branding_fotogalerie',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [2000, 1400],
-            [998, 200],
-            [728, 90],
-            [970, 210],
-            [970, 90]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'm_300x250_clankovy_opak1',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [300, 250],
-            [250, 250],
-            [480, 480]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'm_300x250_clankovy_opak2',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [300, 250],
-            [250, 250],
-            [480, 480]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'm_300x250_clankovy_opak3',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [300, 250],
-            [250, 250]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'm_300x250_foto_end',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [300, 250],
-            [250, 250],
-            [480, 480]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'm_300x250_foto_mid',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [300, 250],
-            [250, 250],
-            [480, 480]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'm_300x250_foto_start',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [300, 250],
-            [250, 250],
-            [480, 480]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
+          siteId: 3131476,
+          networkId: 131
         }
       }]
     }, {
@@ -449,32 +37,22 @@ window.cpexWebsiteSettings = {
             [300, 250],
             [250, 250],
             [480, 820],
-            [480, 480]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'halfpagead_lazyload',
-      mediaTypes: {
-        banner: {
-          sizes: [
             [300, 600],
+            [720, 1280],
+            [768, 1230],
+            [480, 480],
             [300, 300],
-            [300, 250],
-            [160, 600]
+            [160, 600],
+            [120, 600],
+            [338, 280]
           ]
         }
       },
       bids: [{
-        bidder: 'criteo',
+        bidder: 'connectad',
         params: {
-          networkId: 11172
+          siteId: 3131490,
+          networkId: 131
         }
       }]
     }, {
@@ -485,207 +63,22 @@ window.cpexWebsiteSettings = {
             [300, 250],
             [250, 250],
             [480, 820],
-            [480, 480]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'widesquare_clankovy2',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [480, 300],
-            [300, 300],
-            [250, 250],
-            [300, 250],
-            [480, 480]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'm_sticky_foto',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [320, 150],
-            [300, 250],
-            [500, 200]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'm_300x250_foto_mid2',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [300, 250],
-            [250, 250],
-            [480, 480]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'm_300x250_foto_mid3',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [300, 250],
-            [250, 250]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'm_300x250_foto_mid4',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [300, 250],
-            [250, 250]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'm_vignete',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [720, 1280],
-            [480, 820],
-            [300, 600]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'halfpagead_rubrika_lazyload',
-      mediaTypes: {
-        banner: {
-          sizes: [
             [300, 600],
+            [720, 1280],
+            [768, 1230],
+            [480, 480],
             [300, 300],
-            [300, 250],
-            [160, 600]
+            [160, 600],
+            [120, 600],
+            [338, 280]
           ]
         }
       },
       bids: [{
-        bidder: 'criteo',
+        bidder: 'connectad',
         params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'widesquare_clankovy3',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [480, 300],
-            [300, 300],
-            [250, 250],
-            [300, 250],
-            [480, 480]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'nativ',
-      mediaTypes: {
-        native: {}
-      },
-      bids: [{
-        bidder: 'adform',
-        params: {
-          mid: 1763464,
-          rcur: 'USD'
-        }
-      }, {
-        bidder: 'ix',
-        params: {
-          siteId: 1099265
-        }
-      }, {
-        bidder: 'appnexus',
-        params: {
-          placementId: 30878856
-        }
-      }, {
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
-        }
-      }]
-    }, {
-      code: 'm_nativ',
-      mediaTypes: {
-        native: {}
-      },
-      bids: [{
-        bidder: 'adform',
-        params: {
-          mid: 1763463,
-          rcur: 'USD'
-        }
-      }, {
-        bidder: 'ix',
-        params: {
-          siteId: 1099278
-        }
-      }, {
-        bidder: 'appnexus',
-        params: {
-          placementId: 30878858
-        }
-      }, {
-        bidder: 'criteo',
-        params: {
-          networkId: 11172
+          siteId: 3131492,
+          networkId: 131
         }
       }]
     }]
@@ -699,7 +92,8 @@ window.cpexWebsiteSettings = {
       hide: [/*S*/ document.getElementById('r-leaderboard-hp') /*E*/]
     },
     interscroller: {
-      enabled: true
+      enabled: true,
+      offset: 44
     },
     slideup: {
       enabled: true,
