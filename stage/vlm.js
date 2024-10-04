0de19ad55a125a849cee4f1bc0bb0bb213e22be6
CPEx Dev <dev@cpex.cz>
2024-10-04 17:24:29 +0200

    Frantisek: vignette disabled

--- stage/vlm.js
+++ stage/vlm.js
@@ -60,7 +60,7 @@ window.cpexPublisherSettings = {
       closeTextHTML: "<svg width='32' height='32' viewBox='29 15 26 22'><circle cx='42' cy='26' r='12' stroke='white' stroke-width='1.5'></circle><path d='M 36,20 l 12,12 M 36,32 l 12,-12' stroke='white' stroke-width='2'></path></svg>",
       css: '#cpex-vignette {    background-color: #000;    z-index: 2147483642;}#cpex-vignette-header {    margin: 0 18px 18px auto;    font-family: arial, sans-serif;    font-size: 12px;    color: #FFFFFF;    text-align: right;    padding: 4px 8px;    background: black;    position: absolute;    bottom: 0;    right: 0;    z-index: 1;    font-weight: 700;}#cpex-vignette-close {    position: absolute;    right: 0px;    top: 0px;    background: none;    border: none;}',
       adUnits: ['centerVignette'],
-      enabled: true
+      enabled: false
     },
     responsive: {
       enabled: true,
