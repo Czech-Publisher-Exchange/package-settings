ff452fb923516a772a2835ddf1ab74932f477161
CPEx Dev <dev@cpex.cz>
2024-10-04 14:10:20 +0200

    Frantisek: vignette vez cookie

--- stage/vlm.js
+++ stage/vlm.js
@@ -49,15 +49,14 @@ window.cpexPublisherSettings = {
           return bidCpm * 0.8
         } /*E*/
       }
-    }
+    },
+    userIDs: ['czechAdId']
   },
   cmp: {
     enabled: false
   },
   formats: {
     vignette: {
-      cookieDuration: 3600,
-      cookieName: 'vignette',
       closeTextHTML: "<svg width='32' height='32' viewBox='29 15 26 22'><circle cx='42' cy='26' r='12' stroke='white' stroke-width='1.5'></circle><path d='M 36,20 l 12,12 M 36,32 l 12,-12' stroke='white' stroke-width='2'></path></svg>",
       css: '#cpex-vignette {    background-color: #000;    z-index: 2147483642;}#cpex-vignette-header {    margin: 0 18px 18px auto;    font-family: arial, sans-serif;    font-size: 12px;    color: #FFFFFF;    text-align: right;    padding: 4px 8px;    background: black;    position: absolute;    bottom: 0;    right: 0;    z-index: 1;    font-weight: 700;}#cpex-vignette-close {    position: absolute;    right: 0px;    top: 0px;    background: none;    border: none;}',
       adUnits: ['centerVignette'],
@@ -71,46 +70,37 @@ window.cpexPublisherSettings = {
   },
   general: {
     onLoad: /*S*/() => {
-    console.log('loglog ');
-    window.cX = window.cX || {};
-    window.cX.callQueue = window.cX.callQueue || [];
-    console.log('cX initialized or exists:', window.cX);
-
-    window.cX.callQueue.push(['invoke', () => {
-        console.log('invoke called');
-        
-        window.__tcfapi('addEventListener', 2, (data, success) => {
-            console.log('addEventListener callback triggered');
-            
-            if (success === false) {
-                console.log('TCF API call failed');
-                return;
-            }
-            
-            if (data.vendor.consents[570] && data.vendor.consents[755]) {
-                console.log('Required vendor consents are present');
-
-                const pianoId = window.cX.getCxenseUserId();
-                window.pianoId = pianoId;
-                console.log('window.pianoId:', window.pianoId);  // Výpis pianoId do konzole
-
-                if (window.pianoId) {
-                    console.log('pianoId exists, adding element to DOM');
-                    window.cpexPackage.utils.addElement('img', document.body, {
-                        src: 'https://x.cz/?=' + pianoId,
-                        width: 0,
-                        height: 0,
-                        style: 'display: block',
-                    });
-                } else {
-                    console.log('pianoId is undefined or null');
-                }
-            } else {
-                console.log('Required vendor consents are missing');
-            }
-        });
-    }]);
-}
-/*E*/
+     window.cX = window.cX || {};
+     window.cX.callQueue = window.cX.callQueue || [];
+     window.cX.callQueue.push(['invoke', () => {
+       window.__tcfapi('addEventListener', 2, (data, success) => {
+         if (success === false) {
+           return;
+         }
+         if (data.vendor.consents[570]) {
+           const segments = window.cX.getUserSegmentIds({
+             persistedQueryId: '51ff14b454af0cf4aedc891fee56b86c1aa69a31',
+           });
+           if (Array.isArray(segments) && segments.length) {
+             window.cpexPackage.utils.addElement('iframe', document.body, {
+               src: 'https://cdn.cpex.cz/cookies/save.html?name=exc&time=1209600&data=' + encodeURIComponent(segments.toString()),
+               width: 0,
+               height: 0,
+               style: 'border: none; display: block',
+             });
+           }
+           const pianoId = window.cX.getCxenseUserId();
+           if (pianoId && data.vendor.consents[755]) {
+             window.cpexPackage.utils.addElement('img', document.body, {
+               src: 'https://cm.g.doubleclick.net/pixel?google_nid=cpex_ddp&process_consent=T&google_cm&&cxsite=1140709865545450804&cxckp=' + pianoId,
+               width: 0,
+               height: 0,
+               style: 'display: block',
+             });
+           }
+         }
+       });
+     }]);
+   } /*E*/
   }
 }
