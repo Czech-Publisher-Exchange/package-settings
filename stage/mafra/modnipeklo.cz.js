0d87681663af9936e5924fd604c28f06bcc6b2c5
CPEx Dev <dev@cpex.cz>
2024-10-08 11:49:57 +0200

    Markéta: Criteo nové uid

---
+++ stage/mafra/modnipeklo.cz.js
@@ -0,0 +1,648 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'modnipeklo.cz'
+  },
+  headerbidding: {
+    adUnits: [{
+      code: '300x250',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [250, 250],
+            [300, 50],
+            [300, 100],
+            [300, 150],
+            [200, 200]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 144582,
+          zoneId: 1140930,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 14, 44, 19, 13]
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4641398'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1924127,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'fragile-adform',
+        params: {
+          mid: 1955107,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 878589
+        }
+      }, {
+        bidder: 'teads',
+        params: {
+          placementId: 222094,
+          pageId: 205838
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 27157823
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 418806
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'modnipeklo.cz',
+          p: '300x250',
+          m: 0,
+          g: 'hb'
+        }
+      }, {
+        bidder: 'smart',
+        params: {
+          domain: '//prg.smartadserver.com',
+          siteId: 673771,
+          pageId: 2011236,
+          formatId: 108743,
+          tagId: 'sas_108743'
+        }
+      }, {
+        bidder: 'connectad',
+        params: {
+          siteId: 3157916,
+          networkId: 131
+        }
+      }, {
+        bidder: 'sspBC'
+      }]
+    }, {
+      code: 'branding',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [2000, 1400],
+            [998, 200],
+            [728, 90],
+            [970, 210],
+            [970, 90],
+            [2000, 1200],
+            [1920, 1080]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 144582,
+          zoneId: 731446,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [230, 259, 2, 96, 55, 265]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 144584,
+          zoneId: 731560,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [230, 259, 2, 96, 55, 265]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4641399'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1924131,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'fragile-adform',
+        params: {
+          mid: 1955106,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 878590
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 27157824
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 418806
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'modnipeklo.cz',
+          p: 'branding',
+          m: 0,
+          g: 'hb'
+        }
+      }, {
+        bidder: 'smart',
+        params: {
+          domain: '//prg.smartadserver.com',
+          siteId: 673771,
+          pageId: 2011236,
+          formatId: 108619,
+          tagId: 'sas_108619'
+        }
+      }, {
+        bidder: 'connectad',
+        params: {
+          siteId: 3157917,
+          networkId: 131
+        }
+      }, {
+        bidder: 'sspBC'
+      }]
+    }, {
+      code: 'halfpagead',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [300, 300],
+            [300, 250],
+            [160, 600],
+            [250, 250],
+            [120, 600],
+            [250, 360],
+            [250, 600]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 144582,
+          zoneId: 731426,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 48, 15, 9, 14, 8, 32, 179]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 144584,
+          zoneId: 731556,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 48, 15, 9, 14, 8, 32, 179]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4641400'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1924126,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'fragile-adform',
+        params: {
+          mid: 1955104,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 878591
+        }
+      }, {
+        bidder: 'teads',
+        params: {
+          placementId: 222096,
+          pageId: 205840
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 27157825
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 418806
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'modnipeklo.cz',
+          p: '300x600',
+          m: 0,
+          g: 'hb'
+        }
+      }, {
+        bidder: 'smart',
+        params: {
+          domain: '//prg.smartadserver.com',
+          siteId: 673771,
+          pageId: 2011236,
+          formatId: 108618,
+          tagId: 'sas_108618'
+        }
+      }, {
+        bidder: 'connectad',
+        params: {
+          siteId: 3157918,
+          networkId: 131
+        }
+      }, {
+        bidder: 'sspBC'
+      }]
+    }, {
+      code: 'halfpagead_spodni',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [300, 300],
+            [300, 250],
+            [160, 600],
+            [250, 250],
+            [120, 600],
+            [250, 360],
+            [250, 600]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 144582,
+          zoneId: 1139988,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 48, 15, 9, 14, 8, 32, 179]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 144584,
+          zoneId: 1139986,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [10, 48, 15, 9, 14, 8, 32, 179]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4641401'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1924128,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'fragile-adform',
+        params: {
+          mid: 1955108,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 878592
+        }
+      }, {
+        bidder: 'teads',
+        params: {
+          placementId: 222097,
+          pageId: 205841
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 27157827
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 418806
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'modnipeklo.cz',
+          p: '300x600_spodni',
+          m: 0,
+          g: 'hb'
+        }
+      }, {
+        bidder: 'smart',
+        params: {
+          domain: '//prg.smartadserver.com',
+          siteId: 673771,
+          pageId: 2011236,
+          formatId: 108739,
+          tagId: 'sas_108739'
+        }
+      }, {
+        bidder: 'connectad',
+        params: {
+          siteId: 3157919,
+          networkId: 131
+        }
+      }, {
+        bidder: 'sspBC'
+      }]
+    }, {
+      code: 'm_300x250',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [250, 250],
+            [480, 480],
+            [300, 50],
+            [300, 100],
+            [300, 150],
+            [200, 200]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 157330,
+          zoneId: 1145528,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 14, 261, 44, 19, 13]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 157332,
+          zoneId: 1145526,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 14, 261, 44, 19, 13]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4641402'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1924129,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'fragile-adform',
+        params: {
+          mid: 1955109,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 878593
+        }
+      }, {
+        bidder: 'teads',
+        params: {
+          placementId: 222098,
+          pageId: 205842
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 27157832
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 418806
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'modnipeklo.cz',
+          p: '300x250',
+          m: 1,
+          g: 'hb'
+        }
+      }, {
+        bidder: 'smart',
+        params: {
+          domain: '//prg.smartadserver.com',
+          siteId: 673771,
+          pageId: 2011236,
+          formatId: 108759,
+          tagId: 'sas_108759'
+        }
+      }, {
+        bidder: 'connectad',
+        params: {
+          siteId: 3157920,
+          networkId: 131
+        }
+      }, {
+        bidder: 'sspBC'
+      }]
+    }, {
+      code: 'm_branding',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [250, 250],
+            [480, 820],
+            [300, 600],
+            [720, 1280],
+            [768, 1230],
+            [480, 480],
+            [300, 300],
+            [160, 600],
+            [120, 600],
+            [338, 280]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 157330,
+          zoneId: 1148158,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 14, 256, 10, 484, 261, 48, 9, 8]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 157332,
+          zoneId: 1148156,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 14, 256, 10, 484, 261, 48, 9, 8]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4641403'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1924130,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'fragile-adform',
+        params: {
+          mid: 1955110,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 844207
+        }
+      }, {
+        bidder: 'teads',
+        params: {
+          placementId: 222099,
+          pageId: 205843
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 27157834
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 418806
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'modnipeklo.cz',
+          p: 'interscroller',
+          m: 1,
+          g: 'hb'
+        }
+      }, {
+        bidder: 'smart',
+        params: {
+          domain: '//prg.smartadserver.com',
+          siteId: 673771,
+          pageId: 2011236,
+          formatId: 108767,
+          tagId: 'sas_108767'
+        }
+      }, {
+        bidder: 'connectad',
+        params: {
+          siteId: 3157921,
+          networkId: 131
+        }
+      }, {
+        bidder: 'sspBC'
+      }]
+    }, {
+      code: 'm_paticka',
+      mediaTypes: {},
+      bids: [{
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 418806
+        }
+      }, {
+        bidder: 'connectad',
+        params: {
+          siteId: 3157922,
+          networkId: 131
+        }
+      }, {
+        bidder: 'sspBC'
+      }]
+    }, {
+      code: 'm_300x250_souvisejici',
+      mediaTypes: {},
+      bids: [{
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 418806
+        }
+      }, {
+        bidder: 'connectad',
+        params: {
+          siteId: 3157923,
+          networkId: 131
+        }
+      }, {
+        bidder: 'sspBC'
+      }]
+    }]
+  },
+  formats: {
+    interscroller: {
+      enabled: true
+    },
+    slideup: {
+      enabled: true,
+      adUnits: ['m_sticky']
+    }
+  }
+}
