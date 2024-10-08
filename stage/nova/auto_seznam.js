b6a6eaecebefb32c599044070d168351eb575297
CPEx Dev <dev@cpex.cz>
2024-10-08 12:49:55 +0200

    Aleš: Přemapování Criteo uid

---
+++ stage/nova/auto_seznam.js
@@ -0,0 +1,813 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'tn.nova.cz/auto'
+  },
+  adserver: {
+    adapter: 'sastracker'
+  },
+  headerbidding: {
+    adUnits: [{
+      code: 'div-ad-leaderboard',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [2000, 1400],
+            [970, 250],
+            [970, 210],
+            [970, 310],
+            [998, 200]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 416558,
+          zoneId: 3335250,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [230, 57, 96, 94, 259]
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '5851856'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1878670,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1881000,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1094844
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 32431419
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          publisherSubId: 'nova_sz',
+          uid: 418803
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'tn.cz',
+          p: 'branding_seznamHP',
+          m: 0,
+          g: 'hb'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'tn.cz',
+          p: '1000x250_seznamHP',
+          m: 0,
+          g: 'hb'
+        }
+      }],
+      filter: {
+        minWidth: 992
+      }
+    }, {
+      code: 'div-ad-rectangle-bottom',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [336, 280],
+            [480, 300],
+            [300, 300],
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 416558,
+          zoneId: 3335264,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [16, 83, 48, 15]
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '5851863'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1878363,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1094851
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 32431417
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          publisherSubId: 'nova_sz',
+          uid: 418803
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'tn.cz',
+          p: '480x300_2_SeznamHP',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'div-ad-rectangle-mobile1',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [300, 300],
+            [480, 820],
+            [336, 280],
+            [720, 1280]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 416562,
+          zoneId: 3335254,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 48, 256, 16, 484]
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '5851858'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1878674,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1878734,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1094846
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 32431412
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          publisherSubId: 'nova_sz',
+          uid: 418803
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'tn.cz',
+          p: '300x300_1_seznamHP',
+          m: 1,
+          g: 'hb'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'tn.cz',
+          p: 'interscroller_1_seznamHP',
+          m: 1,
+          g: 'hb'
+        }
+      }],
+      filter: {
+        maxWidth: 991
+      }
+    }, {
+      code: 'div-ad-rectangle-mobile2',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [300, 300],
+            [480, 820],
+            [336, 280],
+            [720, 1280]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 416562,
+          zoneId: 3335256,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 48, 256, 16, 484]
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '5851859'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1878675,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1878736,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1094847
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 32431413
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          publisherSubId: 'nova_sz',
+          uid: 418803
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'tn.cz',
+          p: '300x300_2_seznamHP',
+          m: 1,
+          g: 'hb'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'tn.cz',
+          p: 'interscroller_2_seznamHP',
+          m: 1,
+          g: 'hb'
+        }
+      }],
+      filter: {
+        maxWidth: 991
+      }
+    }, {
+      code: 'div-ad-rectangle-mobile3',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [300, 300],
+            [480, 820],
+            [336, 280],
+            [720, 1280]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 416562,
+          zoneId: 3335258,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 48, 256, 16, 484]
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '5851860'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1878676,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1878739,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1094848
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 32431414
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          publisherSubId: 'nova_sz',
+          uid: 418803
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'tn.cz',
+          p: '300x300_3_seznamHP',
+          m: 1,
+          g: 'hb'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'tn.cz',
+          p: 'interscroller_3_seznamHP',
+          m: 1,
+          g: 'hb'
+        }
+      }],
+      filter: {
+        maxWidth: 991
+      }
+    }, {
+      code: 'div-ad-rectangle-right1',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [160, 600],
+            [300, 300],
+            [300, 250],
+            [300, 600]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 416558,
+          zoneId: 3335260,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [9, 48, 15, 10]
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '5851861'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1878367,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1094849
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 32431415
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          publisherSubId: 'nova_sz',
+          uid: 418803
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'tn.cz',
+          p: '300x600_SeznamHP',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'div-ad-rectangle-right2',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [160, 600],
+            [300, 300],
+            [300, 250],
+            [300, 600]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 416558,
+          zoneId: 3335262,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [9, 48, 15, 10]
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '5851862'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1878368,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1094850
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 32431416
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          publisherSubId: 'nova_sz',
+          uid: 418803
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'tn.cz',
+          p: '300x600_2_SeznamHP',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'div-ad-rectangle-top',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [336, 280],
+            [480, 300],
+            [300, 300],
+            [300, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 416558,
+          zoneId: 3335266,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [16, 83, 48, 15]
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '5851864'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1878364,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1094852
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 32431418
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          publisherSubId: 'nova_sz',
+          uid: 418803
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'tn.cz',
+          p: '480x300_SeznamHP',
+          m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'div-ad-gallery-mobile1',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [300, 300],
+            [480, 820],
+            [336, 280],
+            [720, 1280]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 416562,
+          zoneId: 3335252,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 48, 256, 16, 484]
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '5851857'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1094845
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 32431411
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          publisherSubId: 'nova_sz',
+          uid: 418803
+        }
+      }],
+      filter: {
+        maxWidth: 991
+      }
+    }, {
+      code: 'div-ad-gallery-mobile2',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [300, 300],
+            [480, 820],
+            [336, 280],
+            [720, 1280]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 416562,
+          zoneId: 3335252,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 48, 256, 16, 484]
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '5851857'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1094845
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 32431411
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          publisherSubId: 'nova_sz',
+          uid: 418803
+        }
+      }],
+      filter: {
+        maxWidth: 991
+      }
+    }, {
+      code: 'div-ad-gallery-mobile3',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [300, 300],
+            [480, 820],
+            [336, 280],
+            [720, 1280]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 416562,
+          zoneId: 3335252,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 48, 256, 16, 484]
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '5851857'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1094845
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 32431411
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          publisherSubId: 'nova_sz',
+          uid: 418803
+        }
+      }],
+      filter: {
+        maxWidth: 991
+      }
+    }, {
+      code: 'div-ad-gallery-mobile4',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [300, 300],
+            [480, 820],
+            [336, 280],
+            [720, 1280]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 416562,
+          zoneId: 3335252,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 48, 256, 16, 484]
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '5851857'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1094845
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 32431411
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          publisherSubId: 'nova_sz',
+          uid: 418803
+        }
+      }],
+      filter: {
+        maxWidth: 991
+      }
+    }, {
+      code: 'div-ad-gallery-mobile5',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [300, 300],
+            [480, 820],
+            [336, 280],
+            [720, 1280]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 416562,
+          zoneId: 3335252,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [15, 48, 256, 16, 484]
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '5851857'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1094845
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 32431411
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          publisherSubId: 'nova_sz',
+          uid: 418803
+        }
+      }],
+      filter: {
+        maxWidth: 991
+      }
+    }]
+  },
+  formats: {
+    skin: {
+      offset: 80,
+      contentEl: /*S*/ document.getElementsByClassName('c-content')[0] /*E*/,
+      enabled: true,
+      contentCSS: 'position: relative;margin: auto;margin-top: 280px;padding-top: 0px;background-color: var(--theme-bg, white);',
+      generalCSS: '.c-branding .c-leaderboard {   height: 0px;   min-height: 0px }body .c-leaderboard #div-ad-leaderboard {   width: 1920px;    height: 0;    min-height: 0;    position: fixed;    left: 50%;    top: 83px;    transform: translateX(-50%);}body .c-leaderboard #div-ad-leaderboard .banner-highlight {    height: 1400px;}'
+    },
+    interscroller: {
+      enabled: true,
+      height: '300px'
+    }
+  }
+}
