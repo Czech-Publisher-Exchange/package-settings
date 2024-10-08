41c19da2cc3427fe077869440aa3a921fac14598
CPEx Dev <dev@cpex.cz>
2024-10-08 10:45:16 +0200

    Markéta: Criteo nové uid

---
+++ stage/mafra/autozive.cz.js
@@ -0,0 +1,75 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'autozive.cz'
+  },
+  formats: {
+    slideup: {
+      adUnits: ['m_sticky']
+    },
+    skin: {
+      generalCSS: '#r-leaderboardhp {  height: 200px;  margin-bottom: 0px;}#page {  z-index:1;}',
+      zIndex: 1
+    }
+  },
+  headerbidding: {
+    adUnits: [{
+      code: 'branding',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [2000, 1400],
+            [998, 200],
+            [728, 90],
+            [970, 210],
+            [970, 90],
+            [2000, 1200],
+            [1920, 1080]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 144582,
+          zoneId: 3134312,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [230, 259, 2, 96, 55, 265]
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '5545919'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1815173,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 31440167
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 418806
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'mafra.autozive.cz',
+          p: 'branding',
+          m: 0,
+          g: 'hb'
+        }
+      }, {
+        bidder: 'sspBC'
+      }]
+    }]
+  }
+}
