bcd1f70efa29e4d7f2b726087ff2624c692d4f36
CPEx Dev <dev@cpex.cz>
2024-10-08 10:47:01 +0200

    Frantisek: criteo uid

--- stage/eco/vareni.cz.js
+++ stage/eco/vareni.cz.js
@@ -44,6 +44,13 @@ window.cpexWebsiteSettings = {
         },
         bidderModuleName: 'pubmatic'
       }, {
+        bidder: 'knowlimits-adform',
+        params: {
+          mid: 1693297,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
         bidder: 'adform_im_hb',
         params: {
           mid: 1334484,
@@ -71,18 +78,19 @@ window.cpexWebsiteSettings = {
       }, {
         bidder: 'teads',
         params: {
-          placementId: 158423,
-          pageId: 144531
+          placementId: 211430,
+          pageId: 195895
         }
       }, {
         bidder: 'appnexus',
         params: {
-          placementId: 21011671
+          placementId: 32622061
         }
       }, {
         bidder: 'criteo',
         params: {
-          networkId: 11172
+          networkId: 11172,
+          uid: 418810
         }
       }, {
         bidder: 'triplelift',
@@ -138,6 +146,13 @@ window.cpexWebsiteSettings = {
         },
         bidderModuleName: 'pubmatic'
       }, {
+        bidder: 'knowlimits-adform',
+        params: {
+          mid: 1840869,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
         bidder: 'adform_im_hb',
         params: {
           mid: 1334499,
@@ -148,6 +163,13 @@ window.cpexWebsiteSettings = {
         bidder: 'adform_im_hb-mask',
         params: {
           mid: 1334501,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'omg-adform',
+        params: {
+          mid: 1869826,
           rcur: 'USD'
         },
         bidderModuleName: 'adform'
@@ -165,8 +187,8 @@ window.cpexWebsiteSettings = {
       }, {
         bidder: 'teads',
         params: {
-          placementId: 158424,
-          pageId: 144532
+          placementId: 211429,
+          pageId: 195894
         }
       }, {
         bidder: 'appnexus',
@@ -176,7 +198,8 @@ window.cpexWebsiteSettings = {
       }, {
         bidder: 'criteo',
         params: {
-          networkId: 11172
+          networkId: 11172,
+          uid: 418810
         }
       }, {
         bidder: 'triplelift',
@@ -230,7 +253,7 @@ window.cpexWebsiteSettings = {
         },
         bidderModuleName: 'pubmatic'
       }, {
-        bidder: 'adform_knwl_hb',
+        bidder: 'knowlimits-adform',
         params: {
           mid: 1693298,
           rcur: 'USD'
@@ -251,6 +274,13 @@ window.cpexWebsiteSettings = {
         },
         bidderModuleName: 'adform'
       }, {
+        bidder: 'omg-adform',
+        params: {
+          mid: 1869825,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
         bidder: 'adform',
         params: {
           mid: 1109660,
@@ -264,8 +294,8 @@ window.cpexWebsiteSettings = {
       }, {
         bidder: 'teads',
         params: {
-          placementId: 153680,
-          pageId: 140016
+          placementId: 211434,
+          pageId: 195899
         }
       }, {
         bidder: 'appnexus',
@@ -275,7 +305,8 @@ window.cpexWebsiteSettings = {
       }, {
         bidder: 'criteo',
         params: {
-          networkId: 11172
+          networkId: 11172,
+          uid: 418810
         }
       }, {
         bidder: 'triplelift',
@@ -329,7 +360,7 @@ window.cpexWebsiteSettings = {
         },
         bidderModuleName: 'pubmatic'
       }, {
-        bidder: 'adform_knwl_hb',
+        bidder: 'knowlimits-adform',
         params: {
           mid: 1693299,
           rcur: 'USD'
@@ -363,8 +394,8 @@ window.cpexWebsiteSettings = {
       }, {
         bidder: 'teads',
         params: {
-          placementId: 158428,
-          pageId: 144536
+          placementId: 211425,
+          pageId: 195890
         }
       }, {
         bidder: 'appnexus',
@@ -374,7 +405,8 @@ window.cpexWebsiteSettings = {
       }, {
         bidder: 'criteo',
         params: {
-          networkId: 11172
+          networkId: 11172,
+          uid: 418810
         }
       }, {
         bidder: 'triplelift',
@@ -429,7 +461,7 @@ window.cpexWebsiteSettings = {
         },
         bidderModuleName: 'pubmatic'
       }, {
-        bidder: 'adform_knwl_hb',
+        bidder: 'knowlimits-adform',
         params: {
           mid: 1693300,
           rcur: 'USD'
@@ -450,6 +482,13 @@ window.cpexWebsiteSettings = {
         },
         bidderModuleName: 'adform'
       }, {
+        bidder: 'omg-adform',
+        params: {
+          mid: 1869823,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
         bidder: 'adform',
         params: {
           mid: 1109663,
@@ -463,8 +502,8 @@ window.cpexWebsiteSettings = {
       }, {
         bidder: 'teads',
         params: {
-          placementId: 153681,
-          pageId: 140017
+          placementId: 211433,
+          pageId: 195898
         }
       }, {
         bidder: 'appnexus',
@@ -474,7 +513,8 @@ window.cpexWebsiteSettings = {
       }, {
         bidder: 'criteo',
         params: {
-          networkId: 11172
+          networkId: 11172,
+          uid: 418810
         }
       }, {
         bidder: 'triplelift',
@@ -529,7 +569,7 @@ window.cpexWebsiteSettings = {
         },
         bidderModuleName: 'pubmatic'
       }, {
-        bidder: 'adform_knwl_hb',
+        bidder: 'knowlimits-adform',
         params: {
           mid: 1693301,
           rcur: 'USD'
@@ -550,6 +590,13 @@ window.cpexWebsiteSettings = {
         },
         bidderModuleName: 'adform'
       }, {
+        bidder: 'omg-adform',
+        params: {
+          mid: 1869824,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
         bidder: 'adform',
         params: {
           mid: 1109664,
@@ -563,8 +610,8 @@ window.cpexWebsiteSettings = {
       }, {
         bidder: 'teads',
         params: {
-          placementId: 158426,
-          pageId: 144534
+          placementId: 211427,
+          pageId: 195892
         }
       }, {
         bidder: 'appnexus',
@@ -574,7 +621,8 @@ window.cpexWebsiteSettings = {
       }, {
         bidder: 'criteo',
         params: {
-          networkId: 11172
+          networkId: 11172,
+          uid: 418810
         }
       }, {
         bidder: 'triplelift',
@@ -587,6 +635,261 @@ window.cpexWebsiteSettings = {
           d: 'vareni.cz',
           p: '300x600_pos2',
           m: 0,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'mediumrectangle-mob-pos1',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [480, 480],
+            [300, 300],
+            [300, 250],
+            [250, 250],
+            [300, 50]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 361516,
+          zoneId: 1965836,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [261, 48, 15, 14, 44]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 361522,
+          zoneId: 1965838,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [261, 48, 15, 14, 44]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic_eco_hb',
+        params: {
+          publisherId: '161532',
+          adSlot: '4344944'
+        },
+        bidderModuleName: 'pubmatic'
+      }, {
+        bidder: 'knowlimits-adform',
+        params: {
+          mid: 1840870,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1109666,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 759662
+        }
+      }, {
+        bidder: 'teads',
+        params: {
+          placementId: 211432,
+          pageId: 195897
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 21011829
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 418810
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'ECO_vareni_cz_mediumrectangle_mob_pos_1'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'vareni.cz',
+          p: '300x250_pos1',
+          m: 1,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'mediumrectangle-mob-pos2',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [480, 480],
+            [300, 300],
+            [300, 250],
+            [250, 250],
+            [300, 50]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 361516,
+          zoneId: 1965840,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [261, 48, 15, 14, 44]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 361522,
+          zoneId: 1965842,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [261, 48, 15, 14, 44]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic_eco_hb',
+        params: {
+          publisherId: '161532',
+          adSlot: '4344945'
+        },
+        bidderModuleName: 'pubmatic'
+      }, {
+        bidder: 'knowlimits-adform',
+        params: {
+          mid: 1840871,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1109667,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 759663
+        }
+      }, {
+        bidder: 'teads',
+        params: {
+          placementId: 211426,
+          pageId: 195891
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 21011832
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 418810
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'ECO_vareni_cz_mediumrectangle_mob_pos_2'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'vareni.cz',
+          p: '300x250_pos2',
+          m: 1,
+          g: 'hb'
+        }
+      }]
+    }, {
+      code: 'strip',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [728, 90],
+            [320, 50],
+            [300, 50]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 361516,
+          zoneId: 1965852,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [2, 43, 44]
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 361522,
+          zoneId: 1965854,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [2, 43, 44]
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic_eco_hb',
+        params: {
+          publisherId: '161532',
+          adSlot: '4344943'
+        },
+        bidderModuleName: 'pubmatic'
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1109669,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 759666
+        }
+      }, {
+        bidder: 'teads',
+        params: {
+          placementId: 211431,
+          pageId: 195897
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 21011840
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 418810
+        }
+      }, {
+        bidder: 'triplelift',
+        params: {
+          inventoryCode: 'ECO_varenicz_strip'
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'vareni.cz',
+          p: '320x50',
+          m: 1,
           g: 'hb'
         }
       }]
@@ -620,9 +923,29 @@ window.cpexWebsiteSettings = {
         },
         bidderModuleName: 'rubicon'
       }, {
+        bidder: 'knowlimits-adform',
+        params: {
+          mid: 1840872,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'omg-adform',
+        params: {
+          mid: 1869829,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
         bidder: 'appnexus',
         params: {
           placementId: 28273058
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 418810
         }
       }]
     }],
@@ -631,13 +954,14 @@ window.cpexWebsiteSettings = {
   formats: {
     skin: {
       enabled: true,
-      contentEl: /*S*/ document.getElementsByClassName('bhWtgq')[0] /*E*/ ,
+      contentEl: /*S*/ document.getElementsByClassName('bhWtgq')[0] /*E*/,
       contentCSS: ' ',
       zIndex: null,
       backgroundEl: /*S*/ document.getElementById('brand-c') /*E*/
     },
     interscroller: {
-      enabled: true
+      enabled: true,
+      adUnits: ['reklama-medium-rectangle-1']
     },
     slideup: {
       adUnits: ['sas-special'],
@@ -646,9 +970,6 @@ window.cpexWebsiteSettings = {
     }
   },
   general: {
-    errorPath: 'https://73f2bd72d0d2477ab2f976d6098fe246@o530000.ingest.sentry.io/4504531846365184',
-    onLoad: /*S*/ () => {
-      window.cpexPackage.settings.formats.skin.offset = window._ecohec ? (window._ecohec.desktopNavigationHeight || 0) : 0
-    } /*E*/
+    errorPath: 'https://73f2bd72d0d2477ab2f976d6098fe246@o530000.ingest.sentry.io/4504531846365184'
   }
 }
