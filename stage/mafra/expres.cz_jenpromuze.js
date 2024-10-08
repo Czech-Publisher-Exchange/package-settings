08ce5afe1e9dfc1ef786cdb2e7636abe0ebcf814
CPEx Dev <dev@cpex.cz>
2024-10-08 10:48:40 +0200

    Markéta: Criteo nové uid a Mafra: zapojení Connectad

---
+++ stage/mafra/expres.cz_jenpromuze.js
@@ -0,0 +1,791 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'expres.cz_jenpromuze'
+  },
+  headerbidding: {
+    adUnits: [{
+      code: 'branding',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [2000, 1400],
+            [998, 200],
+            [728, 90],
+            [970, 210],
+            [970, 90],
+            [2000, 1200],
+            [1920, 1080]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4641259'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1924067,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'fragile-adform',
+        params: {
+          mid: 1731525,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 27157266
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 418806
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'mafra.jenpromuze.cz',
+          p: 'branding',
+          m: 0,
+          g: 'hb'
+        }
+      }, {
+        bidder: 'smart',
+        params: {
+          domain: '//prg.smartadserver.com',
+          siteId: 670745,
+          pageId: 2011227,
+          formatId: 108619,
+          tagId: 'sas_108619'
+        }
+      }, {
+        bidder: 'connectad',
+        params: {
+          siteId: 3158874,
+          networkId: 131
+        }
+      }, {
+        bidder: 'sspBC'
+      }]
+    }, {
+      code: 'halfpagead',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [300, 300],
+            [300, 250],
+            [160, 600],
+            [250, 250],
+            [120, 600],
+            [250, 360],
+            [250, 600]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4641260'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1924068,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'fragile-adform',
+        params: {
+          mid: 1731526,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'teads',
+        params: {
+          placementId: 221976,
+          pageId: 205719
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 27157269
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 418806
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'mafra.jenpromuze.cz',
+          p: '300x600',
+          m: 0,
+          g: 'hb'
+        }
+      }, {
+        bidder: 'smart',
+        params: {
+          domain: '//prg.smartadserver.com',
+          siteId: 670745,
+          pageId: 2011227,
+          formatId: 108618,
+          tagId: 'sas_108618'
+        }
+      }, {
+        bidder: 'connectad',
+        params: {
+          siteId: 3158875,
+          networkId: 131
+        }
+      }, {
+        bidder: 'sspBC'
+      }]
+    }, {
+      code: 'halfpagead_spodni',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [300, 300],
+            [300, 250],
+            [160, 600],
+            [250, 250],
+            [120, 600],
+            [250, 360],
+            [250, 600]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4641264'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1924072,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'fragile-adform',
+        params: {
+          mid: 1731520,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'teads',
+        params: {
+          placementId: 221977,
+          pageId: 205720
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 27157280
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 418806
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'mafra.jenpromuze.cz',
+          p: '300x600_spodni',
+          m: 0,
+          g: 'hb'
+        }
+      }, {
+        bidder: 'smart',
+        params: {
+          domain: '//prg.smartadserver.com',
+          siteId: 670745,
+          pageId: 2011227,
+          formatId: 108739,
+          tagId: 'sas_108739'
+        }
+      }, {
+        bidder: 'connectad',
+        params: {
+          siteId: 3158876,
+          networkId: 131
+        }
+      }, {
+        bidder: 'sspBC'
+      }]
+    }, {
+      code: 'halfpagead_rubrika',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [300, 300],
+            [300, 250],
+            [160, 600],
+            [250, 250],
+            [120, 600],
+            [250, 360],
+            [250, 600]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4641262'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1924070,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'fragile-adform',
+        params: {
+          mid: 1731518,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'teads',
+        params: {
+          placementId: 221978,
+          pageId: 205721
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 27157271
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 418806
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'mafra.jenpromuze.cz',
+          p: '300x600_rubrika',
+          m: 0,
+          g: 'hb'
+        }
+      }, {
+        bidder: 'smart',
+        params: {
+          domain: '//prg.smartadserver.com',
+          siteId: 670745,
+          pageId: 2011227,
+          formatId: 108740,
+          tagId: 'sas_108740'
+        }
+      }, {
+        bidder: 'connectad',
+        params: {
+          siteId: 3158877,
+          networkId: 131
+        }
+      }, {
+        bidder: 'sspBC'
+      }]
+    }, {
+      code: 'halfpagead_rubrika_spodni',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [300, 300],
+            [300, 250],
+            [160, 600],
+            [250, 250],
+            [120, 600],
+            [250, 360],
+            [250, 600]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4641263'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1924071,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'fragile-adform',
+        params: {
+          mid: 1731519,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'teads',
+        params: {
+          placementId: 221979,
+          pageId: 205722
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 27157272
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 418806
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'mafra.jenpromuze.cz',
+          p: '300x600_rubrika_spodni',
+          m: 0,
+          g: 'hb'
+        }
+      }, {
+        bidder: 'smart',
+        params: {
+          domain: '//prg.smartadserver.com',
+          siteId: 670745,
+          pageId: 2011227,
+          formatId: 108741,
+          tagId: 'sas_108741'
+        }
+      }, {
+        bidder: 'connectad',
+        params: {
+          siteId: 3158878,
+          networkId: 131
+        }
+      }, {
+        bidder: 'sspBC'
+      }]
+    }, {
+      code: 'halfpagead_fotogalerie',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600],
+            [300, 300],
+            [300, 250],
+            [160, 600],
+            [250, 250],
+            [120, 600],
+            [250, 360],
+            [250, 600]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4641261'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1924069,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'fragile-adform',
+        params: {
+          mid: 1731517,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'teads',
+        params: {
+          placementId: 221980,
+          pageId: 205723
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 27157270
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 418806
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'mafra.jenpromuze.cz',
+          p: '300x600_fotogalerie',
+          m: 0,
+          g: 'hb'
+        }
+      }, {
+        bidder: 'smart',
+        params: {
+          domain: '//prg.smartadserver.com',
+          siteId: 670745,
+          pageId: 2011227,
+          formatId: 108742,
+          tagId: 'sas_108742'
+        }
+      }, {
+        bidder: 'connectad',
+        params: {
+          siteId: 3158879,
+          networkId: 131
+        }
+      }, {
+        bidder: 'sspBC'
+      }]
+    }, {
+      code: 'm_300x250',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [250, 250],
+            [480, 480],
+            [300, 50],
+            [300, 100],
+            [300, 150],
+            [200, 200]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4641265'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1924073,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'fragile-adform',
+        params: {
+          mid: 1731521,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'teads',
+        params: {
+          placementId: 221981,
+          pageId: 205724
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 27157281
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 418806
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'mafra.jenpromuze.cz',
+          p: '300x250',
+          m: 1,
+          g: 'hb'
+        }
+      }, {
+        bidder: 'smart',
+        params: {
+          domain: '//prg.smartadserver.com',
+          siteId: 670745,
+          pageId: 2011227,
+          formatId: 108759,
+          tagId: 'sas_108759'
+        }
+      }, {
+        bidder: 'connectad',
+        params: {
+          siteId: 3158880,
+          networkId: 131
+        }
+      }, {
+        bidder: 'sspBC'
+      }]
+    }, {
+      code: 'm_branding',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [250, 250],
+            [480, 820],
+            [300, 600],
+            [720, 1280],
+            [768, 1230],
+            [480, 480],
+            [300, 300],
+            [160, 600],
+            [120, 600],
+            [338, 280]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4641266'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1924074,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'fragile-adform',
+        params: {
+          mid: 1731522,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'teads',
+        params: {
+          placementId: 221982,
+          pageId: 205725
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 27157283
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 418806
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'mafra.jenpromuze.cz',
+          p: 'interscroller',
+          m: 1,
+          g: 'hb'
+        }
+      }, {
+        bidder: 'smart',
+        params: {
+          domain: '//prg.smartadserver.com',
+          siteId: 670745,
+          pageId: 2011227,
+          formatId: 108767,
+          tagId: 'sas_108767'
+        }
+      }, {
+        bidder: 'connectad',
+        params: {
+          siteId: 3158881,
+          networkId: 131
+        }
+      }, {
+        bidder: 'sspBC'
+      }]
+    }, {
+      code: 'm_paticka',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 250],
+            [250, 250],
+            [480, 480],
+            [300, 50],
+            [300, 100],
+            [300, 150],
+            [200, 200]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4641267'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1924075,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'fragile-adform',
+        params: {
+          mid: 1731523,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'teads',
+        params: {
+          placementId: 221983,
+          pageId: 205726
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 27157285
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 418806
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'mafra.jenpromuze.cz',
+          p: '300x250_paticka',
+          m: 1,
+          g: 'hb'
+        }
+      }, {
+        bidder: 'smart',
+        params: {
+          domain: '//prg.smartadserver.com',
+          siteId: 670745,
+          pageId: 2011227,
+          formatId: 108768,
+          tagId: 'sas_108768'
+        }
+      }, {
+        bidder: 'connectad',
+        params: {
+          siteId: 3158882,
+          networkId: 131
+        }
+      }, {
+        bidder: 'sspBC'
+      }]
+    }, {
+      code: 'widesquare',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [480, 300],
+            [300, 300],
+            [250, 250],
+            [300, 250],
+            [480, 480],
+            [336, 280],
+            [200, 200],
+            [320, 50]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '4641268'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1924076,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'fragile-adform',
+        params: {
+          mid: 1731524,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'teads',
+        params: {
+          placementId: 221984,
+          pageId: 205727
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 27157286
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 418806
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'mafra.jenpromuze.cz',
+          p: '480x300',
+          m: 0,
+          g: 'hb'
+        }
+      }, {
+        bidder: 'smart',
+        params: {
+          domain: '//prg.smartadserver.com',
+          siteId: 670745,
+          pageId: 2011227,
+          formatId: 108753,
+          tagId: 'sas_108753'
+        }
+      }, {
+        bidder: 'connectad',
+        params: {
+          siteId: 3158883,
+          networkId: 131
+        }
+      }, {
+        bidder: 'sspBC'
+      }]
+    }]
+  },
+  formats: {
+    interscroller: {
+      enabled: true
+    },
+    slideup: {
+      enabled: true,
+      adUnits: ['m_sticky']
+    }
+  }
+}
