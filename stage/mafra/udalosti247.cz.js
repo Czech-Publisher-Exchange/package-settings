b246aba5fa615efa8128262d75e99674d2ff9dbb
CPEx Dev <dev@cpex.cz>
2024-10-08 11:54:21 +0200

    Markéta: Criteo nové uid

---
+++ stage/mafra/udalosti247.cz.js
@@ -0,0 +1,66 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'udalosti247.cz'
+  },
+  headerbidding: {
+    adUnits: [{
+      code: 'branding',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [2000, 1400],
+            [998, 200],
+            [728, 90],
+            [970, 210],
+            [970, 90],
+            [2000, 1200],
+            [1920, 1080]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 144582,
+          zoneId: 3178142,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [230, 259, 2, 96, 55, 265]
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '5652823'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1835441,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 31700238
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 418806
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'mafra.udalosti247',
+          p: 'branding',
+          m: 0,
+          g: 'hb'
+        }
+      }, {
+        bidder: 'sspBC'
+      }]
+    }]
+  }
+}
