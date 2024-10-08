d649d625ab3317afc9b79cb560205d6939d16cb3
CPEx Dev <dev@cpex.cz>
2024-10-08 10:30:05 +0200

    Markéta: Criteo nové uid

---
+++ stage/mafra/adbz.cz.js
@@ -0,0 +1,65 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'adbz.cz'
+  },
+  headerbidding: {
+    adUnits: [{
+      code: 'branding',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [2000, 1400],
+            [998, 200],
+            [728, 90],
+            [970, 210],
+            [970, 90],
+            [2000, 1200],
+            [1920, 1080]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 144582,
+          zoneId: 3134316,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [230, 259, 2, 96, 55, 265]
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1815172,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1053063
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 31440172
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 418806
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'mafra.adbz.cz',
+          p: 'branding',
+          m: 0,
+          g: 'hb'
+        }
+      }, {
+        bidder: 'sspBC'
+      }]
+    }]
+  }
+}
