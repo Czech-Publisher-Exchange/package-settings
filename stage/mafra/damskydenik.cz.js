a59d73ddf80d0c085e2e72f35da98a8755e86318
CPEx Dev <dev@cpex.cz>
2024-10-08 10:46:04 +0200

    Markéta: Criteo nové uid

---
+++ stage/mafra/damskydenik.cz.js
@@ -0,0 +1,71 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'damskydenik.cz'
+  },
+  headerbidding: {
+    adUnits: [{
+      code: 'branding',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [2000, 1400],
+            [998, 200],
+            [728, 90],
+            [970, 210],
+            [970, 90],
+            [2000, 1200],
+            [1920, 1080]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 144582,
+          zoneId: 3178140,
+          accountId: '10900',
+          position: 'atf',
+          sizes: [230, 259, 2, 96, 55, 265]
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '5652822'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 1835440,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1065813
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 31700237
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 418806
+        }
+      }, {
+        bidder: 'r2b2',
+        params: {
+          d: 'mafra.damskydenik.cz',
+          p: 'branding',
+          m: 0,
+          g: 'hb'
+        }
+      }, {
+        bidder: 'sspBC'
+      }]
+    }]
+  }
+}
