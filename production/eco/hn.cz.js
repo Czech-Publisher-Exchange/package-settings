e7c732f44138d57a010328c5f6c8182f16872922
CPEx Dev <dev@cpex.cz>
2024-11-07 16:22:06 +0100

    Frantisek: uprava onload, aby zde nebyl offset

--- production/eco/hn.cz.js
+++ production/eco/hn.cz.js
@@ -485,6 +485,31 @@ window.cpexWebsiteSettings = {
     }
   },
   general: {
-    errorPath: 'https://73f2bd72d0d2477ab2f976d6098fe246@o530000.ingest.sentry.io/4504531846365184'
+    errorPath: 'https://73f2bd72d0d2477ab2f976d6098fe246@o530000.ingest.sentry.io/4504531846365184',
+    onLoad: /*S*/() => {
+     /* piano segment export */
+     window.cX = window.cX || {};
+     window.cX.callQueue = window.cX.callQueue || [];
+     window.cX.callQueue.push(['invoke', () => {
+       window.__tcfapi('addEventListener', 2, (data, success) => {
+         if (success === false) { return; }
+         if (data.vendor.consents[570]) {
+           const segments = window.cX.getUserSegmentIds({ persistedQueryId: '51ff14b454af0cf4aedc891fee56b86c1aa69a31' });
+           if (Array.isArray(segments) && segments.length) { /* use html from cdn.cpex.cz to save it as a 3rd party cookie, for 14 days */
+             window.cpexPackage.utils.addElement('iframe', document.body, { src: 'https://cdn.cpex.cz/cookies/save.html?name=exc&time=1209600&data=' + encodeURIComponent(segments.toString()), width: 0, height: 0, style: 'border: none; display: block' });
+           }
+           const pianoId = window.cX.getCxenseUserId();
+           if (pianoId && data.vendor.consents[755]) {
+             window.cpexPackage.utils.addElement('img', document.body, {
+               src: 'https://cm.g.doubleclick.net/pixel?google_nid=cpex_ddp&process_consent=T&google_cm&cxsite=4732541702467398411&cxckp=' + pianoId,
+               width: 0,
+               height: 0,
+               style: 'display: block',
+             });
+           }
+         }
+       })
+     }])
+   } /*E*/
   }
 }
