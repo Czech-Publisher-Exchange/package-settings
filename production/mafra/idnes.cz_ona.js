0fd32ab478dfd468657384d258e1220b1185d74d
CPEx Dev <dev@cpex.cz>
2024-11-22 12:26:51 +0100

    Frantisek: hb adform outstream

--- production/mafra/idnes.cz_ona.js
+++ production/mafra/idnes.cz_ona.js
@@ -4037,6 +4037,26 @@ window.cpexWebsiteSettings = {
           uid: 418807
         }
       }]
+    }, {
+      code: 'outstream',
+      mediaTypes: {
+        video: {
+          playerSize: [
+            [600, 400]
+          ],
+          type: 'outstream'
+        }
+      },
+      bids: [{
+        bidder: 'adform',
+        params: {
+          mid: 2016605,
+          rcur: 'USD'
+        }
+      }],
+      video: {
+        divId: 'outstream'
+      }
     }]
   },
   formats: {
