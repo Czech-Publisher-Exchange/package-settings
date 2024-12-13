a6af528f095d2a58771567393a61f3e1850c34ac
CPEx Dev <dev@cpex.cz>
2024-12-13 14:00:29 +0100

    Lucie Špytková: zapojení RTB House

--- production/mafra/vedazive.cz.js
+++ production/mafra/vedazive.cz.js
@@ -39,6 +39,12 @@ window.cpexWebsiteSettings = {
           placementId: 31700236
         }
       }, {
+        bidder: 'rtbhouse',
+        params: {
+          publisherId: '7LTOeORMcRSgGMug4SXQ',
+          region: 'prebid-eu'
+        }
+      }, {
         bidder: 'criteo',
         params: {
           networkId: 11172,
