a1e389a3251cc531e97d5be1c53a02620b08cf5a
CPEx Dev <dev@cpex.cz>
2025-01-15 12:24:03 +0100

    Frantisek: inicializace settings finmag.cz

---
+++ production/mafra/finmag.cz.js
@@ -0,0 +1,70 @@
+window.cpexWebsiteSettings = {
+  headerbidding: {
+    adUnits: [{
+      code: 'm_sticky',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [320, 150],
+            [300, 250],
+            [500, 200],
+            [320, 100],
+            [970, 250],
+            [336, 280],
+            [320, 50],
+            [728, 90],
+            [970, 310],
+            [480, 480],
+            [300, 300],
+            [480, 300],
+            [998, 200],
+            [300, 50],
+            [300, 100],
+            [970, 210],
+            [750, 200],
+            [750, 100]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 157330,
+          zoneId: 3667286,
+          accountId: '10900',
+          position: 'atf'
+        }
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '6575564'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1162336
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 34930362
+        }
+      }, {
+        bidder: 'rtbhouse',
+        params: {
+          publisherId: '7LTOeORMcRSgGMug4SXQ',
+          region: 'prebid-eu'
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 418806
+        }
+      }, {
+        bidder: 'sspBC'
+      }]
+    }]
+  }
+}
