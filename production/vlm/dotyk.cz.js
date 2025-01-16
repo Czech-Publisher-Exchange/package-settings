77d911469c00909659c0f5b683fc22aa4d6f9a49
CPEx Dev <dev@cpex.cz>
2025-01-16 12:56:04 +0100

    Iveta: vypnutí vignette

--- production/vlm/dotyk.cz.js
+++ production/vlm/dotyk.cz.js
@@ -3777,6 +3777,9 @@ window.cpexWebsiteSettings = {
       adUnits: ['m-square-1', 'm-square-2', 'm-square-3', 'm-square-bottom-1', 'm-square-bottom-2', 'm-square-bottom-3', 'm-square-4', 'm-square-5', 'm-square-gallery-1', 'm-square-gallery-2', 'm-square-gallery-3', 'm-seznam-feed', 'm-square-gallery-4', 'm-square-gallery-5', 'm-square-gallery-6', 'm-square-gallery-7', 'm-square-gallery-8', 'm-square-gallery-9'],
       width: 480,
       height: 480
+    },
+    vignette: {
+      enabled: false
     }
   },
   adserver: {
