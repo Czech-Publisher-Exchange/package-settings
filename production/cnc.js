976f7a9ab18fbb44d6cd304dc6539afb95913bc8
CPEx Dev <dev@cpex.cz>
2025-01-31 10:55:07 +0100

    ales@cpex.cz: Vytvoření CNC publisher settings

---
+++ production/cnc.js
@@ -0,0 +1,19 @@
+window.cpexPublisherSettings = {
+  publisher: {
+    code: 'cnc',
+    name: 'Czech News Center',
+    sellerId: '001'
+  },
+  cmp: {
+    enabled: false
+  },
+  dsa: {
+    enabled: false
+  },
+  adserver: {
+    adapter: 'googletag'
+  },
+  headerbidding: {
+    auctionTimeoutMs: 1300
+  }
+}
