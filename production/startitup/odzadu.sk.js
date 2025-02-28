858b6da6c4ac03f657bbd3cb428dc2538556759d
CPEx Dev <dev@cpex.cz>
2025-02-28 09:40:59 +0100

    ales@cpex.cz: Vytvoření odzadu.sk website settings

---
+++ production/startitup/odzadu.sk.js
@@ -0,0 +1,493 @@
+window.cpexWebsiteSettings = {
+  website: {
+    name: 'odzadu.sk'
+  },
+  adserver: {
+    adapter: 'googletag'
+  },
+  headerbidding: {
+    adUnits: [{
+      code: 'inventory_container_mob_top_square_336x280',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 100],
+            [300, 250],
+            [300, 300],
+            [336, 280]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 583186,
+          zoneId: 3715110,
+          accountId: '10900',
+          position: 'atf'
+        }
+      }, {
+        bidder: 'rubi-mask',
+        params: {
+          siteId: 583188,
+          zoneId: 3715126,
+          accountId: '10900',
+          position: 'atf'
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '6646273'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1188446
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 35241616
+        }
+      }, {
+        bidder: 'rtbhouse',
+        params: {
+          publisherId: 'kCK7yICTB7tEYQDz20G2',
+          region: 'prebid-eu'
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 440003
+        }
+      }, {
+        bidder: 'sspBC'
+      }]
+    }, {
+      code: 'inventory_container_mob_gallery_aft_img_300x250',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 100],
+            [300, 250],
+            [300, 300],
+            [300, 600]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 583186,
+          zoneId: 3715112,
+          accountId: '10900',
+          position: 'atf'
+        }
+      }, {
+        bidder: 'rubi-mask',
+        params: {
+          siteId: 583188,
+          zoneId: 3715128,
+          accountId: '10900',
+          position: 'atf'
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '6646274'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1188447
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 35241614
+        }
+      }, {
+        bidder: 'rtbhouse',
+        params: {
+          publisherId: 'kCK7yICTB7tEYQDz20G2',
+          region: 'prebid-eu'
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 440003
+        }
+      }, {
+        bidder: 'sspBC'
+      }]
+    }, {
+      code: 'inventory_container_mob_art_after_9th_p_300x250',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 100],
+            [300, 250],
+            [300, 300],
+            [336, 280],
+            [300, 600]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 583186,
+          zoneId: 3715114,
+          accountId: '10900',
+          position: 'atf'
+        }
+      }, {
+        bidder: 'rubi-mask',
+        params: {
+          siteId: 583188,
+          zoneId: 3715130,
+          accountId: '10900',
+          position: 'atf'
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '6646275'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1188448
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 35241617
+        }
+      }, {
+        bidder: 'rtbhouse',
+        params: {
+          publisherId: 'kCK7yICTB7tEYQDz20G2',
+          region: 'prebid-eu'
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 440003
+        }
+      }, {
+        bidder: 'sspBC'
+      }]
+    }, {
+      code: 'inventory_container_mob_art_after_share_300x250',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 100],
+            [300, 250],
+            [300, 300],
+            [336, 280]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 583186,
+          zoneId: 3715116,
+          accountId: '10900',
+          position: 'atf'
+        }
+      }, {
+        bidder: 'rubi-mask',
+        params: {
+          siteId: 583188,
+          zoneId: 3715132,
+          accountId: '10900',
+          position: 'atf'
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '6646276'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1188449
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 35241613
+        }
+      }, {
+        bidder: 'rtbhouse',
+        params: {
+          publisherId: 'kCK7yICTB7tEYQDz20G2',
+          region: 'prebid-eu'
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 440003
+        }
+      }, {
+        bidder: 'sspBC'
+      }]
+    }, {
+      code: 'inventory_container_mob_art_after_1st_p_300x250',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 100],
+            [300, 250],
+            [300, 300],
+            [336, 280],
+            [300, 600]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 583186,
+          zoneId: 3715118,
+          accountId: '10900',
+          position: 'atf'
+        }
+      }, {
+        bidder: 'rubi-mask',
+        params: {
+          siteId: 583188,
+          zoneId: 3715134,
+          accountId: '10900',
+          position: 'atf'
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '6646277'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1188450
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 35241618
+        }
+      }, {
+        bidder: 'rtbhouse',
+        params: {
+          publisherId: 'kCK7yICTB7tEYQDz20G2',
+          region: 'prebid-eu'
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 440003
+        }
+      }, {
+        bidder: 'sspBC'
+      }]
+    }, {
+      code: 'inventory_container_mob_art_after_4th_p_300x250',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 50],
+            [320, 100],
+            [300, 100],
+            [300, 250],
+            [300, 300],
+            [336, 280],
+            [300, 600]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 583186,
+          zoneId: 3715120,
+          accountId: '10900',
+          position: 'atf'
+        }
+      }, {
+        bidder: 'rubi-mask',
+        params: {
+          siteId: 583188,
+          zoneId: 3715136,
+          accountId: '10900',
+          position: 'atf'
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '6646278'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1188451
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 35241615
+        }
+      }, {
+        bidder: 'rtbhouse',
+        params: {
+          publisherId: 'kCK7yICTB7tEYQDz20G2',
+          region: 'prebid-eu'
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 440003
+        }
+      }, {
+        bidder: 'sspBC'
+      }]
+    }, {
+      code: 'div-gpt-ad-1693829117697-0',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 600]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 583186,
+          zoneId: 3715122,
+          accountId: '10900',
+          position: 'atf'
+        }
+      }, {
+        bidder: 'rubi-mask',
+        params: {
+          siteId: 583188,
+          zoneId: 3715138,
+          accountId: '10900',
+          position: 'atf'
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '6646279'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1188452
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 35241619
+        }
+      }, {
+        bidder: 'rtbhouse',
+        params: {
+          publisherId: 'kCK7yICTB7tEYQDz20G2',
+          region: 'prebid-eu'
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 440003
+        }
+      }, {
+        bidder: 'sspBC'
+      }]
+    }, {
+      code: 'inventory_container_mob_art_sticky_to_fscreen',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [300, 50],
+            [320, 100],
+            [728, 90],
+            [970, 250]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 583186,
+          zoneId: 3715124,
+          accountId: '10900',
+          position: 'atf'
+        }
+      }, {
+        bidder: 'rubi-mask',
+        params: {
+          siteId: 583188,
+          zoneId: 3715140,
+          accountId: '10900',
+          position: 'atf'
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '6646280'
+        }
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1188453
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 35241620
+        }
+      }, {
+        bidder: 'rtbhouse',
+        params: {
+          publisherId: 'kCK7yICTB7tEYQDz20G2',
+          region: 'prebid-eu'
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 440003
+        }
+      }, {
+        bidder: 'sspBC'
+      }]
+    }]
+  }
+}
