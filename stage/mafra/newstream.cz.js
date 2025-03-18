4c6d4f339d8961218b644a0129d4733145af9c9d
CPEx Dev <dev@cpex.cz>
2025-03-18 13:08:43 +0100

    dev@cpex.cz: Added native templates

--- stage/mafra/newstream.cz.js
+++ stage/mafra/newstream.cz.js
@@ -103,103 +103,6 @@ window.cpexWebsiteSettings = {
         bidder: 'sspBC'
       }]
     }, {
-      code: 'branding',
-      mediaTypes: {
-        banner: {
-          sizes: [
-            [2000, 1400],
-            [998, 200],
-            [728, 90],
-            [970, 210],
-            [970, 90],
-            [2000, 1200],
-            [1920, 1080]
-          ]
-        }
-      },
-      bids: [{
-        bidder: 'rubicon',
-        params: {
-          siteId: 144582,
-          zoneId: 3693492,
-          accountId: '10900',
-          position: 'atf'
-        }
-      }, {
-        bidder: 'rubicon-mask',
-        params: {
-          siteId: 144584,
-          zoneId: 3695326,
-          accountId: '10900',
-          position: 'atf'
-        },
-        bidderModuleName: 'rubicon'
-      }, {
-        bidder: 'pubmatic',
-        params: {
-          publisherId: '158732',
-          adSlot: '6608669'
-        }
-      }, {
-        bidder: 'adform',
-        params: {
-          mid: 2044338,
-          rcur: 'USD'
-        }
-      }, {
-        bidder: 'omg-adform',
-        params: {
-          mid: 2047969,
-          rcur: 'USD'
-        },
-        bidderModuleName: 'adform'
-      }, {
-        bidder: 'fragile-adform',
-        params: {
-          mid: 2046700,
-          rcur: 'USD'
-        },
-        bidderModuleName: 'adform'
-      }, {
-        bidder: 'ix',
-        params: {
-          siteId: 1174319
-        }
-      }, {
-        bidder: 'appnexus',
-        params: {
-          placementId: 35094220
-        }
-      }, {
-        bidder: 'rtbhouse',
-        params: {
-          publisherId: '7LTOeORMcRSgGMug4SXQ',
-          region: 'prebid-eu'
-        }
-      }, {
-        bidder: 'criteo',
-        params: {
-          networkId: 11172,
-          uid: 418806
-        }
-      }, {
-        bidder: 'r2b2',
-        params: {
-          d: 'mafra.newstream.cz',
-          p: 'branding',
-          m: 0,
-          g: 'hb'
-        }
-      }, {
-        bidder: 'connectad',
-        params: {
-          siteId: 3168445,
-          networkId: 131
-        }
-      }, {
-        bidder: 'sspBC'
-      }]
-    }, {
       code: 'halfpagead',
       mediaTypes: {
         banner: {
@@ -927,6 +830,112 @@ window.cpexWebsiteSettings = {
         bidder: 'sspBC'
       }]
     }, {
+      code: 'm_sticky',
+      mediaTypes: {
+        banner: {
+          sizes: [
+            [320, 150],
+            [300, 250],
+            [500, 200],
+            [320, 100],
+            [970, 250],
+            [336, 280],
+            [320, 50],
+            [728, 90],
+            [970, 310],
+            [480, 480],
+            [300, 300],
+            [480, 300],
+            [998, 200],
+            [300, 50],
+            [300, 100],
+            [970, 210],
+            [750, 200],
+            [750, 100]
+          ]
+        }
+      },
+      bids: [{
+        bidder: 'rubicon',
+        params: {
+          siteId: 157330,
+          zoneId: 3693512,
+          accountId: '10900',
+          position: 'atf'
+        }
+      }, {
+        bidder: 'rubicon-mask',
+        params: {
+          siteId: 157332,
+          zoneId: 3695342,
+          accountId: '10900',
+          position: 'atf'
+        },
+        bidderModuleName: 'rubicon'
+      }, {
+        bidder: 'pubmatic',
+        params: {
+          publisherId: '158732',
+          adSlot: '6608677'
+        }
+      }, {
+        bidder: 'adform',
+        params: {
+          mid: 2044340,
+          rcur: 'USD'
+        }
+      }, {
+        bidder: 'omg-adform',
+        params: {
+          mid: 2047989,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'fragile-adform',
+        params: {
+          mid: 2046697,
+          rcur: 'USD'
+        },
+        bidderModuleName: 'adform'
+      }, {
+        bidder: 'ix',
+        params: {
+          siteId: 1174329
+        }
+      }, {
+        bidder: 'teads',
+        params: {
+          placementId: 236882,
+          pageId: 220271
+        }
+      }, {
+        bidder: 'appnexus',
+        params: {
+          placementId: 35094226
+        }
+      }, {
+        bidder: 'rtbhouse',
+        params: {
+          publisherId: '7LTOeORMcRSgGMug4SXQ',
+          region: 'prebid-eu'
+        }
+      }, {
+        bidder: 'criteo',
+        params: {
+          networkId: 11172,
+          uid: 418806
+        }
+      }, {
+        bidder: 'connectad',
+        params: {
+          siteId: 3168453,
+          networkId: 131
+        }
+      }, {
+        bidder: 'sspBC'
+      }]
+    }, {
       code: 'wallpaper',
       mediaTypes: {
         banner: {
@@ -1166,13 +1175,23 @@ window.cpexWebsiteSettings = {
   },
   formats: {
     slideup: {
-      enabled: false,
+      enabled: true,
       adUnits: ['m_sticky']
     },
     skin: {
       enabled: true,
       contentEl: /*S*/ () => { return document.getElementsByClassName('body')[0] } /*E*/,
-      hide: [/*S*/ () => { return document.getElementById('r-leaderboardhp') } /*E*/]
+      hide: [/*S*/ () => { return document.getElementById('r-leaderboardhp') } /*E*/],
+      adUnitsForced: []
+    },
+    native: {
+      templates: {
+        nativ: "<article class='article'> \t<figure class='article_photo'> \t\t<img src='${img}' alt='reklama' class='article_image' width='320' height='225' loading='lazy'> \t</figure> \t<div class='article_text'> \t\t<h2 class='article_heading'>${title}</h2> \t\t<span class='article_category'>Reklama</span> \t\t<p class='article_desc'>${desc}</p> \t</div> \t<a href='${link}' class='article_coverLink' target='_blank'><span>Otevřít reklamu</span></a> </article>",
+        m_nativ: "<article class='article'> \t<figure class='article_photo'> \t\t<img src='${img}' alt='reklama' class='article_image' width='320' height='225' loading='lazy'> \t</figure> \t<div class='article_text'> \t\t<h2 class='article_heading'>${title}</h2> \t\t<span class='article_category'>Reklama</span> \t\t<p class='article_desc'>${desc}</p> \t</div> \t<a href='${link}' class='article_coverLink' target='_blank'><span>Otevřít reklamu</span></a> </article>"
+      }
     }
+  },
+  website: {
+    name: 'newstream.cz'
   }
 }
