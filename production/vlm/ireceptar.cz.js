f91c1f775c01aa490e9614c1b88278d8e57dc5a5
CPEx Dev <dev@cpex.cz>
2025-03-26 15:01:17 +0100

    iveta@cpex.cz: navrácení vignette

--- production/vlm/ireceptar.cz.js
+++ production/vlm/ireceptar.cz.js
@@ -34,7 +34,7 @@ window.cpexWebsiteSettings = {
       adUnits: ['m-square-1', 'm-square-2', 'm-square-3', 'm-square-4', 'm-seznam-feed', 'm-square-gallery-1', 'm-square-gallery-2', 'm-square-gallery-3', ' m-square-gallery-4', 'm-square-gallery-5', 'm-square-gallery-6', 'm-square-gallery-7', 'm-square-gallery-8', 'm-square-gallery-9', 'm-square-5']
     },
     vignette: {
-      enabled: false
+      enabled: true
     }
   },
   website: {
