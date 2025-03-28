bfc7d56aea98cd5218ceb1f569b46b8a2b442961
CPEx Dev <dev@cpex.cz>
2025-03-28 14:03:10 +0100

    dev@cpex.cz: Fix for AB test

--- production/vlm/iglanc.cz_b.js
+++ production/vlm/iglanc.cz_b.js
@@ -3346,25 +3346,56 @@ window.cpexWebsiteSettings = {
     enabled: false
   },
   general: {
-    beforeLoad: /*S*/() => {
+    onLoad: /*S*/() => {
   window.addEventListener('cpexPrebidConfigured', () => {
-    pbjs.setConfig({
-      consentManagement: {
-        gdpr: {
-          cmpApi: 'iab',
-          defaultGdprScope: true,
-          rules: [
-            { purpose: 'storage', enforcePurpose: true, enforceVendor: true },
-            { purpose: 'basicAds', enforcePurpose: false, enforceVendor: false },
-            { purpose: 'measurement', enforcePurpose: false, enforceVendor: false }
-          ],
-          timeout: cpexPackage.settings.headerbidding.cmpLoadTimeoutMs || 2000,
-          actionTimeout: cpexPackage.settings.headerbidding.cmpActionTimeoutMs || 0
-        }
-      },
-    });
     pbjs.bidderSettings.standard.storageAllowed = true
-  })
-}/*E*/
+  });
+  pbjs.setConfig({
+    consentManagement: {
+      gdpr: {
+        cmpApi: 'iab',
+        defaultGdprScope: true,
+        rules: [
+          { purpose: 'storage', enforcePurpose: true, enforceVendor: true },
+          { purpose: 'basicAds', enforcePurpose: false, enforceVendor: false },
+          { purpose: 'measurement', enforcePurpose: false, enforceVendor: false }
+        ],
+        timeout: cpexPackage.settings.headerbidding.cmpLoadTimeoutMs || 2000,
+        actionTimeout: cpexPackage.settings.headerbidding.cmpActionTimeoutMs || 0
+      }
+    }
+  });
+  window.cX = window.cX || {};
+  window.cX.callQueue = window.cX.callQueue || [];
+  window.cX.callQueue.push(['invoke', () => {
+    window.__tcfapi('addEventListener', 2, (data, success) => {
+      if (success === false) {
+        return;
+      }
+      if (data.vendor.consents[570]) {
+        const segments = window.cX.getUserSegmentIds({
+          persistedQueryId: '51ff14b454af0cf4aedc891fee56b86c1aa69a31',
+        });
+        if (Array.isArray(segments) && segments.length) {
+          window.cpexPackage.utils.addElement('iframe', document.body, {
+            src: 'https://cdn.cpex.cz/cookies/save.html?name=exc&time=1209600&data=' + encodeURIComponent(segments.toString()),
+            width: 0,
+            height: 0,
+            style: 'border: none; display: block',
+          });
+        }
+        const pianoId = window.cX.getCxenseUserId();
+        if (pianoId && data.vendor.consents[755]) {
+          window.cpexPackage.utils.addElement('img', document.body, {
+            src: 'https://cm.g.doubleclick.net/pixel?google_nid=cpex_ddp&process_consent=T&google_cm&&cxsite=1140709865545450804&cxckp=' + pianoId,
+            width: 0,
+            height: 0,
+            style: 'display: block',
+          });
+        }
+      }
+    })
+  }])
+} /*E*/
   }
 }
